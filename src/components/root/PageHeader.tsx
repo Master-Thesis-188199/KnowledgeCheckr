2f15c62206dc81de665ba35084f53a96af6663a8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-02 17:39:23 +0100

    style: added dark-text style to PageHeader component

--- src/components/root/PageHeader.tsx
+++ src/components/root/PageHeader.tsx
@@ -1,6 +1,6 @@
 export default async function PageHeader() {
   return (
-    <div className='flex items-center justify-between border-b border-gray-800 bg-gray-400/70 px-6 py-3 text-neutral-600 shadow shadow-neutral-500 dark:gray-500 dark:border-neutral-400 dark:bg-neutral-800 dark:shadow-neutral-600'>
+    <div className='flex items-center justify-between border-b border-gray-800 bg-gray-400/70 px-6 py-3 text-neutral-600 dark:text-neutral-200 shadow shadow-neutral-500 dark:gray-500 dark:border-neutral-400 dark:bg-neutral-800 dark:shadow-neutral-600'>
       <span className='text-xl font-semibold'>KnowledgeCheckr</span>
       <div className='flex items-center gap-2'>
         <span className='rounded-full bg-gray-500 size-6 dark:bg-gray-600' />
