81440fb0993ed10727ce5e211b6180d330ecdf9d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-03 09:39:46 +0100

    ref: updated initial Sidebar width based on AnimationState

--- src/components/root/Navigation/DesktopSideBar.tsx
+++ src/components/root/Navigation/DesktopSideBar.tsx
@@ -13,7 +13,7 @@ export const DesktopSidebar = ({ className, ...props }: React.ComponentProps<typ
     <>
       <motion.div
         className={tw('h-full px-4 py-4 hidden md:flex md:flex-col bg-neutral-100 dark:bg-neutral-800 max-w-[300px] flex-shrink-0', className)}
-        initial={{ width: '60px' }}
+        initial={{ width: isAnimationEnabled ? '60px' : '300px' }}
         animate={{
           width: isAnimationEnabled ? (isOpen ? '300px' : '60px') : '300px',
         }}
