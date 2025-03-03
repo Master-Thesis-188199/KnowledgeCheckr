98f32aa711ac98ef57e1c56fee8559486a85f5d3
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-03 12:03:40 +0100

    feat: added SidebarHoverabilityDetection component

---
+++ src/components/root/Navigation/SidebarHoverabilityDetection.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useSidebarStore } from '@/components/root/Navigation/SidebarStoreProvider'
+import { useEffect } from 'react'
+import { usePrimaryPointerQuery } from '@/hooks/root/Navigation/usePrimaryPointerQuery'
+import useStylusPointerQuery from '@/hooks/root/Navigation/useStylusPointerQuery'
+
+export default function SidebarHoverabilityDetection() {
+  const { setDeviceHoverable } = useSidebarStore((state) => state)
+  const primaryPointer = usePrimaryPointerQuery()
+  const hasStylus = useStylusPointerQuery()
+
+  useEffect(() => {
+    if (primaryPointer === undefined) return
+
+    if (primaryPointer === 'fine' && !hasStylus) {
+      setDeviceHoverable(true)
+      console.info(`Your device does support hovering -> hover animations are enabled.`)
+    } else {
+      console.info(`Your device does not support hovering ${primaryPointer} -> hover animations are disabled.`)
+      setDeviceHoverable(false)
+    }
+  }, [primaryPointer])
+
+  return null
+}
