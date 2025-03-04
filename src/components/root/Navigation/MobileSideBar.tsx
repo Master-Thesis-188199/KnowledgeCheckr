6318ba7b987897783d9e410d96e3a2042b71525d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-04 11:29:48 +0100

    ref: added MobileSideBarToggle to mobile Sidebar

--- src/components/root/Navigation/MobileSideBar.tsx
+++ src/components/root/Navigation/MobileSideBar.tsx
@@ -1,6 +1,19 @@
 'use client'
 import { twMerge as tw } from 'tailwind-merge'
+import { useSidebarStore } from '@/components/root/Navigation/SidebarStoreProvider'
+import ThemeSwitcher from '@/components/root/ThemeSwitcher'
+import MobileSidebarToggleButton from '@/components/root/Navigation/mobile/MobilebarToggleButton'
 
 export default function MobileSideBar({ className }: { className?: string }) {
-  return <div className={tw('h-10 w-full flex-row items-center justify-between bg-neutral-100 px-4 py-4 dark:bg-neutral-800', className)}>Mobile Sidebar</div>
+  const {
+    config: { title },
+  } = useSidebarStore((state) => state)
+
+  return (
+    <div className={tw('h-10 w-full flex-row items-center justify-between bg-neutral-200/70 px-4 py-6 dark:bg-neutral-800', className)}>
+      <MobileSidebarToggleButton />
+      <span className='text-xl font-semibold'>{title}</span>
+      <ThemeSwitcher />
+    </div>
+  )
 }
