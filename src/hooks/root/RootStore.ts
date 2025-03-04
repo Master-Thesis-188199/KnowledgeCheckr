43c241bc4cc191da33bd190a8fa2a080bfa608c0
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-04 10:38:29 +0100

    feat: added RootStore and RootStoreProvider

---
+++ src/hooks/root/RootStore.ts
@@ -0,0 +1,11 @@
+import { createStore } from 'zustand/vanilla'
+
+export type RootState = {
+  theme_cookie: 'dark' | 'light' | undefined
+}
+
+export type RootStore = RootState
+
+export const createRootStore = (initialState?: RootState) => {
+  return createStore<RootStore>()((set) => initialState || ({} as RootState))
+}
