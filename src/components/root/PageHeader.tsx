9337e7a6bd7efdfaa7ddc5d20a4bbde560fbd2d9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-04 08:44:26 +0100

    ref: added ThemeSwitcher to PageHeader component

--- src/components/root/PageHeader.tsx
+++ src/components/root/PageHeader.tsx
@@ -1,11 +1,14 @@
+import ThemeSwitcher from '@/components/root/ThemeSwitcher'
+import getTheme from '@/lib/Shared/getTheme'
+
 export default async function PageHeader() {
+  const theme = await getTheme()
+
   return (
     <div className='flex items-center justify-between bg-gray-300/80 px-6 py-3 text-neutral-600 dark:bg-neutral-900 dark:text-neutral-200 dark:shadow-neutral-600'>
       <span className='text-xl font-semibold'>KnowledgeCheckr</span>
       <div className='flex items-center gap-2'>
-        <span className='size-6 rounded-full bg-gray-500 dark:bg-gray-600' />
-        <span className='text-lg'>Profile</span>
-        <span className='ml-2 rounded-md bg-gray-400 px-3 py-1'>Theme</span>
+        <ThemeSwitcher defaultValue={theme} />
       </div>
     </div>
   )
