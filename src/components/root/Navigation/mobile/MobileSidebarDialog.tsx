0892589e42cf86da992412edb4d9de0ae050879b
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-05 07:59:12 +0100

    ref: updated MobileSidebarDialog animation
    The reason for this change is that this way the animation times are more transparent and intuitive.

--- src/components/root/Navigation/mobile/MobileSidebarDialog.tsx
+++ src/components/root/Navigation/mobile/MobileSidebarDialog.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { Transition } from '@headlessui/react'
 import { twMerge } from 'tailwind-merge'
 import { useSidebarStore } from '@/components/root/Navigation/SidebarStoreProvider'
-import { motion } from 'motion/react'
+import { motion, useAnimate } from 'motion/react'
 
 /**
  * Renders the dialog that slides in from the left and displays renders the provided children in it
@@ -12,6 +12,33 @@ import { motion } from 'motion/react'
  */
 export default function MobileSideBarDialog({ children, visibilityBreakpoints }: { children: React.ReactNode; visibilityBreakpoints?: string }) {
   const { isOpen, toggleSidebar } = useSidebarStore((state) => state)
+  const [scope, animate] = useAnimate()
+
+  const EXIT_ANIMATION_TIME = 1.8
+
+  const animateOpen = async () => {
+    const { current } = scope
+    if (!current) return
+
+    animate(current, { x: '100%' }, { duration: 0.4 })
+    await animate(current, { opacity: 1 }, { duration: 0.4 })
+  }
+
+  const animateClosure = async () => {
+    const { current } = scope
+    if (!current) return
+
+    animate(current, { x: '-80%' }, { duration: EXIT_ANIMATION_TIME })
+    await animate(current, { opacity: 0 }, { duration: EXIT_ANIMATION_TIME * 0.25 })
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      animateOpen()
+    } else {
+      animateClosure()
+    }
+  }, [isOpen])
 
   return (
     <>
@@ -33,22 +60,8 @@ export default function MobileSideBarDialog({ children, visibilityBreakpoints }:
       <motion.div
         className={twMerge('fixed inset-0 z-50 flex', visibilityBreakpoints)}
         initial={{ display: 'none' }}
-        animate={{ display: isOpen ? 'flex' : 'none', transition: { delay: 0, delayChildren: 0 } }}>
-        <motion.div
-          className='relative flex w-full max-w-xs flex-10 sm:max-w-sm'
-          initial={{ translateX: '-100%' }}
-          animate={{
-            transition: {
-              type: 'spring',
-              bounce: 0,
-              translate: {
-                duration: 1.5,
-              },
-              opacity: {}, // sets some value for the opacity animation -> if removed that the animation is not as smooth.
-            },
-            translateX: isOpen ? '0%' : '-100%',
-            opacity: isOpen ? 1 : 0,
-          }}>
+        animate={{ display: isOpen ? 'flex' : 'none', transition: { delay: isOpen ? 0 : 0.3, delayChildren: 0 } }}>
+        <motion.div className='relative flex w-full max-w-xs flex-10 sm:max-w-sm' initial={{ translateX: '-100%' }} ref={scope}>
           {children}
         </motion.div>
         <div className='flex-1' onClick={toggleSidebar} />
