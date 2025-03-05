3769106992d6671d69a13e46b3ccfb633e3f7eb0
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-05 09:22:38 +0100

    ref: simplified ContentPanel component in DesktopSidebar
    The reason for this change is that the nested div elements that wrap the children of the ContentPanel are not needed  and only clutter the DOM structure.

--- src/components/root/Navigation/DesktopSideBar.tsx
+++ src/components/root/Navigation/DesktopSideBar.tsx
@@ -44,10 +44,8 @@ export const DesktopSidebar = ({ className, children }: { children: React.ReactN
 
 function ContentPanel({ children }: { children?: React.ReactNode }) {
   return (
-    <div className='flex flex-1'>
-      <div className='flex h-full w-full flex-1 flex-col gap-2 overflow-auto rounded-tl-2xl rounded-bl-2xl border border-neutral-200 bg-gray-100 p-2 md:p-4 dark:border-neutral-700 dark:bg-neutral-900/60'>
-        {children}
-      </div>
+    <div className='flex flex-1 flex-col gap-2 overflow-auto rounded-tl-2xl rounded-bl-2xl border border-neutral-200 bg-gray-100 p-2 md:p-4 dark:border-neutral-700 dark:bg-neutral-900/60'>
+      {children}
     </div>
   )
 }
