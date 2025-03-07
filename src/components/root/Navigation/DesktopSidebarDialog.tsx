ac8219ed36050d75e6e47a7f5a0ecd0baa910812
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-07 09:14:39 +0100

    ref: externalized DesktopSidebar dialog
    The reason for this change is that this way the DesktopSidebar component must no longer be a client side component. This way we can easily access session information from the server-side in the future.

---
+++ src/components/root/Navigation/DesktopSidebarDialog.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useSidebarStore } from '@/components/root/Navigation/SidebarStoreProvider'
+import { motion } from 'framer-motion'
+import { twMerge as tw } from 'tailwind-merge'
+
+export default function DesktopSidebarDialog({ children, visibilityBreakpoints }: { children: React.ReactNode; visibilityBreakpoints?: string }) {
+  const { isOpen, isAnimationEnabled, debounceClosure } = useSidebarStore((state) => state)
+
+  return (
+    <motion.div
+      className={tw('flex h-full max-w-[300px] flex-shrink-0 bg-neutral-100 px-4 py-4 md:flex-col dark:bg-neutral-800', visibilityBreakpoints)}
+      initial={{ width: isAnimationEnabled ? '60px' : '300px' }}
+      animate={{
+        width: isAnimationEnabled ? (isOpen ? '300px' : '60px') : '300px',
+      }}
+      onMouseEnter={() => debounceClosure(true)}
+      onMouseLeave={() => debounceClosure(false)}>
+      {children}
+    </motion.div>
+  )
+}
