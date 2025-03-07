2f1db485a376b05e64e9ea7932a803b981e5a480
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-07 07:55:41 +0100

    chore: added mongoDB adapter to nextauth options

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,17 @@
 import type { NextAuthOptions } from 'next-auth'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import mongoDbClient from '@/app/api/auth/[...nextauth]/MongoDB-Auth-Client'
+import env from '@/lib/Shared/Env'
 
 export const options: NextAuthOptions = {
   providers: [],
+  adapter: MongoDBAdapter(mongoDbClient, {
+    databaseName: env.NEXTAUTH_MONGODB_DATABASE_NAME,
+    collections: {
+      Users: 'users',
+      Accounts: 'accounts',
+      Sessions: 'sessions',
+      VerificationTokens: 'verification_tokens',
+    },
+  }),
 }
