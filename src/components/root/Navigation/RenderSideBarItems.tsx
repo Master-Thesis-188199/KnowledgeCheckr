db10d39e7fc3ed3e8dbc2e019e9e1d855bb7b87b
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-08 09:42:37 +0100

    ref: applied SidebarElement component to RenderSidebarItems

--- src/components/root/Navigation/RenderSideBarItems.tsx
+++ src/components/root/Navigation/RenderSideBarItems.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import Link, { LinkProps } from 'next/link'
 import { motion } from 'motion/react'
-import { SideBarProps } from '@/components/root/Navigation/SideBar'
 import { twMerge as tw } from 'tailwind-merge'
 import { useSidebarStore } from '@/components/root/Navigation/SidebarStoreProvider'
 import { Pin } from 'lucide-react'
+import SidebarElement from '@/components/root/Navigation/elements/SidebarElement'
 
 export default function RenderSideBarItems() {
   const {
@@ -19,7 +18,7 @@ export default function RenderSideBarItems() {
 
       <div className='mt-8 flex flex-col gap-2 md:gap-3'>
         {elements.map((item, idx) => (
-          <RenderSideBarItem key={idx} item={item} />
+          <SidebarElement key={idx} icon={item.icon} href={item.href || '#'} children={item.label} />
         ))}
       </div>
     </div>
@@ -37,24 +36,5 @@ function PinSidebarButton() {
       onClick={toggleAnimation}>
       <Pin className={tw('size-5 stroke-1', !isAnimationEnabled && 'stroke-2')} />
     </motion.div>
-  )
-}
-
-export function RenderSideBarItem({ item, className, ...props }: { item: SideBarProps['elements'][number]; className?: string; props?: LinkProps }) {
-  const { isAnimationEnabled, isOpen } = useSidebarStore((state) => state)
-
-  return (
-    <Link href={item.href || '#'} className={tw('group/sidebar flex items-center justify-start gap-4 py-2', className)} {...props}>
-      {item.icon}
-
-      <motion.span
-        animate={{
-          display: isAnimationEnabled ? (isOpen ? 'inline-block' : 'none') : 'inline-block',
-          opacity: isAnimationEnabled ? (isOpen ? 1 : 0.0) : 1,
-        }}
-        className='!m-0 inline-block overflow-hidden !p-0 text-sm whitespace-pre text-neutral-700 transition-transform duration-300 group-hover/sidebar:translate-x-1 dark:text-neutral-200'>
-        {item.label}
-      </motion.span>
-    </Link>
   )
 }
