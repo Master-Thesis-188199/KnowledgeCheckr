1054c9e708af320525e3c37f611511a7b1dfbf82
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-08 09:48:00 +0100

    ref: exported iconClasses in sidebarConfig

--- src/components/root/Navigation/SideBarConfiguration.tsx
+++ src/components/root/Navigation/SideBarConfiguration.tsx
@@ -1,7 +1,7 @@
 import { SideBarProps } from '@/components/root/Navigation/SideBar'
 import { ArrowLeftEndOnRectangleIcon, BookOpenIcon, Cog8ToothIcon } from '@heroicons/react/24/outline'
 
-const iconClasses = 'text-neutral-700 dark:text-neutral-200 size-6 flex-shrink-0 ml-[9px]'
+export const iconClasses = 'text-neutral-700 dark:text-neutral-200 size-6 flex-shrink-0 ml-[9px]'
 
 export const sideBarConfiguration: SideBarProps = {
   title: 'KnowledgeCheckr',
