e336e18d5e00e9967a3406ce186a8e2964fd426c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-08 06:50:13 +0100

    chore: allowed github avater paths in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/*',
+      },
+    ],
+  },
   devIndicators: false,
   reactStrictMode: false,
   output: 'standalone',
