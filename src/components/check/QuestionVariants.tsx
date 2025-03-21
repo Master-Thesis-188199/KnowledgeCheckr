fccb5a55c69f225b26757250a049ebb91f191f6b
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-21 07:48:42 +0100

    feat: added OpenQuestion answering option component

--- src/components/check/QuestionVariants.tsx
+++ src/components/check/QuestionVariants.tsx
@@ -2,6 +2,10 @@ import ChoiceQuestionOption from '@/components/check/ChoiceQuestionOption'
 import { ChoiceQuestion } from '@/components/check/ChoiceQuestion'
 import QuestionSelectionProvider from '@/components/check/QuestionSelectionProvider'
 
+export function OpenQuestionAnswerOption() {
+  return <textarea className='size-3.5 h-full flex-1 resize-none appearance-none rounded-md p-2 ring-1 ring-neutral-500 dark:bg-neutral-700/30 dark:ring-neutral-500/80' />
+}
+
 export function ChoiceQuestionAnswerOptions(question: ChoiceQuestion) {
   return (
     <QuestionSelectionProvider maxSelection={question.type === 'single-choice' ? 1 : undefined} autoSwitchAnswer={question.type === 'single-choice'}>
