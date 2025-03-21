df3c4b5e8221eaa1cd447f111693c9fc9721e122
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-21 07:57:27 +0100

    style: added min-height for open-question textarea

--- src/components/check/QuestionVariants.tsx
+++ src/components/check/QuestionVariants.tsx
@@ -3,7 +3,7 @@ import { ChoiceQuestion } from '@/components/check/ChoiceQuestion'
 import QuestionSelectionProvider from '@/components/check/QuestionSelectionProvider'
 
 export function OpenQuestionAnswerOption() {
-  return <textarea className='size-3.5 h-full flex-1 resize-none appearance-none rounded-md p-2 ring-1 ring-neutral-500 dark:bg-neutral-700/30 dark:ring-neutral-500/80' />
+  return <textarea className='size-3.5 h-full min-h-60 flex-1 resize-none appearance-none rounded-md p-2 ring-1 ring-neutral-500 dark:bg-neutral-700/30 dark:ring-neutral-500/80' />
 }
 
 export function ChoiceQuestionAnswerOptions(question: ChoiceQuestion) {
