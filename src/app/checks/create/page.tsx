92a53a0ffa8033ea27d484523e4753db27bc288a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-24 07:03:59 +0100

    style: increased spacing between inputs and labels

--- src/app/checks/create/page.tsx
+++ src/app/checks/create/page.tsx
@@ -8,13 +8,13 @@ export default async function CreateCheckPage() {
     <div>
       <PageHeading title='Create KnowledgeCheck' />
       <div className='columns-xl gap-12 space-y-12'>
-        <Card className='@container p-3' disableHoverStyles>
-          <div className='header -m-3 mb-4 flex flex-col rounded-t-md border-b border-neutral-400 bg-neutral-300 p-2 px-3 text-neutral-600 dark:border-neutral-500 dark:bg-neutral-700/60 dark:text-neutral-300'>
+        <Card className='@container flex flex-col gap-8 p-3' disableHoverStyles>
+          <div className='header -m-3 flex flex-col rounded-t-md border-b border-neutral-400 bg-neutral-300 p-2 px-3 text-neutral-600 dark:border-neutral-500 dark:bg-neutral-700/60 dark:text-neutral-300'>
             <div className='flex items-center justify-between'>
               <h2 className=''>General Information</h2>
             </div>
           </div>
-          <div className='grid grid-cols-[90px_1fr] items-center gap-6 p-2'>
+          <div className='grid grid-cols-[90px_1fr] items-center gap-9 p-2'>
             <InputGroup label='Name' placeholder='Enter the name of your knowledge check' />
             <InputGroup label='Description' className='min-h-20 resize-none' as='textarea' placeholder='Describe the concept of your knowledge check using a few words.' />
             <InputGroup
