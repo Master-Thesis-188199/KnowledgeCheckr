5db47b0f46625690fdf420c01c272bbc728c9e24
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-24 06:51:30 +0100

    style: increased columns-size, gap between label and input

--- src/app/checks/create/page.tsx
+++ src/app/checks/create/page.tsx
@@ -8,15 +8,15 @@ export default async function CreateCheckPage() {
     <div>
       <PageHeading title='Create KnowledgeCheck' />
       {/*grid grid-cols-1 grid-rows-[masonry] gap-8 @[1000px]:grid-cols-2 @[1500px]:grid-cols-3*/}
-      <div className='columns-lg gap-12 space-y-12'>
-        <Card className='@container col-span-1 p-3' disableHoverStyles>
+      <div className='columns-xl gap-12 space-y-12'>
+        <Card className='@container p-3' disableHoverStyles>
           <div className='header -m-3 mb-4 flex flex-col rounded-t-md border-b border-neutral-400 bg-neutral-700/60 p-2 px-3 dark:border-neutral-500 dark:text-neutral-300'>
             <div className='flex items-center justify-between'>
-              <h2 className=''>Basic Information</h2>
+              <h2 className=''>General Information</h2>
             </div>
           </div>
           {/*<div className='grid grid-cols-1 items-start gap-6 @[700px]:grid-cols-1'>*/}
-          <div className='grid grid-cols-[80px_1fr] items-center gap-6 p-2'>
+          <div className='grid grid-cols-[90px_1fr] items-center gap-6 p-2'>
             <InputGroup label='Name' placeholder='Enter the name of your knowledge check' />
             <InputGroup label='Description' className='min-h-20 resize-none' as='textarea' placeholder='Describe the concept of your knowledge check using a few words.' />
             <InputGroup
