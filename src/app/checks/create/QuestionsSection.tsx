379ab53b912012b723d1286cf920ea5a7a4de374
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-25 17:13:44 +0100

    ref: externalized question section from check/create page

---
+++ src/app/checks/create/QuestionsSection.tsx
@@ -0,0 +1,59 @@
+import Card from '@/components/Shared/Card'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/Shared/Popover'
+import { Info, Plus } from 'lucide-react'
+import { DynamicIcon, IconName } from 'lucide-react/dynamic'
+import { cn } from '@/lib/Shared/utils'
+import React from 'react'
+
+export default function QuestionsSection() {
+  return (
+    <Card disableHoverStyles className='break-inside-avoid'>
+      <div className='header -m-3 mb-0 flex flex-col rounded-t-md border-b border-neutral-400 bg-neutral-300 p-2 px-3 text-neutral-600 dark:border-neutral-500 dark:bg-neutral-700/60 dark:text-neutral-300'>
+        <div className='flex items-center justify-between'>
+          <h2 className=''>Questions</h2>
+        </div>
+      </div>
+      <div className='questions'>
+        <div className='if-no-questions-show-empty-container my-20 flex flex-col items-center justify-center gap-6'>
+          <Info className='size-16 text-neutral-400 dark:text-neutral-500' />
+          <span className='tracking-wide text-neutral-500 dark:text-neutral-400'>Currently there are no questions associated to this quiz</span>
+        </div>
+      </div>
+      <div className='flex justify-center gap-8'>
+        <Popover>
+          <PopoverTrigger className='mx-4 flex w-72 items-center justify-center gap-2 rounded-md border-2 border-dashed border-blue-500/70 p-3 tracking-wider hover:cursor-pointer dark:border-neutral-300/70 dark:text-neutral-300 dark:hover:bg-neutral-500/30'>
+            <Plus className='size-5' />
+            Add Question
+          </PopoverTrigger>
+
+          <PopoverContent side='bottom' sideOffset={20} className='border-0 p-0 dark:bg-neutral-900'>
+            <div className='rounded-md border border-neutral-400/40 bg-neutral-200 p-3.5 shadow-md shadow-neutral-300 dark:border-neutral-600 dark:bg-neutral-700/50 dark:text-neutral-200 dark:shadow-neutral-900'>
+              <ul className='space-y-4'>
+                <QuestionPopoverOption text='Single Choice Question' icon='mouse-pointer' />
+                <QuestionPopoverOption text='Multiple Choice Question' icon='list' />
+                <QuestionPopoverOption text='Open Answer Question' icon='type' />
+                <QuestionPopoverOption text='Drag Drop Question' icon='square-dashed-mouse-pointer' />
+              </ul>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    </Card>
+  )
+}
+
+export function QuestionPopoverOption({ icon, text }: { text: string; icon: IconName }) {
+  return (
+    <li>
+      <div
+        className={cn(
+          'flex w-full items-center gap-4 rounded-md p-2 px-3 hover:cursor-pointer hover:ring-[1.5px]',
+          'bg-neutral-300/60 ring-neutral-400/70',
+          'ring-1 dark:bg-neutral-800/60 dark:text-neutral-200/80 dark:ring-neutral-600/80 dark:hover:bg-neutral-800/80 dark:hover:text-neutral-200 dark:hover:ring-neutral-500/90',
+        )}>
+        <DynamicIcon name={icon} className='size-4' />
+        {text}
+      </div>
+    </li>
+  )
+}
--- src/app/checks/create/page.tsx
+++ src/app/checks/create/page.tsx
@@ -2,11 +2,7 @@ import PageHeading from '@/components/Shared/PageHeading'
 import React, { ElementType, InputHTMLAttributes } from 'react'
 import Card from '@/components/Shared/Card'
 import { twMerge } from 'tailwind-merge'
-import { Info } from 'lucide-react'
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/Shared/Popover'
-import { Close } from '@radix-ui/react-popover'
-import { cn } from '@/lib/Shared/utils'
-import { DynamicIcon, IconName } from 'lucide-react/dynamic'
+import QuestionsSection from '@/app/checks/create/QuestionsSection'
 
 export default async function CreateCheckPage() {
   return (
@@ -40,37 +36,7 @@ export default async function CreateCheckPage() {
           <h2 className='text-lg'>Settings</h2>
           <div className='h-[500px]'></div>
         </Card>
-        <Card disableHoverStyles className='break-inside-avoid'>
-          <div className='header -m-3 mb-0 flex flex-col rounded-t-md border-b border-neutral-400 bg-neutral-300 p-2 px-3 text-neutral-600 dark:border-neutral-500 dark:bg-neutral-700/60 dark:text-neutral-300'>
-            <div className='flex items-center justify-between'>
-              <h2 className=''>Questions</h2>
-            </div>
-          </div>
-          <div className='questions'>
-            <div className='if-no-questions-show-empty-container my-20 flex flex-col items-center justify-center gap-6'>
-              <Info className='size-16 text-neutral-400 dark:text-neutral-500' />
-              <span className='tracking-wide text-neutral-500 dark:text-neutral-400'>Currently there are no questions associated to this quiz</span>
-            </div>
-          </div>
-          <div className='flex justify-center gap-8'>
-            <Popover>
-              <PopoverTrigger className='mx-4 w-72 rounded-md border-2 border-dashed border-blue-500/70 p-3 tracking-wider hover:cursor-pointer dark:border-neutral-300/70 dark:text-neutral-300 dark:hover:bg-neutral-500/30'>
-                Add Question
-              </PopoverTrigger>
-
-              <PopoverContent side='bottom' sideOffset={20} className='border-0 p-0 dark:bg-neutral-900'>
-                <div className='rounded-md border border-neutral-400/40 bg-neutral-200 p-3.5 shadow-md shadow-neutral-300 dark:border-neutral-600 dark:bg-neutral-700/50 dark:text-neutral-200 dark:shadow-neutral-900'>
-                  <ul className='space-y-4'>
-                    <QuestionPopoverOption text='Single Choice Question' icon='mouse-pointer' />
-                    <QuestionPopoverOption text='Multiple Choice Question' icon='list' />
-                    <QuestionPopoverOption text='Open Answer Question' icon='type' />
-                    <QuestionPopoverOption text='Drag Drop Question' icon='square-dashed-mouse-pointer' />
-                  </ul>
-                </div>
-              </PopoverContent>
-            </Popover>
-          </div>
-        </Card>
+        <QuestionsSection />
         <Card className='h-60 break-inside-avoid' children={<></>} disableHoverStyles></Card>
       </div>
     </div>
@@ -92,21 +58,5 @@ function InputGroup<E extends ElementType>({ label, as, ...props }: { label: str
         )}
       />
     </>
-  )
-}
-
-function QuestionPopoverOption({ icon, text }: { text: string; icon: IconName }) {
-  return (
-    <li>
-      <Close
-        className={cn(
-          'flex w-full items-center gap-4 rounded-md p-2 px-3 hover:cursor-pointer hover:ring-[1.5px]',
-          'bg-neutral-300/60 ring-neutral-400/70',
-          'ring-1 dark:bg-neutral-800/60 dark:text-neutral-200/80 dark:ring-neutral-600/80 dark:hover:bg-neutral-800/80 dark:hover:text-neutral-200 dark:hover:ring-neutral-500/90',
-        )}>
-        <DynamicIcon name={icon} className='size-4' />
-        {text}
-      </Close>
-    </li>
   )
 }
