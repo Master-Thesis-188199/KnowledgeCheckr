73fc2bfb3ab7d5246873b8d3d7bc3583e1d61cf9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-03-29 07:01:01 +0100

    feat: added Input component for unified styles

---
+++ src/components/Shared/form/Input.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { InputHTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      placeholder='Enter some text'
+      {...props}
+      className={twMerge(
+        'rounded-md px-3 py-1.5 text-neutral-600 ring-1 ring-neutral-400 outline-none placeholder:text-neutral-400/90 hover:cursor-text hover:ring-neutral-500 focus:ring-[1.2px] focus:ring-neutral-700 dark:text-neutral-300/80 dark:ring-neutral-500 dark:placeholder:text-neutral-400/50 dark:hover:ring-neutral-300/60 dark:focus:ring-neutral-300/80',
+        props.className,
+      )}
+    />
+  )
+}
