2f0ca0a4481441baac41efdd4d04fdf37bc50587
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-11 16:58:04 +0200

    ref: updated user session avatar display in UserBanner
    The reason for this change is that some users may not be have an image associated to their account, depending on the login / signup method.

--- src/components/root/Navigation/elements/SidebarUserBanner.tsx
+++ src/components/root/Navigation/elements/SidebarUserBanner.tsx
@@ -1,16 +1,19 @@
-import { getServerSession } from 'next-auth'
-import { options } from '@/app/api/auth/[...nextauth]/options'
-import Image from 'next/image'
-import { iconClasses } from '@/components/root/Navigation/SideBarConfiguration'
-import { UserRound } from 'lucide-react'
 import { SidebarElement } from '@/components/root/Navigation/elements/RenderSideBarItems'
-
+import { iconClasses } from '@/components/root/Navigation/SideBarConfiguration'
+import { getServerSession } from '@/src/lib/auth/server'
+import { UserRound } from 'lucide-react'
+import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
 export default async function SidebarUserBanner() {
-  const session = await getServerSession(options)
+  const session = await getServerSession()
 
   if (!session || !session.user) return LoginBanner()
 
-  const UserAvatar = <Image src={session.user.image || ''} alt='User Avatar' height={24} width={24} className={iconClasses} />
+  const UserAvatar = session.user.image ? (
+    <Image src={session.user.image} alt='User Avatar' height={24} width={24} className={iconClasses} />
+  ) : (
+    <Image src={`https://ui-avatars.com/api/?name=${encodeURI(session.user.name)}`} className={twMerge(iconClasses, 'rounded-full')} alt='User Avatar' height={24} width={24} />
+  )
 
   return (
     <SidebarElement icon={UserAvatar} href={'api/auth/signout'}>
