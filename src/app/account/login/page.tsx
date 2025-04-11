8a0489e6a9c8b99f702388a43a6e4b1909c2608d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-11 17:29:24 +0200

    style: added light-mode styles for login page

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -16,7 +16,7 @@ export default async function LoginPage({ searchParams }: { searchParams: Promis
 
   return (
     <div className='flex h-full flex-1 items-center justify-center'>
-      <form className='flex max-w-md flex-1 flex-col gap-6 rounded-md bg-gradient-to-b from-neutral-800 via-neutral-800/70 to-neutral-800 p-6 ring-1 ring-neutral-600/70'>
+      <form className='flex max-w-md flex-1 flex-col gap-6 rounded-md bg-gradient-to-b from-neutral-200/40 via-neutral-200/70 to-neutral-200/40 p-6 ring-1 ring-neutral-300 dark:from-neutral-800 dark:via-neutral-800/70 dark:to-neutral-800 dark:ring-neutral-600/70'>
         <div className='flex flex-col gap-6'>
           {type === 'signup' ? <SignupFields /> : <LoginFields />}
           <div className='relative'>
@@ -25,7 +25,7 @@ export default async function LoginPage({ searchParams }: { searchParams: Promis
             </div>
 
             <div className='relative flex justify-center'>
-              <p className='flex gap-2 bg-neutral-800 px-3 text-sm leading-6 tracking-widest text-gray-900 dark:text-neutral-400'>
+              <p className='flex gap-2 bg-neutral-200 px-3 text-sm leading-6 tracking-widest text-gray-900 dark:bg-neutral-800 dark:text-neutral-400'>
                 <span className='capitalize'>{type}</span>
                 <span>via</span>
               </p>
@@ -45,7 +45,10 @@ export default async function LoginPage({ searchParams }: { searchParams: Promis
 
 function SocialButton({ icon: Icon, iconClassName, ...props }: { icon: React.ComponentType<{ className?: string }>; iconClassName?: string } & ProviderButtonProps) {
   return (
-    <ProviderButton type='button' className='flex items-center justify-evenly gap-4 rounded-sm bg-neutral-800/50 px-3 py-2.5 tracking-wide ring-1 ring-neutral-600 hover:cursor-pointer' {...props}>
+    <ProviderButton
+      type='button'
+      className='flex items-center justify-evenly gap-4 rounded-sm bg-neutral-300/60 px-3 py-2.5 tracking-wide ring-1 ring-neutral-400 hover:cursor-pointer dark:bg-neutral-800/50 dark:ring-neutral-600'
+      {...props}>
       <Icon className={twMerge('size-6', iconClassName)} />
     </ProviderButton>
   )
@@ -55,7 +58,7 @@ function FormHeader({ title, subTitle }: { title: string; subTitle?: string }) {
     <div className='mb-2 flex flex-col items-center gap-2'>
       <Image src={KnowledgeCheckrIcon} alt='KnowledgeCheck-Icon' className='size-12' />
       <h1 className='text-xl font-semibold'>{title}</h1>
-      <span className='-mt-1 text-sm text-gray-300/70'>{subTitle}</span>
+      <span className='-mt-1 text-sm text-neutral-400 dark:text-gray-300/70'>{subTitle}</span>
     </div>
   )
 }
@@ -75,12 +78,12 @@ function LoginFields() {
       </label>
 
       <div className='mt-2 flex flex-col items-center justify-center gap-3'>
-        <button className='mt-2 w-full max-w-xs self-center rounded-lg bg-neutral-700/40 px-4 py-2 ring-1 ring-neutral-700 outline-0 hover:cursor-pointer hover:bg-neutral-700/70 hover:ring-[1.8px] hover:ring-neutral-600/80 active:bg-neutral-700/90 active:ring-neutral-600'>
+        <button className='mt-2 w-full max-w-xs self-center rounded-lg bg-neutral-300 px-4 py-2 ring-1 ring-neutral-400 outline-0 hover:cursor-pointer hover:bg-neutral-400/40 hover:ring-[1.8px] hover:ring-neutral-400/70 active:bg-neutral-700/90 active:ring-neutral-600 dark:bg-neutral-700/40 dark:ring-neutral-700 dark:hover:bg-neutral-700/70 dark:hover:ring-neutral-600/80'>
           Login
         </button>
-        <p className='text-sm text-neutral-400/70'>
+        <p className='text-sm text-neutral-400 dark:text-neutral-400/70'>
           Don&apos;t have an Account?{' '}
-          <Link href='/account/login?type=signup' className='px-2 text-neutral-200 hover:cursor-pointer hover:underline'>
+          <Link href='/account/login?type=signup' className='px-2 text-neutral-600 hover:cursor-pointer hover:underline dark:text-neutral-200'>
             Signup
           </Link>
         </p>
@@ -108,12 +111,12 @@ function SignupFields() {
       </label>
 
       <div className='mt-2 flex flex-col items-center justify-center gap-3'>
-        <button className='mt-2 w-full max-w-xs self-center rounded-lg bg-neutral-700/40 px-4 py-2 ring-1 ring-neutral-700 outline-0 hover:cursor-pointer hover:bg-neutral-700/70 hover:ring-[1.8px] hover:ring-neutral-600/80 active:bg-neutral-700/90 active:ring-neutral-600'>
+        <button className='mt-2 w-full max-w-xs self-center rounded-lg bg-neutral-300 px-4 py-2 ring-1 ring-neutral-400 outline-0 hover:cursor-pointer hover:bg-neutral-400/40 hover:ring-[1.8px] hover:ring-neutral-400/70 active:bg-neutral-700/90 active:ring-neutral-600 dark:bg-neutral-700/40 dark:ring-neutral-700 dark:hover:bg-neutral-700/70 dark:hover:ring-neutral-600/80'>
           Login
         </button>
-        <p className='text-sm text-neutral-400/70'>
+        <p className='text-sm text-neutral-400 dark:text-neutral-400/70'>
           Already have an Account?
-          <Link href='/account/login?type=signin' className='px-2 text-neutral-200 hover:cursor-pointer hover:underline'>
+          <Link href='/account/login?type=signin' className='px-2 text-neutral-600 hover:cursor-pointer hover:underline dark:text-neutral-200'>
             Signin
           </Link>
         </p>
