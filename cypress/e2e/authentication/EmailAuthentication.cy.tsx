c3f62055df0e4abfe6af65f72aeb14e396540afd
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-12 12:59:19 +0200

    ref: improved signup test-case

--- cypress/e2e/authentication/EmailAuthentication.cy.tsx
+++ cypress/e2e/authentication/EmailAuthentication.cy.tsx
@@ -10,12 +10,13 @@ describe('Better Auth: Email Authentication - ', () => {
   })
 
   it('verify that users can signup', () => {
-    const random_email = `test${Math.floor(Math.random() * 10000)}@example.com`
+    const EMAIL = `test${Math.floor(Math.random() * 10000)}@example.com`
+    const USERNAME = `Test User`
+
     cy.visit('/account/login?type=signup')
 
-    // cy.get('input[name="name"]').should('be.visible').type('Test User')
-    cy.get('input[name="name"]').filter(':visible').type('Test User')
-    cy.get('input[name="email"]').filter(':visible').type(random_email)
+    cy.get('input[name="name"]').filter(':visible').type(USERNAME)
+    cy.get('input[name="email"]').filter(':visible').type(EMAIL)
     cy.get('input[name="password"]').filter(':visible').type('1234567890')
 
     cy.get('button[type="submit"]').filter(':visible').click()
@@ -24,8 +25,8 @@ describe('Better Auth: Email Authentication - ', () => {
     cy.url().should('equal', `${Cypress.config('baseUrl')}/`)
 
     cy.task('removeDBUser', {
-      email: random_email,
-      username: 'Test User',
+      email: EMAIL,
+      username: USERNAME,
     })
   })
 
