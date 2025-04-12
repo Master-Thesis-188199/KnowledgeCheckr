247f4758b587033eae7c49f0c947afe90d0b39f0
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-12 12:02:07 +0200

    test: added test-case to verify signout after signin

--- cypress/e2e/authentication/EmailAuthentication.cy.tsx
+++ cypress/e2e/authentication/EmailAuthentication.cy.tsx
@@ -23,4 +23,27 @@ describe('Better Auth: Email Authentication - ', () => {
     // Verify redirect after signup
     cy.url().should('equal', `${Cypress.config('baseUrl')}/`)
   })
+
+  it.only('verify that users can logout again after signup', () => {
+    const EMAIL = `test${Math.floor(Math.random() * 10000)}@example.com`
+    const USERNAME = `Test User`
+
+    cy.visit('/account/login?type=signup')
+
+    cy.get('input[name="name"]').filter(':visible').type(USERNAME)
+    cy.get('input[name="email"]').filter(':visible').type(EMAIL)
+    cy.get('input[name="password"]').filter(':visible').type('1234567890')
+
+    cy.get('button[type="submit"]').filter(':visible').click()
+
+    // Verify redirect after signup
+    cy.url().should('equal', `${Cypress.config('baseUrl')}/`)
+
+    cy.visit('/account')
+    cy.get('main * span').filter(':visible').contains(EMAIL).should('be.visible')
+    cy.get('main * span').filter(':visible').contains(USERNAME).should('be.visible')
+    cy.get('main * button').contains('Signout').filter(':visible').click()
+
+    cy.url().should('equal', `${Cypress.config('baseUrl')}/account/login`)
+  })
 })
