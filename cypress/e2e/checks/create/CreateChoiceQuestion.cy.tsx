3ffaec2455a097b0bae59eb8d025f808f81318d8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-13 15:05:28 +0200

    test: verifies errors for invalid question inputs are shown

--- cypress/e2e/checks/create/CreateChoiceQuestion.cy.tsx
+++ cypress/e2e/checks/create/CreateChoiceQuestion.cy.tsx
@@ -48,7 +48,50 @@ describe('Check: Create Choice Question -', () => {
     cy.get("[data-slot='dialog-trigger']").contains('Create Question').should('have.attr', 'data-state', 'closed')
   })
 
-  it.only('Verify that answers can be added and removed', () => {
+  it('Verify that errors are displayed when the inputs are invalid', () => {
+    const { question, points, type, answers }: Partial<ChoiceQuestion> = {
+      question: 'Invalid Question',
+      points: -5,
+      type: 'multiple-choice',
+      answers: [
+        { answer: 'Duplicate', correct: false },
+        { answer: 'Duplicate', correct: false },
+        { answer: 'Madrid', correct: false },
+        { answer: 'Rome', correct: false },
+      ],
+    }
+
+    cy.get("input[name='question']").clear().type(question)
+    cy.get("input[name='points']").clear().type(points.toString())
+
+    cy.get("input[name='type'] + button[aria-label='popover-trigger-type']").click()
+    cy.get(`[aria-label="popover-content-type"] * div[data-slot="command-item"][data-value="${type}"]`).click()
+
+    for (let i = 0; i < answers.length; i++) {
+      cy.get(`#question-answers * input[name='answers.${i}.answer']`).should('exist').clear().type(answers[i].answer)
+
+      cy.get(`#question-answers * input[name='answers.${i}.correct']`)
+        .should('exist')
+        .then((val) => {
+          if (val.is(':checked') && !answers[i].correct) {
+            // Uncheck
+            cy.get(`#question-answers * input[name='answers.${i}.correct']`).should('exist').parent().click()
+          } else if (val.is(':not(:checked)') && answers[i].correct) {
+            // Check
+            cy.get(`#question-answers * input[name='answers.${i}.correct']`).should('exist').parent().click()
+          }
+        })
+    }
+
+    cy.get("#question-dialog * button[type='submit']").should('exist').click({ force: true })
+
+    // Check for error messages
+    cy.get('#question-dialog * div[aria-label="field-error-question"]').should('exist').contains('Please reformulate your question to be at least 3 words long.')
+    cy.get('#question-dialog * div[aria-label="field-error-points"]').should('exist').contains('Number must be greater than 0')
+    cy.get('#question-dialog * div[aria-label="field-error-answers.root"]').should('exist').contains('At least one answer has to be correct!')
+  })
+
+  it('Verify that answers can be added and removed', () => {
     cy.get('#question-answers').should('exist')
 
     cy.get('#question-answers * input')
