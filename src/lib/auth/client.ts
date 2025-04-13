0081c3b9e173801caa10389ddf3c7841e47cae3e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-13 16:56:38 +0200

    fix: replaced static baseUrl with environment variable
    The reason for this change is that previously the base-url of the better auth client was set statically, which lead to issues when accessing the website from an external domain.

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,8 +1,9 @@
+import env from '@/src/lib/Shared/Env'
 import { createAuthClient } from 'better-auth/react'
 
 export const auth_client = createAuthClient({
   /** the base url of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL: env.BASE_URL,
 })
 
 export const { signIn, signUp, signOut, useSession } = auth_client
