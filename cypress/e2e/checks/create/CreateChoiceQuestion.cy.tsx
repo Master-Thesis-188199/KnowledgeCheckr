0c10d00c3f98135d32cdd501bf511ceacb8a251c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-04-13 12:26:44 +0200

    test: verifies that valid choice-question can be created

---
+++ cypress/e2e/checks/create/CreateChoiceQuestion.cy.tsx
@@ -0,0 +1,51 @@
+import { ChoiceQuestion } from '@/src/schemas/QuestionSchema'
+
+describe('Check: Create Choice Question -', () => {
+  beforeEach(() => {
+    cy.visit('/checks/create')
+
+    cy.get("[data-slot='dialog-trigger']").should('exist').contains('Create Question').click()
+    cy.get("[data-slot='dialog-trigger']").contains('Create Question').should('have.attr', 'data-state', 'open')
+    cy.get('#question-dialog').should('be.visible')
+  })
+
+  it.only('Verify that a choice question can be added when the inputs are valid', () => {
+    const { question, points, type, answers }: Partial<ChoiceQuestion> = {
+      question: 'What is the capital of France?',
+      points: 5,
+      type: 'single-choice',
+      answers: [
+        { answer: 'Berlin', correct: false },
+        { answer: 'Paris', correct: true },
+        { answer: 'Madrid', correct: false },
+        { answer: 'Rome', correct: false },
+      ],
+    }
+
+    cy.get("input[name='question']").clear().type(question)
+    cy.get("input[name='points']").clear().type(points.toString())
+
+    cy.get("input[name='type'] + button[aria-label='popover-trigger-type']").click()
+    cy.get(`[aria-label="popover-content-type"] * div[data-slot="command-item"][data-value="${type}"]`).click()
+
+    for (let i = 0; i < answers.length; i++) {
+      cy.get(`#question-answers * input[name='answers.${i}.answer']`).should('exist').clear().type(answers[i].answer)
+
+      cy.get(`#question-answers * input[name='answers.${i}.correct']`)
+        .should('exist')
+        .then((val) => {
+          if (val.is(':checked') && !answers[i].correct) {
+            // Uncheck
+            cy.get(`#question-answers * input[name='answers.${i}.correct']`).should('exist').parent().click()
+          } else if (val.is(':not(:checked)') && answers[i].correct) {
+            // Check
+            cy.get(`#question-answers * input[name='answers.${i}.correct']`).should('exist').parent().click()
+          }
+        })
+    }
+
+    cy.get("#question-dialog * button[type='submit']").should('exist').click({ force: true })
+
+    cy.get("[data-slot='dialog-trigger']").contains('Create Question').should('have.attr', 'data-state', 'closed')
+  })
+})
