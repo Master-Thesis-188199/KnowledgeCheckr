f28052c1e87bf3681bd63db18fee0915dc994efb
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-05-04 09:34:51 +0200

    ref: added insert utility method to Database handler
    The reason for this change is that the insert statement now returns a ResultSetHeader object instead of a QueryResult, which does not contain the proper id of the new element. Hence a select statement retrieves the newly inserted element and returns its fields.

--- database/Database.ts
+++ database/Database.ts
@@ -1,23 +1,67 @@
 'use server'
 
 import env from '@/src/lib/Shared/Env'
-import mysql from 'mysql2/promise'
+import { Any } from '@/types'
+import mysql, { Connection } from 'mysql2/promise'
 
+export type DBConnection = Connection & {
+  insert: <T = Any>(query: string, values?: Any[]) => Promise<{ [key: string]: T } | never>
+}
+
+let connection: DBConnection | null = null
 export default async function getDatabase() {
-  const connection = await getConnection()
+  if (connection === null) {
+    connection = await getConnection()
+  }
 
   return connection
 }
 
 async function getConnection() {
-  const connection = await mysql.createConnection({
+  const connection: DBConnection = (await mysql.createConnection({
     host: env.DATABASE_HOST,
     user: env.DATABASE_USER,
     password: env.DATABASE_PASSWORD,
     database: env.DATABASE_NAME,
-  })
+  })) as DBConnection
 
   await connection.connect()
 
+  connection.insert = insert
+
   return connection
+}
+
+async function insert(query: string, values?: Any[]) {
+  await connection!.execute<Any>(query, values)
+
+  //? Select the last element that was inserted that matches the query-fields and values
+  const table = query.split(' ').at(2)
+  const fields = query
+    .split('(')!
+    .at(1)!
+    .split(')')[0]
+    .split(',')
+    .map((f) => f.trim())
+
+  const valueWhereClause = (field: string, value: unknown) => {
+    if (typeof value === 'string') {
+      return `${field} = '${value}'`
+    } else if (value === null) {
+      return `${field} IS NULL`
+    } else {
+      return `${field} = ` + value
+    }
+  }
+
+  const select_query = `SELECT ${fields.join(', ')} FROM ${table} WHERE ${fields.map((f, i) => valueWhereClause(f, values?.at(i))).join(' AND ')}`
+  const [elements] = await connection!.query<Any>(select_query)
+
+  if (elements.length === 0) {
+    throw new Error(`Inserted Element was not found! ${select_query}`)
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1500))
+
+  return elements.at(0)
 }
