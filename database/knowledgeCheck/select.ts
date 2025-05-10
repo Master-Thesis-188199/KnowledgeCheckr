46e6ec8b0788f3b844b497c7efe397a30acf495e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-05-10 15:31:56 +0200

    feat: created select knowledgeCheck dao module

---
+++ database/knowledgeCheck/select.ts
@@ -0,0 +1,51 @@
+'use server'
+
+import getDatabase from '@/database/Database'
+import getKnowledgeCheckQuestions from '@/database/knowledgeCheck/questions/select'
+import { DbKnowledgeCheck } from '@/database/knowledgeCheck/type'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { Question } from '@/src/schemas/QuestionSchema'
+
+export async function getKnowledgeChecks({ limit = 10 }: { limit?: number }) {
+  const db = await getDatabase()
+  const checks: KnowledgeCheck[] = []
+
+  const knowledgeChecks = await db.exec<DbKnowledgeCheck[]>(`SELECT * FROM KnowledgeCheck Limit ${limit > 100 ? 100 : limit}`)
+  for (const knowledgeCheck of knowledgeChecks) {
+    const questions = await getKnowledgeCheckQuestions(db, knowledgeCheck.id)
+    const parsedKnowledgeCheck = parseKnowledgeCheck(knowledgeCheck, questions)
+
+    checks.push(parsedKnowledgeCheck)
+  }
+
+  return checks
+}
+
+export async function getKnowledgeCheckById(id: KnowledgeCheck['id']): Promise<KnowledgeCheck | null> {
+  const db = await getDatabase()
+  const checks: KnowledgeCheck[] = []
+
+  const knowledgeChecks = await db.exec<DbKnowledgeCheck[]>(`SELECT * FROM KnowledgeCheck WHERE id = ?`, [id])
+  for (const knowledgeCheck of knowledgeChecks) {
+    const questions = await getKnowledgeCheckQuestions(db, knowledgeCheck.id)
+    const parsedKnowledgeCheck = parseKnowledgeCheck(knowledgeCheck, questions)
+
+    checks.push(parsedKnowledgeCheck)
+  }
+
+  return checks?.at(0) || null
+}
+
+function parseKnowledgeCheck(knowledgeCheck: DbKnowledgeCheck, questions: Question[]): KnowledgeCheck {
+  return {
+    id: knowledgeCheck.id,
+    name: knowledgeCheck.name,
+    description: knowledgeCheck.description,
+    share_key: knowledgeCheck.public_token,
+    questions,
+    difficulty: knowledgeCheck.difficulty,
+    closeDate: knowledgeCheck.closeDate,
+    openDate: knowledgeCheck.openDate,
+    questionCategories: [],
+  }
+}
