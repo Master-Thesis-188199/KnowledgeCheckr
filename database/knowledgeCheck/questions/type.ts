1b6367801239c6454b3ca12b876ec83db8742101
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-05-10 15:27:46 +0200

    feat: declared interfaces for database table structures

---
+++ database/knowledgeCheck/questions/type.ts
@@ -0,0 +1,22 @@
+export interface DbQuestion {
+  id: string
+  type: 'single-choice' | 'multiple-choice' | 'drag-drop' | 'open-question'
+  points: number
+  question: string
+  category_id: string
+  knowledgecheck_id: string
+}
+
+export interface DBAnswer {
+  id: string
+  answer: string
+  correct: 0 | 1 | null
+  position: number | null
+  Question_id: string
+}
+
+export interface DBCategory {
+  id: string
+  name: string
+  prerequisite_category_id: string | null
+}
