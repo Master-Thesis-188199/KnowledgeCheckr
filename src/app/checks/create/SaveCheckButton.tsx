0cfd9a8ae9cd8ad91a98e2f147a9f299bd87bf0a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-05-10 15:58:49 +0200

    ref: drafted SaveCreateCheckButton component

---
+++ src/app/checks/create/SaveCheckButton.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { saveAction } from '@/src/app/checks/create/SaveAction'
+import { useCreateCheckStore } from '@/src/components/check/create/CreateCheckProvider'
+import { Button } from '@/src/components/shadcn/button'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { User } from 'better-auth'
+
+export function SaveCreateCheckButton({ user_id }: { user_id: User['id'] }) {
+  const store = useCreateCheckStore((store) => store)
+  const check: KnowledgeCheck = {
+    id: store.id,
+    name: store.name,
+    description: store.description,
+    share_key: store.share_key,
+    closeDate: store.closeDate,
+    difficulty: store.difficulty,
+    openDate: store.openDate,
+    questionCategories: store.questionCategories,
+    questions: store.questions,
+  }
+
+  return (
+    <Button type='submit' formAction={() => saveAction({ user_id, check })}>
+      Save
+    </Button>
+  )
+}
