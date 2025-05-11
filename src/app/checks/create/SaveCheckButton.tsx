c435d50b7913630ea5dc49fd0524d1745d823a01
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-05-11 09:56:11 +0200

    ref: added aria-label to SaveCheckButton

--- src/app/checks/create/SaveCheckButton.tsx
+++ src/app/checks/create/SaveCheckButton.tsx
@@ -21,7 +21,7 @@ export function SaveCreateCheckButton({ user_id }: { user_id: User['id'] }) {
   }
 
   return (
-    <Button type='submit' formAction={() => saveAction({ user_id, check })}>
+    <Button aria-label='save created knowledge check' type='submit' formAction={() => saveAction({ user_id, check })}>
       Save
     </Button>
   )
