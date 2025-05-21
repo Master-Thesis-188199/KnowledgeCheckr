a970436e052abaffb9dd2c33b227ebed24d97514
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-05-21 17:57:18 +0200

    style: adjusted Card dark mode bg and ring styles

--- src/components/Shared/Card.tsx
+++ src/components/Shared/Card.tsx
@@ -4,8 +4,8 @@ export default function Card({ className, children, disableHoverStyles }: { clas
   return (
     <div
       className={twMerge(
-        'rounded-md bg-neutral-200/40 p-3 ring-1 ring-neutral-400/70 dark:bg-neutral-700/40 dark:ring-neutral-600',
-        !disableHoverStyles && 'dark:hover:bg-neutral-700 dark:hover:ring-neutral-500',
+        'rounded-md bg-neutral-200/40 p-3 ring-1 ring-neutral-400/70 dark:bg-neutral-700/30 dark:ring-neutral-600',
+        !disableHoverStyles && 'dark:hover:bg-neutral-700/70 dark:hover:ring-neutral-500/70',
         className,
       )}>
       {children}
