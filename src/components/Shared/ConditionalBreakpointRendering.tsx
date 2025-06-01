a47d4f6dee40c20935ff3d77d8590f12c398a44a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-01 09:24:58 +0200

    feat: created conditional rendering component
    This component is used to conditionally render its children based on a breakpoint. This way the we can remove the duplicate rendering of the desktop-sidebar variants' children.

---
+++ src/components/Shared/ConditionalBreakpointRendering.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import useMatchMedia from '@/hooks/Shared/useMatchMedia'
+
+interface ConditionalSidebarProps {
+  children: React.ReactNode
+
+  hideBreakPoint?: string
+  showBreakPoint?: string
+  logIdentifier?: string
+}
+
+export default function ConditionalBreakpointRendering({ children, showBreakPoint, hideBreakPoint, logIdentifier }: ConditionalSidebarProps) {
+  if (!showBreakPoint && !hideBreakPoint) throw new Error('[ConditionalBreakpointRendering]: Either showBreakPoint or hideBreakPoint must be provided')
+
+  const mediaQuery = hideBreakPoint ? `(max-width: ${hideBreakPoint})` : `(min-width: ${showBreakPoint})`
+  const matches = useMatchMedia(mediaQuery)
+
+  if (!matches) {
+    console.debug(`[ConditionalRendering]: ${logIdentifier ?? '[?]'} - was not rendered because the media-query "${mediaQuery}" did not match.`)
+    return null
+  }
+
+  return <>{children}</>
+}
