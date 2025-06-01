aec32ea4d76110f5e91c00e738a74c191cd219c4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-01 13:48:33 +0200

    Revert "ref: added single mount safety check to ToastBox"
    This reverts commit d864f51edb506148dd135311cbc41d283aa60a4e.

--- src/components/Shared/Toast/ToastBox.tsx
+++ src/components/Shared/Toast/ToastBox.tsx
@@ -1,23 +1,7 @@
 'use client'
-
 import '@/components/Shared/Toast/ToastStyles.css'
-import { useEffect } from 'react'
 import { ToastContainer, ToastContainerProps } from 'react-toastify'
 
-// Module-level flag – its value is shared by every instance
-let alreadyMounted = false
-
 export default function ToastBox(config?: ToastContainerProps) {
-  useEffect(() => {
-    if (alreadyMounted) {
-      throw new Error('ToastBox has already been mounted. Only one instance is allowed.')
-    }
-    alreadyMounted = true
-
-    return () => {
-      alreadyMounted = false
-    }
-  }, [])
-
-  return <ToastContainer toastClassName='md:ml-12' autoClose={5000} closeOnClick limit={5} pauseOnHover={false} pauseOnFocusLoss theme='colored' position='bottom-center' {...config} />
+  return <ToastContainer toastClassName='md:ml-12' autoClose={5000} closeOnClick limit={5} pauseOnHover={false} pauseOnFocusLoss={true} theme={'colored'} position={'bottom-center'} {...config} />
 }
