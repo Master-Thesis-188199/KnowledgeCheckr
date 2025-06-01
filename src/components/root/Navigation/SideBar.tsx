6aeebdda7226d4979784636975a00be3b804bdae
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-01 09:26:44 +0200

    ref: conditionalized rendering of Sidebar variants
    This way the content (children) passed to the sidebar variants is only rendered once, resolving many issues that one previously faced due to the duplicate rendering.

--- src/components/root/Navigation/SideBar.tsx
+++ src/components/root/Navigation/SideBar.tsx
@@ -1,7 +1,8 @@
-import { SidebarStoreProvider, SidebarStoreProviderProps } from '@/components/root/Navigation/SidebarStoreProvider'
 import { DesktopSidebar } from '@/components/root/Navigation/desktop/DesktopSideBar'
 import MobileSideBar from '@/components/root/Navigation/mobile/MobileSideBar'
+import { SidebarStoreProvider, SidebarStoreProviderProps } from '@/components/root/Navigation/SidebarStoreProvider'
 import { ActiveDelimiter } from '@/src/components/root/Navigation/ActiveDelimiter'
+import ConditionalBreakpointRendering from '@/src/components/Shared/ConditionalBreakpointRendering'
 import { ReactNode } from 'react'
 
 export interface SideBarProps {
@@ -17,8 +18,14 @@ export interface SideBarProps {
 export default async function SideBar({ children, initialStoreProps }: { initialStoreProps?: SidebarStoreProviderProps['initialStoreProps']; children: ReactNode }) {
   return (
     <SidebarStoreProvider initialStoreProps={initialStoreProps}>
-      <DesktopSidebar children={children} className='hidden md:flex' />
-      <MobileSideBar children={children} visibilityBreakpoints='flex md:hidden opacity-100 md:opacity-0' />
+      <ConditionalBreakpointRendering hideBreakPoint='768px' logIdentifier='mobile-sidebar'>
+        <MobileSideBar children={children} />
+      </ConditionalBreakpointRendering>
+
+      <ConditionalBreakpointRendering showBreakPoint='768px' logIdentifier='desktop-sidebar'>
+        <DesktopSidebar className='flex' children={children} />
+      </ConditionalBreakpointRendering>
+
       <ActiveDelimiter />
     </SidebarStoreProvider>
   )
