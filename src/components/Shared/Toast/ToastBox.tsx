bad4196acae41f750d0c7846fe520d1820f10f8b
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-01 11:40:55 +0200

    ref: removed unused codeblocks and type definitions

--- src/components/Shared/Toast/ToastBox.tsx
+++ src/components/Shared/Toast/ToastBox.tsx
@@ -1,31 +1,7 @@
 'use client'
 import '@/components/Shared/Toast/ToastStyles.css'
-import { ToastContainer, ToastContainerProps, toast } from 'react-toastify'
+import { ToastContainer, ToastContainerProps } from 'react-toastify'
 
 export default function ToastBox(config?: ToastContainerProps) {
   return <ToastContainer toastClassName='md:ml-12' autoClose={5000} closeOnClick limit={5} pauseOnHover={false} pauseOnFocusLoss={true} theme={'colored'} position={'bottom-center'} {...config} />
-}
-
-export type ContainerProps = {
-  className?: string
-  autoClose?: number
-  hideProgressBar?: boolean
-  pauseOnHover?: boolean
-  theme?: 'colored' | 'dark' | 'light'
-  position?: 'top-right' | 'top-center' | 'top-left' | 'bottom-right' | 'bottom-center' | 'bottom-left'
-  pauseOnFocusLoss?: boolean
-}
-
-export type ToastProps = {
-  type?: 'info' | 'success' | 'warning' | 'error'
-  autoClose?: number
-  isClosable?: boolean
-  hideProgressBar?: boolean
-
-  pauseOnHover?: boolean
-  theme?: 'colored' | 'dark' | 'light'
-}
-
-export function createToast(message: string, options?: ToastProps) {
-  return toast(message, options)
 }
