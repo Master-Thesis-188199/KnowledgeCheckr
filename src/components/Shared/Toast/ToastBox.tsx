7ff5217d4c4501a6cc050e70da35acc34a7f0a7f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-01 11:40:08 +0200

    ref: updated default toast settings in ToastBox
    Changed the auto-close delay from 3 seconds to 5 seconds and added a limit 5 toasts so that the screen is not cluttered with toast messages

--- src/components/Shared/Toast/ToastBox.tsx
+++ src/components/Shared/Toast/ToastBox.tsx
@@ -1,9 +1,9 @@
 'use client'
 import '@/components/Shared/Toast/ToastStyles.css'
-import { ToastContainer, toast } from 'react-toastify'
+import { ToastContainer, ToastContainerProps, toast } from 'react-toastify'
 
-export default function ToastBox(config?: ContainerProps) {
-  return <ToastContainer toastClassName='md:ml-12' autoClose={3000} closeOnClick pauseOnHover pauseOnFocusLoss={false} theme={'colored'} position={'bottom-center'} {...config} />
+export default function ToastBox(config?: ToastContainerProps) {
+  return <ToastContainer toastClassName='md:ml-12' autoClose={5000} closeOnClick limit={5} pauseOnHover={false} pauseOnFocusLoss={true} theme={'colored'} position={'bottom-center'} {...config} />
 }
 
 export type ContainerProps = {
