bc24490c60ea83e913da3f2fabdfc98dda75e058
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-07 08:45:18 +0200

    ref: updated SaveAction to either save or update check
    This way this the save-action can be reused to save any kind of check. In other words, it can be used to both save a new check and update an existing one.

--- src/app/checks/create/SaveAction.ts
+++ src/app/checks/create/SaveAction.ts
@@ -1,10 +1,40 @@
 'use server'
 
 import insertKnowledgeCheck from '@/database/knowledgeCheck/insert'
+import { getKnowledgeCheckById } from '@/database/knowledgeCheck/select'
+import { updateKnowledgeCheck } from '@/database/knowledgeCheck/update'
+import { getServerSession } from '@/src/lib/auth/server'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
-import { redirect } from 'next/navigation'
+import _ from 'lodash'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+import { redirect, unauthorized } from 'next/navigation'
 
 export async function saveAction({ user_id, check }: { user_id: string; check: KnowledgeCheck }) {
-  await insertKnowledgeCheck(user_id, check)
-  redirect('/checks')
+  const { user } = await getServerSession()
+  if (!user) unauthorized()
+
+  try {
+    const exists = await getKnowledgeCheckById(check.id)
+    if (exists) {
+      if (!_.isEqual(exists, check)) {
+        console.log(
+          'Updating existing knowledge check -> changes',
+          _.differenceWith(_.toPairs(exists), _.toPairs(check), _.isEqual).map(([key, value]) => ({ key, value })),
+        )
+        updateKnowledgeCheck(user.id, check)
+      } else {
+        console.log('Knowledge check is unchanged, skipping update')
+      }
+    } else {
+      console.log('Inserting new knowledge check', check)
+      await insertKnowledgeCheck(user_id, check)
+    }
+
+    redirect('/checks')
+  } catch (err) {
+    if (isRedirectError(err)) {
+      throw err
+    }
+    console.error('Error saving knowledge check:', err)
+  }
 }
