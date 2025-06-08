6617c4eecc9d8c5411b639a4116405e272a11281
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-08 08:00:59 +0200

    ref: updated addQuestion to update question if exists
    The reason for this change is that when the addQuestion action is called with a question that is already part of the store based on its unique id, then it would add another question that even though it was meant to update the existing one.

--- src/hooks/checks/create/CreateCheckStore.ts
+++ src/hooks/checks/create/CreateCheckStore.ts
@@ -1,5 +1,6 @@
 import { KnowledgeCheck } from '@/schemas/KnowledgeCheck'
 import { Question } from '@/schemas/QuestionSchema'
+import _ from 'lodash'
 import { v4 as uuid } from 'uuid'
 import { createStore } from 'zustand/vanilla'
 
@@ -34,26 +35,8 @@ const defaultInitState: CreateCheckState = {
 }
 
 export const createCheckCreateStore = (initialState: CreateCheckState = defaultInitState) => {
-  return createStore<CreateCheckStore>()((set) => ({
-    ...initialState,
-    setName: (name) => set((prev) => ({ ...prev, name })),
-    setDescription: (description) => set((prev) => ({ ...prev, description })),
-    addQuestion: (question: Question) =>
-      set((prev) => {
-        const { questionCategories } = prev
-
-        // Add new category if not part of check-categories
-        if (!questionCategories.find((category) => category.name === question.category)) {
-          questionCategories.push({
-            id: uuid(),
-            name: question.category,
-            skipOnMissingPrequisite: false,
-          })
-        }
-
-        return { questions: [...prev.questions, question], questionCategories }
-      }),
-    removeQuestion: (questionId) =>
+  return createStore<CreateCheckStore>()((set) => {
+    const removeQuestion: CreateCheckActions['removeQuestion'] = (questionId) =>
       set((prev) => {
         const toRemoveQuestion = prev.questions.find((question) => question.id !== questionId)
 
@@ -64,6 +47,38 @@ export const createCheckCreateStore = (initialState: CreateCheckState = defaultI
           questions: prev.questions.filter((question) => question.id !== questionId),
           questionCategories: isCategoryUsed ? prev.questionCategories : prev.questionCategories.filter((cat) => cat.name !== category),
         }
-      }),
-  }))
+      })
+
+    return {
+      ...initialState,
+      setName: (name) => set((prev) => ({ ...prev, name })),
+      setDescription: (description) => set((prev) => ({ ...prev, description })),
+      addQuestion: (question: Question) =>
+        set((prev) => {
+          const { questionCategories } = prev
+
+          // Check if question already exists and has changed
+          const exists = prev.questions.find((q) => q.id === question.id)
+          if (exists && !_.isEqual(exists, question)) {
+            console.log('Removing existing question to update it...')
+            removeQuestion(question.id)
+          } else if (exists && _.isEqual(exists, question)) {
+            console.log('Question already exists and is unchanged, skipping...')
+            return prev // No changes needed
+          }
+
+          // Add new category if not part of check-categories
+          if (!questionCategories.find((category) => category.name === question.category)) {
+            questionCategories.push({
+              id: uuid(),
+              name: question.category,
+              skipOnMissingPrequisite: false,
+            })
+          }
+
+          return { questions: [...prev.questions.filter((q) => question.id !== q.id), question], questionCategories }
+        }),
+      removeQuestion,
+    }
+  })
 }
