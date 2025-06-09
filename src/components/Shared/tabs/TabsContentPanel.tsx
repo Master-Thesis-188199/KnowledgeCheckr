74f71e2b75fe51372d57ece6354204572ffb45a0
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-09 08:56:55 +0200

    feat: created TabsContentPanel component
    This component uses the TabsContext to decide whether or not to render its children. Thus, this component conditionally renders its children, hence a given tab based on the given tab-name.

---
+++ src/components/Shared/tabs/TabsContentPanel.tsx
@@ -0,0 +1,16 @@
+import { Tab, useTabsContext } from '@/src/components/Shared/tabs/TabsProvider'
+import { cn } from '@/src/lib/Shared/utils'
+
+export function TabsContentPanel({ children, tab, className }: { children: React.ReactNode; tab: Tab['name']; className?: string }) {
+  const { currentTab, tabs } = useTabsContext()
+
+  if (!tabs.some((t) => t.name.toLocaleLowerCase().includes(tab.toLowerCase()))) {
+    throw new Error(`Tab with name "${tab}" does not exist in the current TabsProvider context.`)
+  }
+
+  return (
+    <div data-current={currentTab.toLocaleLowerCase().includes(tab.toLowerCase())} className={cn(className, 'data-[current=false]:hidden')}>
+      {children}
+    </div>
+  )
+}
