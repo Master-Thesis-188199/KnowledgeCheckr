6fed6adf80979b1e62d5b7e63169d251656cbdae
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-17 16:52:26 +0200

    ref: replaced typo toLocaleLowerCase with toLowerCase

--- src/components/Shared/tabs/TabsContentPanel.tsx
+++ src/components/Shared/tabs/TabsContentPanel.tsx
@@ -4,12 +4,12 @@ import { cn } from '@/src/lib/Shared/utils'
 export function TabsContentPanel({ children, tab, className }: { children: React.ReactNode; tab: Tab['name']; className?: string }) {
   const { currentTab, tabs } = useTabsContext()
 
-  if (!tabs.some((t) => t.name.toLocaleLowerCase().includes(tab.toLowerCase()))) {
+  if (!tabs.some((t) => t.name.toLowerCase().includes(tab.toLowerCase()))) {
     throw new Error(`Tab with name "${tab}" does not exist in the current TabsProvider context.`)
   }
 
   return (
-    <div data-current={currentTab.toLocaleLowerCase().includes(tab.toLowerCase())} className={cn(className, 'data-[current=false]:hidden')}>
+    <div data-current={currentTab.toLowerCase().includes(tab.toLowerCase())} className={cn(className, 'data-[current=false]:hidden')}>
       {children}
     </div>
   )
