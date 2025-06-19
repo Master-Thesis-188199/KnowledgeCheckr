8b499b4d86cbd5fd35fb361fd1ab30c759abe78c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-19 07:55:06 +0200

    test: verifies that mobile-sidebar closes on navigate

---
+++ src/tests/e2e/sidebar/MobileSidebar.cy.tsx
@@ -0,0 +1,15 @@
+describe('Mobile-Sidebar TestSuite: ', () => {
+  it('Verify that the sidebar is closed when item is clicked (onNavigate)', () => {
+    cy.viewport('iphone-x')
+    cy.visit('/')
+
+    cy.get("button[aria-label='open mobile sidebar']").should('be.visible').click()
+
+    cy.wait(500)
+    cy.get('#mobile-sidebar-dialog').should('be.visible')
+
+    cy.get('#mobile-sidebar-dialog * a[aria-label="sidebar item"]').should('be.visible').filter(':not(a[href="/"])').filter(":not(a[href*='login'])").first().click()
+    cy.wait(500)
+    cy.get('#mobile-sidebar-dialog').should('not.be.visible')
+  })
+})
