0f3de55cef5581fe5151b6fa5947bc65dd223907
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-19 08:14:21 +0200

    test: verifies that mobile-sidebar can be open/closed

--- src/tests/e2e/sidebar/MobileSidebar.cy.tsx
+++ src/tests/e2e/sidebar/MobileSidebar.cy.tsx
@@ -1,4 +1,32 @@
 describe('Mobile-Sidebar TestSuite: ', () => {
+  it('Verify that the mobile sidebar can be opened and closed via buttons', () => {
+    cy.viewport('iphone-x')
+    cy.visit('/')
+
+    cy.get("button[aria-label='open mobile sidebar']").should('be.visible').click()
+
+    cy.wait(500)
+    cy.get('#mobile-sidebar-dialog').should('be.visible')
+
+    cy.get('#mobile-sidebar-dialog * button[aria-label="close mobile sidebar"]').should('be.visible').click()
+    cy.wait(500)
+    cy.get('#mobile-sidebar-dialog').should('not.be.visible')
+  })
+
+  it('Verify that mobile sidebar can be closed by clicking on blurred overlay', () => {
+    cy.viewport('iphone-x')
+    cy.visit('/')
+
+    cy.get("button[aria-label='open mobile sidebar']").should('be.visible').click()
+
+    cy.wait(500)
+    cy.get('#mobile-sidebar-dialog').should('be.visible')
+
+    cy.get('#mobile-sidebar-dialog').parent().children().filter(':not(#mobile-sidebar-dialog)').click()
+    cy.wait(500)
+    cy.get('#mobile-sidebar-dialog').should('not.be.visible')
+  })
+
   it('Verify that the sidebar is closed when item is clicked (onNavigate)', () => {
     cy.viewport('iphone-x')
     cy.visit('/')
