b8ec80be4d386565bbd4f392938f2fb4aab1c8a8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-19 07:54:40 +0200

    ref: added identifiers to mobile-sidebar elements
    These descriptors / identifiers are used to identify these elements within test-cases more easily.

--- src/components/root/Navigation/elements/RenderSideBarItems.tsx
+++ src/components/root/Navigation/elements/RenderSideBarItems.tsx
@@ -24,6 +24,7 @@ export default function RenderSideBarItems() {
 export function SidebarElement({ children, icon, className, href }: { children: React.ReactNode; className?: string; href: string; icon: React.ReactNode }) {
   return (
     <CloseMobileSidebarLink
+      aria-label='sidebar item'
       href={href}
       className={twMerge('group/sidebar flex items-center justify-start gap-4 rounded-md py-2 hover:cursor-pointer hover:bg-neutral-200/75 hover:font-semibold dark:hover:bg-neutral-700', className)}>
       {icon}
