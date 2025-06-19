17ada9d218b46747cfa7876e5922e76173e6d413
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-19 17:01:54 +0200

    ref: simplified onChange event handler in TabSelect

--- src/components/Shared/tabs/TabSelect.tsx
+++ src/components/Shared/tabs/TabSelect.tsx
@@ -10,10 +10,7 @@ export function TabSelect({ ...props }: HTMLProps<HTMLSelectElement>) {
     <select
       name='tabs'
       className='flex w-full rounded-md p-2 ring-1 ring-neutral-400 ring-offset-3 ring-offset-neutral-100 outline-none focus:ring-neutral-600 dark:ring-neutral-500 dark:ring-offset-neutral-800 dark:focus:ring-neutral-400'
-      onChange={(e) => {
-        console.log(`selected tab: ${e.currentTarget.value}`)
-        return setCurrentTab(tabs.find((tab) => tab.name === e.currentTarget.value)?.name ?? tabs[0].name)
-      }}
+      onChange={(e) => setCurrentTab(tabs.find((tab) => tab.name === e.currentTarget.value)?.name ?? tabs[0].name)}
       defaultValue={tabs.find((tab) => currentTab === tab.name)?.name}
       {...props}>
       {tabs.map((tab) => (
