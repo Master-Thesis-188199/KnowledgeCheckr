4c6c728473004cbf5e2310da00782731724dcd3d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-28 12:12:20 +0200

    ref: drafted practice action button next to share-button
    This is how the card component could look like by moving the share and practice to the top right instead of animating their appearance at the bottom of the card.

--- src/components/check/KnowledgeCheckCard.tsx
+++ src/components/check/KnowledgeCheckCard.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { ShareKnowledgeCheckButton } from '@/src/components/check/ShareKnowledgeCheckButton'
-import { Button } from '@/src/components/shadcn/button'
 import { InitialsIcon } from '@/src/components/Shared/InitialsIcon'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { Tooltip } from '@heroui/tooltip'
 import { motion, Variants } from 'framer-motion'
+import { InfoIcon, PlayIcon } from 'lucide-react'
 
 // Parent needs empty states just to trigger its children
 const cardVariants = {
@@ -38,7 +39,26 @@ export function KnowledgeCheckCard(check: KnowledgeCheck) {
       initial='rest'
       whileHover='hover'
       animate='rest'>
-      <ShareKnowledgeCheckButton check={check} className='absolute top-3 right-4' />
+      <div className='absolute top-3 right-4 flex gap-1'>
+        <ShareKnowledgeCheckButton check={check} />
+
+        <Tooltip
+          content={
+            <div className='flex items-center gap-1.5'>
+              <InfoIcon className='size-4' />
+              Practice Knowledge
+            </div>
+          }
+          delay={250}
+          offset={8}
+          closeDelay={0}
+          shouldFlip
+          className='rounded-md bg-neutral-100 p-2 text-sm shadow-sm shadow-neutral-400 dark:bg-neutral-800 dark:text-neutral-300 dark:shadow-neutral-700'>
+          <button className='group/play p-1.5'>
+            <PlayIcon className='size-4.5 group-hover/play:animate-pulse group-hover/play:cursor-pointer dark:text-neutral-400 dark:group-hover/play:text-green-500/90' />
+          </button>
+        </Tooltip>
+      </div>
       <div className='flex flex-col items-center gap-1 px-4'>
         <InitialsIcon size={64} name={check.name} className='mx-auto mt-4 mb-2 size-auto' />
         <h2 className='text-center text-xl font-semibold dark:text-neutral-300'>{check.name}</h2>
@@ -61,14 +81,14 @@ export function KnowledgeCheckCard(check: KnowledgeCheck) {
         </div>
       </div>
 
-      <motion.div className='absolute inset-x-0 bottom-0 z-10 flex justify-center gap-12 rounded-md border-0 bg-white/90 py-3 ring-0 outline-none dark:bg-neutral-900' variants={actionVariants}>
+      {/* <motion.div className='absolute inset-x-0 bottom-0 z-10 flex justify-center gap-12 rounded-md border-0 bg-white/90 py-3 ring-0 outline-none dark:bg-neutral-900' variants={actionVariants}>
         <Button variant='primary' disabled>
           Exercise
         </Button>
         <Button variant='outline' disabled>
           Edit
         </Button>
-      </motion.div>
+      </motion.div> */}
     </motion.a>
   )
 }
