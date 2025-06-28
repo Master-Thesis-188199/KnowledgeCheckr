5ec07f1e0fd2dcbfb5607775c1a6770f841c3c42
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-28 11:25:56 +0200

    feat: created ShareKnowledgeCheckButton component
    This component renders a button that will copy the share-token for a given KnowledgeCheck to the clipboard.

---
+++ src/components/check/ShareKnowledgeCheckButton.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { saveGeneratedShareToken } from '@/src/lib/Shared/generateToken'
+import { cn } from '@/src/lib/Shared/utils'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { Share2Icon } from 'lucide-react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+
+export function ShareKnowledgeCheckButton({ check, className }: { check: KnowledgeCheck; className?: string }) {
+  const [shareToken, setShareToken] = useState(check.share_key)
+
+  return (
+    <button
+      aria-label='share KnowledgeCheck'
+      onClick={(e) => {
+        e.preventDefault()
+
+        if (shareToken) {
+          navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_BASE_URL}/checks/${shareToken}`)
+          toast('Successfully saved token to your clipboard.', { type: 'success' })
+          return
+        }
+
+        saveGeneratedShareToken(check.id)
+          .then((token) => {
+            navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_BASE_URL}/checks/${token}`)
+            toast('Successfully saved token to your clipboard.', { type: 'success' })
+            setShareToken(token)
+          })
+          .catch((e) => toast('Failed to save generate and save share-token', { type: 'error' }))
+      }}
+      className={cn('group rounded-md p-1.5 hover:cursor-pointer hover:ring-1 dark:text-neutral-400 dark:ring-neutral-500', className)}>
+      <Share2Icon className='size-4.5 group-active:stroke-3' />
+    </button>
+  )
+}
