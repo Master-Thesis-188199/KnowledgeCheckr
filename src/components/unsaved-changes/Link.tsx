73bbbbe05d83e006314643514ef92723b88e62ab
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-06-29 08:51:10 +0200

    feat: added Link component to support Nav.Abortion
    With the help of this Link component the navigation can be aborted when e.g. there are unsaved changes.

---
+++ src/components/unsaved-changes/Link.tsx
@@ -0,0 +1,35 @@
+import { useNavigationAbort } from '@/src/components/unsaved-changes/NavigationAbortProvider'
+import NextLink, { LinkProps } from 'next/link'
+import { useRouter } from 'next/navigation'
+import React, { useCallback, type MouseEvent } from 'react'
+
+/**
+ * This component wraps the next/Link component and allows navigation-abortion by checking whether the navigationAbortion is enabled.
+ */
+export default function Link({ onClick, href, children, ...nextLinkProps }: LinkProps & { children?: React.ReactNode }) {
+  const nextRouter = useRouter()
+  const { isNavigationAbortSet, showNavigationAbortModal } = useNavigationAbort()
+
+  const handleLinkClick = useCallback(
+    (e: MouseEvent<HTMLAnchorElement>) => {
+      e.preventDefault()
+
+      if (onClick) {
+        onClick(e)
+      }
+
+      if (isNavigationAbortSet) {
+        showNavigationAbortModal(href.toString())
+      } else {
+        nextRouter.push(href.toString())
+      }
+    },
+    [isNavigationAbortSet, href, nextRouter, onClick, showNavigationAbortModal],
+  )
+
+  return (
+    <NextLink href={href} onClick={handleLinkClick} {...nextLinkProps}>
+      {children}
+    </NextLink>
+  )
+}
