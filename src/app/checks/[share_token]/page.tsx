8cbf376deec843d9789e7ea1712077fcefb02d18
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-07-19 09:26:18 +0200

    ref: externalized question navigation menu

--- src/app/checks/[share_token]/page.tsx
+++ src/app/checks/[share_token]/page.tsx
@@ -1,7 +1,7 @@
 import DisplayQuestion from '@/components/check/DisplayQuestion'
 import { getKnowledgeCheckByShareToken } from '@/database/knowledgeCheck/select'
+import { QuestionNavigationMenu } from '@/src/components/checks/[share_token]/QuestionNavigationMenu'
 import PageHeading from '@/src/components/Shared/PageHeading'
-import { cn } from '@/src/lib/Shared/utils'
 import { notFound } from 'next/navigation'
 
 export default async function CheckPage({ params }: { params: Promise<{ share_token: string }> }) {
@@ -19,30 +19,7 @@ export default async function CheckPage({ params }: { params: Promise<{ share_to
 
       <div className='grid grid-cols-[1fr_auto] gap-12'>
         <div className='mx-auto max-h-fit w-full max-w-7xl'>{check.questions && <DisplayQuestion {...check.questions.at(0)!} />}</div>
-        <div className='flex max-h-fit min-h-24 min-w-72 flex-col justify-evenly gap-3 rounded-md p-4 ring-2 dark:ring-neutral-500'>
-          <span className='font-semibold dark:text-neutral-300'>Questions</span>
-          <div className='grid grid-cols-[repeat(auto-fill,30px)] gap-1.5'>
-            {check.questions.map((question, i) => (
-              <button
-                className={cn(
-                  'flex size-7 items-center justify-center rounded-lg p-1 text-sm ring-1 hover:cursor-pointer hover:ring-neutral-300/60 dark:ring-neutral-500 dark:hover:bg-neutral-600',
-                  i === 0 && 'hover:cursor-default dark:bg-neutral-600 dark:ring-neutral-300/60',
-                )}
-                key={`question-nav-${i}`}>
-                {i + 1}
-              </button>
-            ))}
-          </div>
-          <button className='ml-auto text-sm hover:cursor-pointer hover:underline dark:text-neutral-200/60'>Finish Check</button>
-        </div>
-      </div>
-
-      <div className='flex hidden flex-col gap-8'>
-        <div className='grid grid-cols-1 gap-8 @[1000px]:grid-cols-2 @[1400px]:grid-cols-3 @[1400px]:gap-14'>
-          {check.questions.map((question) => (
-            <DisplayQuestion key={question.id} {...question} />
-          ))}
-        </div>
+        <QuestionNavigationMenu />
       </div>
     </>
   )
---
+++ src/components/checks/[share_token]/QuestionNavigationMenu.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useExaminationStore } from '@/src/components/checks/[share_token]/ExaminationStoreProvider'
+import { cn } from '@/src/lib/Shared/utils'
+
+export function QuestionNavigationMenu() {
+  const { knowledgeCheck, setCurrentQuestionIndex, currentQuestionIndex } = useExaminationStore((store) => store)
+
+  return (
+    <>
+      <div className='flex max-h-fit min-h-24 min-w-72 flex-col justify-evenly gap-3 rounded-md p-4 ring-2 dark:ring-neutral-600'>
+        <span className='font-semibold dark:text-neutral-300'>Questions</span>
+        <div className='grid grid-cols-[repeat(auto-fill,30px)] gap-1.5'>
+          {knowledgeCheck.questions.map((_, i) => (
+            <button
+              className={cn(
+                'flex size-7 items-center justify-center rounded-lg p-1 text-sm ring-1 hover:cursor-pointer hover:ring-neutral-300/60 dark:ring-neutral-500 dark:hover:bg-neutral-600',
+                i === currentQuestionIndex && 'hover:cursor-default dark:bg-neutral-600 dark:ring-neutral-300/60',
+              )}
+              onClick={() => setCurrentQuestionIndex(i)}
+              key={`question-nav-${i}`}>
+              {i + 1}
+            </button>
+          ))}
+        </div>
+        <button className='ml-auto text-sm hover:cursor-pointer hover:underline dark:text-neutral-200/60'>Finish Check</button>
+      </div>
+    </>
+  )
+}
