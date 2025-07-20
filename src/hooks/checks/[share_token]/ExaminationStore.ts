dbc5d1d58cc0ccc49e6d2395c546a1a0b21392f3
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-07-20 06:42:01 +0200

    ref: added isLastQuestion to ExaminationStore
    This state-value will keep track of whether or not the currently displayed question (index) is the last one. This way buttons can be disabled without having to duplicate the respective conditions.

--- src/hooks/checks/[share_token]/ExaminationStore.ts
+++ src/hooks/checks/[share_token]/ExaminationStore.ts
@@ -4,6 +4,7 @@ import { createStore } from 'zustand/vanilla'
 export type ExaminationState = {
   knowledgeCheck: KnowledgeCheck
   currentQuestionIndex: number
+  isLastQuestion: boolean
 }
 
 export type ExaminationActions = {
@@ -17,18 +18,26 @@ export type ExaminationStore = ExaminationState & ExaminationActions
 const defaultInitState: ExaminationState = {
   currentQuestionIndex: 0,
   knowledgeCheck: instantiateKnowledgeCheck(),
+  isLastQuestion: false,
 }
 
 export const createExaminationStore = (initialState: ExaminationState = defaultInitState) => {
   return createStore<ExaminationStore>()((set) => {
     return {
       ...initialState,
-      setCurrentQuestionIndex: (index) => set((prev) => ({ ...prev, currentQuestionIndex: index })),
-      nextQuestion: () => set((prev) => ({ ...prev, currentQuestionIndex: (prev.currentQuestionIndex + 1) % prev.knowledgeCheck.questions.length })),
+      // isLastQuestion: set((prev) => ({ ...prev, isLastQuestion: prev.currentQuestionIndex + 1 === prev.knowledgeCheck.questions.length })),
+      setCurrentQuestionIndex: (index) => set((prev) => ({ ...prev, currentQuestionIndex: index, isLastQuestion: index === prev.knowledgeCheck.questions.length })),
+      nextQuestion: () =>
+        set((prev) => ({
+          ...prev,
+          currentQuestionIndex: (prev.currentQuestionIndex + 1) % prev.knowledgeCheck.questions.length,
+          isLastQuestion: prev.currentQuestionIndex + 1 + 1 === prev.knowledgeCheck.questions.length,
+        })),
       previousQuestion: () =>
         set((prev) => ({
           ...prev,
           currentQuestionIndex: prev.currentQuestionIndex === 0 ? prev.knowledgeCheck.questions.length - 1 : prev.currentQuestionIndex - 1,
+          isLastQuestion: (prev.currentQuestionIndex === 0 ? prev.knowledgeCheck.questions.length - 1 : prev.currentQuestionIndex - 1) + 1 === prev.knowledgeCheck.questions.length,
         })),
     }
   })
