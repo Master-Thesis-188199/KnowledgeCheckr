0a7dcec8213a3d91f4bbbdd8c6dbb6c0a0692ab3
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-07-20 06:04:11 +0200

    ref: added next and previous buttons to QuestionRenderer

--- src/components/checks/[share_token]/ExamQuestionRenderer.tsx
+++ src/components/checks/[share_token]/ExamQuestionRenderer.tsx
@@ -2,11 +2,12 @@
 
 import DisplayQuestion from '@/src/components/check/DisplayQuestion'
 import { useExaminationStore } from '@/src/components/checks/[share_token]/ExaminationStoreProvider'
+import { Button } from '@/src/components/shadcn/button'
 import { motion } from 'framer-motion'
 import { InfoIcon } from 'lucide-react'
 
 export function ExamQuestionRenderer() {
-  const { currentQuestionIndex, knowledgeCheck } = useExaminationStore((store) => store)
+  const { currentQuestionIndex, knowledgeCheck, nextQuestion, previousQuestion } = useExaminationStore((store) => store)
   if (knowledgeCheck.questions?.length === 0)
     return (
       <div className='mx-auto flex max-h-fit w-full max-w-7xl items-center justify-center gap-2 text-neutral-600 dark:text-neutral-300'>
@@ -23,8 +24,14 @@ export function ExamQuestionRenderer() {
         opacity: [0.75, 1, 1],
         transition: { duration: 0.15, ease: 'easeInOut' },
       }}
-      className='mx-auto max-h-fit w-full max-w-7xl'>
+      className='relative mx-auto max-h-fit w-full max-w-7xl'>
       {<DisplayQuestion {...knowledgeCheck.questions.at(currentQuestionIndex)!} />}
+      <div className='absolute right-0 -bottom-16 left-0 flex justify-between px-8'>
+        <Button variant='outline' onClick={previousQuestion}>
+          Previous
+        </Button>
+        <Button onClick={nextQuestion}>Next</Button>
+      </div>
     </motion.div>
   )
 }
