a5adfad8504e0f1a0937f25533b552ef6a93d8d9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-07-26 08:45:45 +0200

    feat: introduced Examination helper module
    This module exports a function prepareExaminationCheck which takes in a KnowledgeCheck and removes correctness information from its answers and randomizes their order.

---
+++ src/lib/checks/[share_token]/Examination.ts
@@ -0,0 +1,46 @@
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { ChoiceQuestion, DragDropQuestion, OpenQuestion, Question } from '@/src/schemas/QuestionSchema'
+
+/**
+ * This function takes in a given knowledgeCheck and removes each answer's correctness information and randomizes their order.
+ */
+export default function prepareExaminationCheck(check: KnowledgeCheck) {
+  return {
+    ...check,
+    questions: check.questions.map(hideCorrectness).map(randomizeAnswerOrder),
+  }
+}
+
+function randomizeAnswerOrder(question: Question): Question {
+  return question
+}
+
+function hideCorrectness(question: Question): Question {
+  if (question.type === 'single-choice' || question.type === 'multiple-choice') {
+    question = question as ChoiceQuestion
+
+    return {
+      ...question,
+      answers: question.answers.map((answer) => ({ ...answer, correct: false })),
+    }
+  } else if (question.type === 'open-question') {
+    question = question as OpenQuestion
+
+    return {
+      ...question,
+      expectation: '',
+    }
+  } else if (question.type === 'drag-drop') {
+    question = question as DragDropQuestion
+
+    return {
+      ...question,
+      answers: question.answers
+        .map((answer) => ({ answer: answer, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ answer }, i) => ({ ...answer, position: i })),
+    }
+  }
+
+  return question
+}
