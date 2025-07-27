3149d85a0910ab2a3b9a20ca5359bc87aa78d15e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-07-27 10:28:03 +0200

    feat: added finish attempt button onClick handler

--- src/components/checks/[share_token]/ExamFinishDialog.tsx
+++ src/components/checks/[share_token]/ExamFinishDialog.tsx
@@ -1,18 +1,18 @@
+'use client'
+
 import { useExaminationStore } from '@/src/components/checks/[share_token]/ExaminationStoreProvider'
+import { useNavigationAbort } from '@/src/components/navigation-abortion/NavigationAbortProvider'
 import { Button } from '@/src/components/shadcn/button'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/src/components/Shared/Dialog'
+import finishExaminationAttempt from '@/src/lib/checks/[share_token]/FinishExaminationAttempt'
 import { cn } from '@/src/lib/Shared/utils'
 import { DialogDescription } from '@radix-ui/react-dialog'
+import { redirect } from 'next/navigation'
+import { toast } from 'react-toastify'
 
 export default function ExamFinishDialog({ children, triggerClassname }: { children: React.ReactNode; triggerClassname?: string }) {
   const { knowledgeCheck } = useExaminationStore((state) => state)
-
-  /* //todo onSubmit:
-          - disable navigationAbort menu
-          - store examination results in database
-          - redirect user away from examination
-          - clear examination session-cache 
-  */
+  const { clearNavigationAbort } = useNavigationAbort()
 
   return (
     <Dialog>
@@ -37,7 +37,22 @@ export default function ExamFinishDialog({ children, triggerClassname }: { child
             <Button variant='outline' className='rounded-md px-4 py-1.5 ring-1 dark:ring-neutral-600'>
               Cancel
             </Button>
-            <Button className='rounded-md px-4 py-1.5 ring-[1.5px] dark:bg-black dark:ring-neutral-600'>Submit</Button>
+            <Button
+              onClick={() =>
+                finishExaminationAttempt(knowledgeCheck)
+                  .catch((e) => {
+                    toast(`Failed to submit examination results. ${e}`, { type: 'error' })
+                  })
+                  .then(() => {
+                    toast('Successfully submitted examination results', { type: 'success' })
+                    sessionStorage.removeItem('examination-store')
+                    clearNavigationAbort()
+                    redirect('/checks')
+                  })
+              }
+              className='rounded-md px-4 py-1.5 ring-[1.5px] dark:bg-black dark:ring-neutral-600'>
+              Submit
+            </Button>
           </div>
         </div>
       </DialogContent>
