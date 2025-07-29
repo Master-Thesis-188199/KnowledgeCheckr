5ddbd17b4ece6889a2f6b359b928dd1d7809170b
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-07-29 17:46:31 +0200

    ref: added type-safety to debounceFunction function

--- src/hooks/Shared/debounceFunction.ts
+++ src/hooks/Shared/debounceFunction.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Any } from '@/types'
 
-export default function debounceFunction(func: Any, delay: number) {
+export default function debounceFunction<T extends (...args: Any[]) => Any>(func: T, delay: number): T {
   let timer: NodeJS.Timeout
-  return function (...args: Any) {
+  return function (...args: Parameters<T>) {
     clearTimeout(timer)
-    //@ts-expect-error
+    // @ts-expect-error
     timer = setTimeout(() => func.apply(this, args), delay)
-  }
+  } as T
 }
