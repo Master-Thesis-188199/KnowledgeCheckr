09611729a7b332564b709cd3d5b5231737058539
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-08-03 18:02:59 +0200

    ref: applied ExamSchema type and util functions
    Replaced the previously used KnowledgeCheck type with the new ExaminationSchema type within exam-related functions.

--- database/examination/insert.ts
+++ database/examination/insert.ts
@@ -3,21 +3,21 @@
 import getDatabase from '@/database/Database'
 import requireAuthentication from '@/src/lib/auth/requireAuthentication'
 import { formatDatetime } from '@/src/lib/Shared/formatDatetime'
-import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { ExaminationSchema } from '@/src/schemas/ExaminationSchema'
 
-export default async function insertExaminationResults(results: KnowledgeCheck, score: number) {
+export default async function insertExaminationResults(examinationResult: ExaminationSchema) {
   const { user } = await requireAuthentication()
   const db = await getDatabase()
 
   try {
     await db.exec('INSERT INTO `User_has_done_KnowledgeCheck`(`user_id`, `knowledgeCheck_id`, `startedAt`, `finishedAt`, `score`, `results`) VALUES (?, ?, ?, ?, ?, ?)', [
       user.id,
-      results.id,
+      examinationResult.knowledgeCheck.id,
       // todo use actual start and end times
-      formatDatetime(new Date(Date.now())),
-      formatDatetime(new Date(Date.now() + 3600 * 1000)),
-      score,
-      JSON.stringify(results.questions),
+      formatDatetime(examinationResult.startedAt),
+      formatDatetime(examinationResult.finishedAt ?? new Date(Date.now())),
+      examinationResult.score,
+      JSON.stringify(examinationResult.results),
     ])
   } catch (e) {
     console.error('Failed to save examination results to database', e)
--- src/lib/checks/[share_token]/FinishExaminationAttempt.ts
+++ src/lib/checks/[share_token]/FinishExaminationAttempt.ts
@@ -1,11 +1,11 @@
 'use server'
 
 import insertExaminationResults from '@/database/examination/insert'
-import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { ExaminationSchema } from '@/src/schemas/ExaminationSchema'
 
-export default async function finishExaminationAttempt(result: KnowledgeCheck) {
+export default async function finishExaminationAttempt(result: ExaminationSchema) {
   // todo Determinate examination score
-  const score = result.questions.reduce((acc, q) => (acc += q.points), 0) * Math.random()
+  const score = result.knowledgeCheck.questions.reduce((acc, q) => (acc += q.points), 0) * Math.random()
 
-  await insertExaminationResults(result, score)
+  await insertExaminationResults({ ...result, score })
 }
