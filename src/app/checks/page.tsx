5ffc3f94964a28c7a11811f81155e5829e815082
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-08-31 11:50:10 +0200

    ref: drafted InfinityScroll fetching of new items

--- src/app/checks/page.tsx
+++ src/app/checks/page.tsx
@@ -1,7 +1,9 @@
 import { getKnowledgeChecksByOwner } from '@/database/knowledgeCheck/select'
 import { KnowledgeCheckCard } from '@/src/components/check/KnowledgeCheckCard'
+import { InfiniteScrollProvider, InfinityScrollFetcher, InfinityScrollRenderer } from '@/src/components/Shared/InfiniteScroll'
 import PageHeading from '@/src/components/Shared/PageHeading'
 import requireAuthentication from '@/src/lib/auth/requireAuthentication'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 import Link from 'next/link'
 
 export default async function ChecksPage() {
@@ -9,22 +11,33 @@ export default async function ChecksPage() {
   const checks = await getKnowledgeChecksByOwner(user.id)
 
   return (
-    <div>
-      <PageHeading title='Your Checks' />
-      {checks.length === 0 && (
-        <div>
-          No checks found. Create a new one{' '}
-          <Link href='/checks/create' className='text-blue-500 underline'>
-            here
-          </Link>
-          .
+    <InfiniteScrollProvider initialItems={checks}>
+      <div>
+        <PageHeading title='Your Checks' />
+        {checks.length === 0 && (
+          <div>
+            No checks found. Create a new one{' '}
+            <Link href='/checks/create' className='text-blue-500 underline'>
+              here
+            </Link>
+            .
+          </div>
+        )}
+        <div className='checks grid grid-cols-[repeat(auto-fill,minmax(380px,1fr))] gap-8'>
+          {/* <InfinityScrollRenderer<KnowledgeCheck> render={(check, i) => <KnowledgeCheckCard key={i} {...check} />} /> */}
+          <InfinityScrollRenderer<KnowledgeCheck> render={RenderCheck} component={KnowledgeCheckCard} />
+
+          {/* {checks.map((check, i) => (
+            <KnowledgeCheckCard key={i} {...check} />
+          ))} */}
         </div>
-      )}
-      <div className='checks grid grid-cols-[repeat(auto-fill,minmax(380px,1fr))] gap-8'>
-        {checks.map((check, i) => (
-          <KnowledgeCheckCard key={i} {...check} />
-        ))}
+        <InfinityScrollFetcher user_id={user.id} />
       </div>
-    </div>
+    </InfiniteScrollProvider>
   )
+}
+
+async function RenderCheck(check: KnowledgeCheck, i: number, a: KnowledgeCheck[]) {
+  'use server'
+  return <KnowledgeCheckCard key={i} {...check} />
 }
--- src/components/Shared/InfiniteScroll.tsx
+++ src/components/Shared/InfiniteScroll.tsx
@@ -1,6 +1,11 @@
 'use client'
+import { getKnowledgeChecksByOwner } from '@/database/knowledgeCheck/select'
+import { KnowledgeCheckCard } from '@/src/components/check/KnowledgeCheckCard'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 import { Any } from '@/types'
-import { createContext, useContext, useState } from 'react'
+import { useInView } from 'framer-motion'
+import { createContext, useContext, useEffect, useRef, useState } from 'react'
+import { JSX } from 'react/jsx-runtime'
 
 interface InfiniteScrollContext<T = Any> {
   items: T[]
@@ -26,4 +31,31 @@ export function useInfiniteScrollContext<TElement>() {
   if (!context) throw new Error('useInfiniteScrollContext must be used within a InfiniteScrollProvider')
 
   return context as InfiniteScrollContext<TElement>
+}
+
+export function InfinityScrollFetcher({ user_id }: { user_id: string }) {
+  const { addItems, items } = useInfiniteScrollContext()
+  const ref = useRef(null)
+  const inView = useInView(ref)
+
+  useEffect(() => {
+    if (!ref.current) return
+    if (!inView) return
+
+    console.log('Infinite Scroll - fetching new items...')
+    getKnowledgeChecksByOwner(user_id, { offset: items.length })
+      .then((checks) => {
+        console.log(`Fetched ... ${checks.length} new checks..`)
+        return checks
+      })
+      .then(addItems)
+  }, [ref.current, inView])
+
+  return <div ref={ref}>Loading...</div>
+}
+
+export function InfinityScrollRenderer<TItem extends KnowledgeCheck>({ render }: { render: (item: TItem, index: number, array: TItem[]) => Promise<JSX.Element> }) {
+  const { items } = useInfiniteScrollContext<TItem>()
+
+  return items.map((check, i) => <KnowledgeCheckCard key={i} {...check} />)
 }
