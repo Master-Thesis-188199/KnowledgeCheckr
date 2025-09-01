d95f4a9214a0c82a6d04b6ddffc962c12ef1d29c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-01 17:07:55 +0200

    ref: updated unauthorized login link path
    The reason for this change is that the login page was originally rendered at the /account path but was then moved to /login. While unauthorized users are internally redirected to the /login route anyway, the referrer changes through this additional redirect.

--- src/app/unauthorized.tsx
+++ src/app/unauthorized.tsx
@@ -7,7 +7,7 @@ export default async function Unauthorized() {
         <div className='flex h-12 items-center justify-center border-r-[0.5px] border-black pr-4 text-xl font-semibold dark:border-gray-500'>401</div>
         <h1>You&apos;re not authorized to access this page.</h1>
       </div>
-      <Link className='mt-8 rounded-md px-3 py-1.5 ring-1 dark:bg-neutral-800 dark:ring-neutral-600 dark:hover:bg-neutral-700' href={'/account?type=signin'}>
+      <Link className='mt-8 rounded-md px-3 py-1.5 ring-1 dark:bg-neutral-800 dark:ring-neutral-600 dark:hover:bg-neutral-700' href={'/account/login?type=signin'}>
         Login
       </Link>
     </div>
