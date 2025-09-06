ca957dcc388dc1468e3a19fcf77ef2e471111f9f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-06 20:28:00 +0200

    ref: updated CreateStoreType definition
    The reason for this change is that the `createStore` function from the zustand library does not return the State but rather the Store, thus state & actions. For that reason, the return type of the CreateStoreType was adjusted accordingly.

--- types/Shared/CreateStoreType.ts
+++ types/Shared/CreateStoreType.ts
@@ -1,10 +1,11 @@
+import { Any } from '@/types'
 import type { StoreApi } from 'zustand/vanilla'
 
 /**
  * Defines how a `createStore` function should look like.
  * It takes an optional initial state and options, and returns a Zustand store API.
  */
-export type CreateStoreType<Store extends object> = (initialState?: Store, options?: CreateStoreOptions) => StoreApi<Store>
+export type CreateStoreType<State extends object, Store extends object = Any> = (initialState?: State, options?: CreateStoreOptions) => StoreApi<Store>
 
 /**
  * Defines what options should be available when creating a store.
