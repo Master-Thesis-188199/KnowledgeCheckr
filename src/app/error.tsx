c7e854eda2a37290ab72352a2cffd795479b9410
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-06 08:30:40 +0200

    feat: drafted root error boundary to catch errors
    This way users are not shown the default "Application Error" screen when an internal error occurs, like loosing the database connection.

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+'use client' // Error boundaries must be Client Components
+
+import Card from '@/src/components/Shared/Card'
+import { cn } from '@/src/lib/Shared/utils'
+import { useEffect } from 'react'
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  const isDevelelopment = process.env.NODE_ENV === 'development'
+  const isProduction = process.env.NODE_ENV === 'production'
+
+  useEffect(() => {
+    if (isProduction) return
+
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className='mb-10 flex h-full items-center justify-center'>
+      <Card disableHoverStyles className={cn('flex max-w-xl flex-col gap-4', isDevelelopment && 'max-w-7xl')}>
+        <h2 className='border-b border-neutral-600 pb-2 text-lg font-semibold text-red-400'>{error.name} </h2>
+        <div className={cn('px-4 text-red-400/90', isProduction && 'hidden')}>
+          <p>{error.message}</p>
+          <p className='mt-4'>{error.stack}</p>
+        </div>
+        <div className={cn('px-4 text-red-400/90', isDevelelopment && 'hidden')}>
+          <p>Unfortunately, an internal issue caused your request to fail. Retry your previous action after a few minutes. In case the issue persists please contact the team.</p>
+        </div>
+      </Card>
+    </div>
+  )
+}
