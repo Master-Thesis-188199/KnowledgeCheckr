a5be162e23b2a2c9acc77357a40338a4ee240468
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-07 14:20:23 +0200

    test: verified /edit/[id] page functionality

---
+++ src/tests/e2e/checks/edit/[id]/EditPage.cy.tsx
@@ -0,0 +1,27 @@
+describe('Edit KnowledgeCheck page: ', () => {
+  it('Verify that users must be logged in to access the edit page', () => {
+    cy.visit('/checks/edit/12931293')
+    cy.get('main').should('contain', "You're not authorized to access this page")
+  })
+
+  it('Verify that users can access the edit page when logged in', () => {
+    cy.loginTestUser()
+    cy.visit('/checks/edit/1239')
+
+    cy.get('main').should('contain', 'This page could not be found')
+  })
+
+  it('Verify that users can edit an existing knowledge check that they own', () => {
+    cy.loginTestUser()
+
+    cy.visit('/checks/create')
+    cy.get('input[name="check-name"]').type('Test Check Title')
+    cy.get('button').contains('Save').should('exist').scrollIntoView().should('be.visible').click()
+
+    //? Ensure that the check was created and the user is redirected
+    cy.wait(750)
+    cy.visit('/checks')
+
+    cy.get("main * a[href^='/checks/edit/']").first().click()
+  })
+})
