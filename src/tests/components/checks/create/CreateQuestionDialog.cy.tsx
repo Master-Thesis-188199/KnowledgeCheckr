625e9462d2b322e570f37c328e34c1ebf020ddf9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-07 11:46:12 +0200

    test: verified that users can switch between question types

--- src/tests/components/checks/create/CreateQuestionDialog.cy.tsx
+++ src/tests/components/checks/create/CreateQuestionDialog.cy.tsx
@@ -70,4 +70,39 @@ describe('<CreateQuestionDialog />', () => {
     cy.get('[data-slot="dialog-overlay"]').click({ force: true })
     cy.get('#question-dialog').should('not.exist')
   })
+
+  it('Verify that users can switch between question types in the create-question-dialog', () => {
+    cy.viewport(800, 980)
+    cy.mount(
+      <RootProviders>
+        <CreateCheckStoreProvider>
+          <CreateQuestionDialog>
+            <div className='trigger'>Trigger</div>
+          </CreateQuestionDialog>
+        </CreateCheckStoreProvider>
+      </RootProviders>,
+    )
+
+    cy.get('#question-dialog').should('not.exist')
+    cy.get('.trigger').click()
+    cy.get('#question-dialog').should('be.visible')
+
+    cy.get('#question-dialog input[name="question"]').type('Example Question')
+
+    cy.get('#question-dialog [aria-label="popover-trigger-type"]').click()
+
+    cy.get("[aria-label='popover-content-type'] * div[data-slot='command-item']")
+      .its('length')
+      .then((options: number) => {
+        //? Pre-select the last option to ensure that all options are selectable
+        cy.get('#question-dialog [aria-label="popover-trigger-type"]').click()
+        cy.get("[aria-label='popover-content-type']").should('be.visible').get("[aria-label='popover-content-type'] * div[data-slot='command-item']").last().click()
+
+        for (let i = 0; i < options; i++) {
+          cy.get('#question-dialog [aria-label="popover-trigger-type"]').click()
+          cy.get("[aria-label='popover-content-type']").should('be.visible').get("[aria-label='popover-content-type'] * div[data-slot='command-item']").eq(i).click()
+          cy.wait(200)
+        }
+      })
+  })
 })
