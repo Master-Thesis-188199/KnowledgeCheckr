8d8c6fec52e089f6a92565b35473f9bcad907284
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-07 09:59:29 +0200

    test: verified that debouncing a function works

---
+++ src/tests/components/debounce/Debouncing.cy.tsx
@@ -0,0 +1,18 @@
+import debounceFunction from '@/src/hooks/Shared/debounceFunction'
+
+const DebounceTime = 300
+
+describe('Debouncing Suite: ', () => {
+  it('Verify that debouncing a function delays its excecution and prevents rapid calls', () => {
+    const saveFunction = cy.stub().as('saveFunction')
+    const debouncedSave = debounceFunction(saveFunction, DebounceTime)
+
+    // Simulate rapid calls to the debounced function
+    for (let i = 0; i < 5; i++) {
+      debouncedSave(`Call ${i + 1}`)
+    }
+    cy.get('@saveFunction').should('not.have.been.called')
+    cy.wait(DebounceTime + 100)
+    cy.get('@saveFunction').should('have.been.calledOnceWith', 'Call 5')
+  })
+})
