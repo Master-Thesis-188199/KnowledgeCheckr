7ea76dd7953833ed8c41930240e977cf79e1d3ed
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-07 10:15:57 +0200

    test: verified that create-quest-dialog resets on Escape

--- src/tests/components/checks/create/CreateQuestionDialog.cy.tsx
+++ src/tests/components/checks/create/CreateQuestionDialog.cy.tsx
@@ -22,4 +22,30 @@ describe('<CreateQuestionDialog />', () => {
     cy.get('#question-dialog button[aria-label="close"]').click()
     cy.get('#question-dialog').should('not.exist')
   })
+
+  it('Verify that create-question-dialog resets all inuts when the user presses Escape', () => {
+    cy.viewport(800, 980)
+    cy.mount(
+      <RootProviders>
+        <CreateCheckStoreProvider>
+          <CreateQuestionDialog>
+            <div className='trigger'>Trigger</div>
+          </CreateQuestionDialog>
+        </CreateCheckStoreProvider>
+      </RootProviders>,
+    )
+
+    cy.get('#question-dialog').should('not.exist')
+    cy.get('.trigger').click()
+    cy.get('#question-dialog').should('be.visible')
+
+    cy.get('#question-dialog input[name="question"]').type('Example Question')
+
+    cy.get('#question-dialog ').realPress('Escape')
+    cy.get('#question-dialog').should('not.exist')
+
+    cy.get('.trigger').click()
+    cy.get('#question-dialog').should('be.visible')
+    cy.get('#question-dialog input[name="question"]').should('have.value', '')
+  })
 })
