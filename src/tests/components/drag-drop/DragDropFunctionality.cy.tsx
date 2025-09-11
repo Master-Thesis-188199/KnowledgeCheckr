1b13f8cdfa644da5a1033132e7cb97ca382b08d4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-09-11 16:31:32 +0200

    test: verified drag-drop component functionality

---
+++ src/tests/components/drag-drop/DragDropFunctionality.cy.tsx
@@ -0,0 +1,114 @@
+import DragDropContainer from '@/src/components/Shared/drag-drop/DragDropContainer'
+import { DragDropItem } from '@/src/components/Shared/drag-drop/DragDropItem'
+
+describe('Drag Drop Components:', () => {
+  it('Verify that items can be dragged and dropped, thus switched with each other', () => {
+    cy.viewport(780, 980)
+
+    const items = [
+      { id: '1', content: 'Item 1' },
+      { id: '2', content: 'Item 2' },
+      { id: '3', content: 'Item 3' },
+    ]
+    cy.mount(
+      <DragDropContainer className='drag-drop-container'>
+        {items.map((item, i) => (
+          <DragDropItem name={item.content} key={i} initialIndex={i} showPositionCounter>
+            <span className='flex-1'>{item.content}</span>
+          </DragDropItem>
+        ))}
+      </DragDropContainer>,
+    )
+
+    const dragAndDrop = (dragLocator: Cypress.Chainable<JQuery<HTMLElement>>, dropLocator: Cypress.Chainable<JQuery<HTMLElement>>) => {
+      dragLocator.realHover().realMouseDown({ button: 'left', position: 'center' }).realMouseMove(0, 10, { position: 'center' }).wait(250)
+      dropLocator.realMouseMove(0, 0, { position: 'center' }).realMouseUp()
+    }
+
+    cy.get('.drag-drop-container').should('exist')
+    cy.get('.drag-drop-container  [data-swapy-item]').should('have.length', items.length)
+
+    //? Ensure initial order of items
+    cy.get('.drag-drop-container  [data-swapy-item]').first().should('contain.text', items.at(0)!.content)
+    cy.get('.drag-drop-container  [data-swapy-item]').last().should('contain.text', items.at(-1)!.content)
+
+    dragAndDrop(cy.get('[data-swapy-item]').first(), cy.get('div[data-swapy-item]').last())
+
+    ///? Verify order of switches items
+    cy.get('.drag-drop-container  [data-swapy-item]').first().should('contain.text', items.at(-1)!.content)
+    cy.get('.drag-drop-container  [data-swapy-item]').last().should('contain.text', items.at(0)!.content)
+  })
+
+  it('Verify that the swap event is triggered when items are swapped', () => {
+    cy.viewport(780, 980)
+
+    const eventHandler = cy.stub().as('eventHandler')
+
+    const items = [
+      { id: '1', content: 'Item 1' },
+      { id: '2', content: 'Item 2' },
+      { id: '3', content: 'Item 3' },
+    ]
+    cy.mount(
+      <DragDropContainer className='drag-drop-container' onSwapEnd={eventHandler}>
+        {items.map((item, i) => (
+          <DragDropItem name={item.content} key={i} initialIndex={i} showPositionCounter>
+            <span className='flex-1'>{item.content}</span>
+          </DragDropItem>
+        ))}
+      </DragDropContainer>,
+    )
+
+    const dragAndDrop = (dragLocator: Cypress.Chainable<JQuery<HTMLElement>>, dropLocator: Cypress.Chainable<JQuery<HTMLElement>>) => {
+      dragLocator.realHover().realMouseDown({ button: 'left', position: 'center' }).realMouseMove(0, 10, { position: 'center' }).wait(250)
+      dropLocator.realMouseMove(0, -10, { position: 'center' }).realMouseUp()
+    }
+
+    cy.get('@eventHandler').should('not.have.been.called')
+
+    dragAndDrop(cy.get('[data-swapy-item]').first(), cy.get('div[data-swapy-item]').last())
+
+    ///? Verify order of switches items
+    cy.get('.drag-drop-container  [data-swapy-item]').first().should('contain.text', items.at(-1)!.content)
+    cy.get('.drag-drop-container  [data-swapy-item]').last().should('contain.text', items.at(0)!.content)
+    cy.get('@eventHandler').should('have.been.calledOnce')
+  })
+
+  it('Verify that onSwap event is called on DragDropItem', () => {
+    cy.viewport(780, 980)
+
+    const swapEventHandler = cy.stub().as('eventHandler').log(true)
+
+    const items = [
+      { id: '1', content: 'Item 1' },
+      { id: '2', content: 'Item 2' },
+      { id: '3', content: 'Item 3' },
+    ]
+    cy.mount(
+      <DragDropContainer className='drag-drop-container'>
+        {items.map((item, i) => (
+          <DragDropItem name={item.content} key={i} initialIndex={i} showPositionCounter onSwap={swapEventHandler}>
+            <span className='flex-1'>{item.content}</span>
+          </DragDropItem>
+        ))}
+      </DragDropContainer>,
+    )
+
+    const dragAndDrop = (dragLocator: Cypress.Chainable<JQuery<HTMLElement>>, dropLocator: Cypress.Chainable<JQuery<HTMLElement>>) => {
+      dragLocator.realHover().realMouseDown({ button: 'left', position: 'center' }).realMouseMove(0, 10, { position: 'center' }).wait(250)
+      dropLocator.realMouseMove(0, -10, { position: 'center' }).realMouseUp()
+    }
+
+    cy.get('@eventHandler').should('not.have.been.called')
+
+    dragAndDrop(cy.get('[data-swapy-item]').first(), cy.get('div[data-swapy-item]').last())
+
+    ///? Verify order of switches items
+    cy.get('.drag-drop-container  [data-swapy-item]').first().should('contain.text', items.at(-1)!.content)
+    cy.get('.drag-drop-container  [data-swapy-item]').last().should('contain.text', items.at(0)!.content)
+
+    cy.get('@eventHandler').should('have.been.calledTwice')
+    cy.get('@eventHandler').should('have.been.calledWithMatch', { detail: { name: 'Item 1', new_pos: 2, prev_pos: 0 } })
+    cy.get('@eventHandler').should('have.been.calledWithMatch', { detail: { name: 'Item 3', new_pos: 0, prev_pos: 2 } })
+  })
+})
