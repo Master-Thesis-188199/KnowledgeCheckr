dbe4a85b72169670bd696d069dc1ebee3e1dc66c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-04 13:10:44 +0200

    ref: disabled rendering of unfocussed stages
    The reason for this change is that when components are re-used between stages, then elements may be the same identifiers. This causes the test-cases to find more than element matching said identifiers. By simply not rendering stages that are not displayed anyway this issue is resolved.

--- src/components/Shared/MultiStageProgress/MutliStageRenderer.tsx
+++ src/components/Shared/MultiStageProgress/MutliStageRenderer.tsx
@@ -7,5 +7,7 @@ import React from 'react'
 export function MutliStageRenderer({ children, stage, className }: { children: React.ReactNode; stage: Stage['stage']; className?: string }) {
   const { stage: currentStage } = useMultiStageStore((store) => store)
 
+  if (currentStage !== stage) return null
+
   return <div className={cn(currentStage !== stage && 'hidden', className)}>{children}</div>
 }
