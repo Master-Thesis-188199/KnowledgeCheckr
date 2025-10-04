a7ee2661a402dec1e985fcd3ab61d4b354f59edb
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-04 13:23:52 +0200

    ref: re-applied multi-stage progress to ConfigKnCheck component
    The reason for this change is that by changing the structure of the /create page in both branches at the same time. The merge-conflict resolution removed the usage of the multi-stage progress within the /create page and did not re-apply it within the ConfigureKnowledgeCheck component.

--- src/components/check/ConfigureKnowledgeCheck.tsx
+++ src/components/check/ConfigureKnowledgeCheck.tsx
@@ -2,8 +2,12 @@ import GeneralSection from '@/src/app/checks/create/GeneralSection'
 import QuestionsSection from '@/src/app/checks/create/QuestionsSection'
 import { SaveCheckButton } from '@/src/app/checks/create/SaveCheckButton'
 import SettingsSection from '@/src/app/checks/create/SettingsSection'
+import { OverviewSection } from '@/src/components/check/create/(sections)/OverviewSection'
 import { CheckStoreProvider, CheckStoreProviderProps } from '@/src/components/check/create/CreateCheckProvider'
-import Card from '@/src/components/Shared/Card'
+import { MultiStageBackButton, MultiStageNextButton } from '@/src/components/Shared/MultiStageProgress/MultiStageNavigationButtons'
+import { MultiStageProgressBar } from '@/src/components/Shared/MultiStageProgress/MultiStageProgressBar'
+import { MultiStageStoreProvider } from '@/src/components/Shared/MultiStageProgress/MultiStageStoreProvider'
+import { MutliStageRenderer } from '@/src/components/Shared/MultiStageProgress/MutliStageRenderer'
 import PageHeading from '@/src/components/Shared/PageHeading'
 
 type CreateProps = Pick<CheckStoreProviderProps, 'options' | 'initialStoreProps'> & {
@@ -18,17 +22,45 @@ type EditProps = Required<Pick<CheckStoreProviderProps, 'initialStoreProps'>> &
 export function ConfigureKnowledgeCheck({ mode, initialStoreProps, options }: CreateProps | EditProps = { mode: 'create' }) {
   return (
     <CheckStoreProvider initialStoreProps={initialStoreProps} options={options}>
-      <PageHeading title={`${mode === 'create' ? 'Create KnowledgeCheck' : initialStoreProps.name}`} />
-      <div className='grid grid-cols-1 gap-8 lg:grid-cols-[repeat(auto-fill,minmax(680px,1fr))]'>
-        <GeneralSection />
-        <QuestionsSection />
-        <SettingsSection />
-        <Card className='h-60 break-inside-avoid' disableHoverStyles children={undefined} />
-      </div>
-      <form className='mt-4 flex justify-center gap-4'>
-        <SaveCheckButton cacheKey={options?.cacheKey} />
-      </form>
-      <div />
+      <MultiStageStoreProvider
+        initialStoreProps={{
+          stages: [
+            { stage: 1, title: 'Basic Information' },
+            { stage: 2, title: 'Questions' },
+            { stage: 3, title: 'Settings' },
+            { stage: 4, title: 'Conclusion' },
+          ],
+        }}>
+        <PageHeading title={`${mode === 'create' ? 'Create KnowledgeCheck' : initialStoreProps.name}`} />
+        <MultiStageProgressBar className='-mt-2 mb-12' />
+
+        <div className='mx-[1.5%] grid grid-cols-1 gap-8'>
+          <MutliStageRenderer stage={1}>
+            <GeneralSection />
+          </MutliStageRenderer>
+          <MutliStageRenderer stage={2}>
+            <QuestionsSection />
+          </MutliStageRenderer>
+
+          <MutliStageRenderer stage={3}>
+            <SettingsSection />
+          </MutliStageRenderer>
+
+          <MutliStageRenderer stage={4}>
+            <OverviewSection />
+          </MutliStageRenderer>
+        </div>
+        <div className='mx-[1.5%] mt-4 flex justify-between'>
+          <MultiStageBackButton variant='outline' children='Back' />
+          <MultiStageNextButton variant='primary' children='Next' />
+        </div>
+        <MutliStageRenderer stage={4}>
+          <form className='mt-4 flex justify-center gap-4'>
+            <SaveCheckButton />
+          </form>
+        </MutliStageRenderer>
+        <div />
+      </MultiStageStoreProvider>
     </CheckStoreProvider>
   )
 }
