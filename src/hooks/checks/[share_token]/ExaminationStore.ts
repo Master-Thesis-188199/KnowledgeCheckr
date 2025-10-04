7d2465292032bec7c6d76a2d2430c1d0084daf32
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-04 12:03:14 +0200

    ref: updated examination store cache-key to be dynamic
    The reason for this change is that previously providers used a static-cacheKey. But now the cache-key can be changed, thus the cacheKey within the ExaminationStore was updated accordingly.

--- src/hooks/checks/[share_token]/ExaminationStore.ts
+++ src/hooks/checks/[share_token]/ExaminationStore.ts
@@ -30,7 +30,7 @@ const defaultInitState: ExaminationState = {
 
 export const createExaminationStore: CreateStoreType<ExaminationState> = (initialState = defaultInitState, options) => {
   return createStore<ExaminationStore>()((set) => {
-    const { modify: modifyState } = useCacheStoreUpdate(set, { options, cache_key: 'examination-store' })
+    const { modify: modifyState } = useCacheStoreUpdate(set, { options, cache_key: options?.cacheKey ?? 'examination-store' })
 
     return {
       ...initialState,
