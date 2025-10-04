e70bf6042a0c44b143f8a8133bba889d863b9f66
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-04 12:53:01 +0200

    ref: updated heading within OvervieSection

--- src/components/check/create/(sections)/OverviewSection.tsx
+++ src/components/check/create/(sections)/OverviewSection.tsx
@@ -2,12 +2,14 @@ import GeneralSection from '@/src/app/checks/create/GeneralSection'
 import QuestionsSection from '@/src/app/checks/create/QuestionsSection'
 import SettingsSection from '@/src/app/checks/create/SettingsSection'
 import Card from '@/src/components/Shared/Card'
-import PageHeading from '@/src/components/Shared/PageHeading'
 
 export function OverviewSection() {
   return (
     <>
-      <PageHeading title='Preview' />
+      <div className='mt-4 mb-8 flex flex-col gap-1'>
+        <span children='Preview changes' className='text-xl font-semibold' />
+        <p className='dark:text-neutral-300'>Here is a brief overview of what changes were made</p>
+      </div>
       <div className='3xl:grid-cols-3 grid gap-8 xl:grid-cols-2 **:dark:[&_input]:ring-neutral-600/70 **:dark:[&_textarea]:ring-neutral-600/70' inert>
         <GeneralSection />
         <QuestionsSection />
