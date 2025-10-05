6cc5c472373e7d124e9a361835668844c3992120
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-05 14:08:07 +0200

    feat: created `createZustandStore` utility function
    The reason for this new utility function is that it de-duplicates the instantiations of new stores that are cached. In other words, previously every store that was cached used the same boilerplate code of defining the `modify` function that is used instead of the set function to cache changes.
    
    By using this new createZustandStore function the caching-setup is done under the hood, reducing the overall code duplication.

---
+++ src/hooks/Shared/zustand/createZustandStore.ts
@@ -0,0 +1,52 @@
+'use client'
+
+import useCacheStoreUpdate from '@/src/hooks/Shared/useCacheStoreUpdate'
+import { StoreCachingOptions } from '@/types/Shared/ZustandStore'
+import { createStore, StoreApi } from 'zustand/vanilla'
+type SetFn<T> = StoreApi<T>['setState']
+type GetFn<T> = StoreApi<T>['getState']
+
+/**
+ * Used to initialize the respective store
+ * @param set Modifies the state of the store, while also caching the changes when `caching` is set to true
+ * @param get Returns the current store
+ * @returns
+ */
+type StoreInitializer<T> = (set: SetFn<T>, get: GetFn<T>) => T
+
+type CreateStoreProps_WithCache<T> = {
+  caching: true
+  options: Required<Pick<StoreCachingOptions, 'cacheKey'>> & { debounceTime?: number }
+  /**
+   * Used to initialize the respective store
+   * @param set Modifies the state of the store, while also caching the changes when `caching` is set to true
+   * @param get Returns the current store
+   * @returns
+   */
+  initializer: StoreInitializer<T>
+}
+
+type CreateStoreProps_WithoutCache<T> = {
+  caching: false
+  /**
+   * Used to initialize the respective store
+   * @param set Modifies the state of the store, while also caching the changes when `caching` is set to true
+   * @param get Returns the current store
+   * @returns
+   */
+  initializer: StoreInitializer<T>
+}
+
+type CreateStoreProps<T> = CreateStoreProps_WithCache<T> | CreateStoreProps_WithoutCache<T>
+
+export function createZustandStore<T extends object>({ initializer, ...props }: CreateStoreProps<T>) {
+  const store = createStore<T>()
+
+  return store((set, get) => {
+    if (!props.caching) return initializer(set, get)
+
+    const { modify } = useCacheStoreUpdate<T>(set, props.options)
+
+    return initializer(modify as unknown as (prev: T) => T | Partial<T>, get)
+  })
+}
