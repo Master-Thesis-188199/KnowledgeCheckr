cbd1a43d83244668c164228317d41c1c34ea357e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-13 17:17:34 +0200

    ref: added custom onChange handler to multiple-choice answers
    The reason for this change is that previously, when no an answers was not selected the value at the given index was of type boolean ("false"), which violates the schema definition. By changing the onChange handler to use the answer.name instead, the selection array either includes the answer.name or undefined.

--- src/components/checks/[share_token]/practice/RenderPracticeQuestion.tsx
+++ src/components/checks/[share_token]/practice/RenderPracticeQuestion.tsx
@@ -108,7 +108,7 @@ export function RenderPracticeQuestion() {
                 className='hidden'
                 id={`${question.id}-answer-${i}`}
                 type='checkbox'
-                {...register(`answer.selection.${i}`)}
+                onChange={() => register(`answer.selection.${i}`).onChange({ target: { name: `answer.selection.${i}`, value: a.answer } })}
                 disabled={isSubmitted && isSubmitSuccessful && !isPending}
                 value={a.answer}
               />
