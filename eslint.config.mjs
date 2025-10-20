e233b59d1a3b5d825102ca76329c814a95aead56
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-20 20:46:28 +0200

    chore: added custom server-only rule to eslint config
    This way the custom server-only eslint rule becomes part of the eslint configuration, which means that running eslint will also check whether this rule is violated or not.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import serverOnlyPlugin from './config/eslint_rules/server-only-first.mjs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -18,6 +19,14 @@ const eslintConfig = [
       'react-hooks/exhaustive-deps': 0,
     },
   }),
+  {
+    plugins: {
+      'server-only-first': serverOnlyPlugin,
+    },
+    rules: {
+      'server-only-first/server-only-first': 'error',
+    },
+  },
 ]
 
 export default eslintConfig
