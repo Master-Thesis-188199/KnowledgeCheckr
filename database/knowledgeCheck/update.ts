cec6e875b296036e12d4863f1272b31848b3c1e1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-25 16:21:15 +0200

    chore!: updated update handler to use drizzle-adapter

--- database/knowledgeCheck/update.ts
+++ database/knowledgeCheck/update.ts
@@ -1,23 +1,32 @@
-import getDatabase from '@/database/Database'
-import insertKnowledgeCheck from '@/database/knowledgeCheck/insert'
-import requireAuthentication from '@/src/lib/auth/requireAuthentication'
-import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 import { User } from 'better-auth'
+import { eq } from 'drizzle-orm'
+import { getDrizzleDatabase } from '@/database/Database'
+import { db_knowledgeCheck } from '@/drizzle/schema'
+import requireAuthentication from '@/src/lib/auth/requireAuthentication'
+import { formatDatetime } from '@/src/lib/Shared/formatDatetime'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 
-export async function updateKnowledgeCheck(user_id: User['id'], updatedCheck: KnowledgeCheck) {
+export async function updateKnowledgeCheck(user_id: User['id'], updatedCheck: Pick<KnowledgeCheck, 'id'> & Partial<KnowledgeCheck>) {
   await requireAuthentication()
 
-  const db = await getDatabase()
+  const db = await getDrizzleDatabase()
 
-  await db.beginTransaction()
-
-  try {
-    await db.exec('DELETE FROM KnowledgeCheck WHERE id = ?', [updatedCheck.id])
-    await insertKnowledgeCheck(user_id, updatedCheck, false)
-
-    await db.commit()
-  } catch (err) {
-    await db.rollback()
-    console.error('[Rollback]: Error updating knowledge check:', err)
-  }
+  await db.transaction(async (tx) => {
+    try {
+      await tx.delete(db_knowledgeCheck).where(eq(db_knowledgeCheck.id, updatedCheck.id))
+      await tx
+        .update(db_knowledgeCheck)
+        .set({
+          ...updatedCheck,
+          openDate: updatedCheck.openDate ? formatDatetime(updatedCheck.openDate) : undefined,
+          closeDate: updatedCheck.closeDate ? formatDatetime(updatedCheck.closeDate) : undefined,
+          createdAt: updatedCheck.createdAt ? formatDatetime(updatedCheck.createdAt) : undefined,
+          updatedAt: updatedCheck.updatedAt ? formatDatetime(updatedCheck.updatedAt) : undefined,
+        })
+        .where(eq(db_knowledgeCheck.id, updatedCheck.id))
+    } catch (err) {
+      await tx.rollback()
+      console.error('[Rollback]: Error updating knowledge check:', err)
+    }
+  })
 }
