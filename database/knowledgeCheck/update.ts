cb402ed407b79e71d386daedbc130b443d4885d2
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-10-26 12:18:49 +0100

    ref: changed updateCheck db helper to delete and re-insert
    The reason for this change is that this way the check is truly updated. Previously the helper function only updated the base properties of a KnowledgeCheck. The partial updating shall be implemented in the future.

--- database/knowledgeCheck/update.ts
+++ database/knowledgeCheck/update.ts
@@ -1,12 +1,12 @@
 import { User } from 'better-auth'
 import { eq } from 'drizzle-orm'
 import { getDrizzleDatabase } from '@/database/Database'
+import insertKnowledgeCheck from '@/database/knowledgeCheck/insert'
 import { db_knowledgeCheck } from '@/drizzle/schema'
 import requireAuthentication from '@/src/lib/auth/requireAuthentication'
-import { formatDatetime } from '@/src/lib/Shared/formatDatetime'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 
-export async function updateKnowledgeCheck(user_id: User['id'], updatedCheck: Pick<KnowledgeCheck, 'id'> & Partial<KnowledgeCheck>) {
+export async function updateKnowledgeCheck(user_id: User['id'], updatedCheck: KnowledgeCheck) {
   await requireAuthentication()
 
   const db = await getDrizzleDatabase()
@@ -14,16 +14,7 @@ export async function updateKnowledgeCheck(user_id: User['id'], updatedCheck: Pi
   await db.transaction(async (tx) => {
     try {
       await tx.delete(db_knowledgeCheck).where(eq(db_knowledgeCheck.id, updatedCheck.id))
-      await tx
-        .update(db_knowledgeCheck)
-        .set({
-          ...updatedCheck,
-          openDate: updatedCheck.openDate ? formatDatetime(updatedCheck.openDate) : undefined,
-          closeDate: updatedCheck.closeDate ? formatDatetime(updatedCheck.closeDate) : undefined,
-          createdAt: updatedCheck.createdAt ? formatDatetime(updatedCheck.createdAt) : undefined,
-          updatedAt: updatedCheck.updatedAt ? formatDatetime(updatedCheck.updatedAt) : undefined,
-        })
-        .where(eq(db_knowledgeCheck.id, updatedCheck.id))
+      await insertKnowledgeCheck(user_id, updatedCheck)
     } catch (err) {
       await tx.rollback()
       console.error('[Rollback]: Error updating knowledge check:', err)
