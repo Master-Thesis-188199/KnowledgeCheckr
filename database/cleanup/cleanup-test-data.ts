aabd2c43bfb941be24f6191089c5b988a4540889
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-11-09 16:15:02 +0100

    chore: added safety check if no test-user exists

--- database/cleanup/cleanup-test-data.ts
+++ database/cleanup/cleanup-test-data.ts
@@ -10,8 +10,14 @@ async function cleanupTestData() {
 
   const testEmail = 'test@email.com'
 
-  const [{ userId }] = await db.select({ userId: db_user.id }).from(db_user).where(eq(db_user.email, testEmail)).limit(1)
-  await db.delete(db_knowledgeCheck).where(eq(db_knowledgeCheck.owner_id, userId))
+  const users = await db.select({ userId: db_user.id }).from(db_user).where(eq(db_user.email, testEmail)).limit(1)
+
+  if (users.length === 0 || !users.at(0)?.userId) {
+    console.error(`No test-user found matching email: '${testEmail}'`)
+    process.exit()
+  }
+
+  await db.delete(db_knowledgeCheck).where(eq(db_knowledgeCheck.owner_id, users.at(0)!.userId))
 
   console.log(`Deleted all KnowledgeChecks created by the test-user with the email ${testEmail}`)
 
