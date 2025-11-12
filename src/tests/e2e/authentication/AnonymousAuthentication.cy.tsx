ff839af4a15c50276072c08375ddfd39c978e510
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-11-12 16:09:19 +0100

    test: verified that anonymous users can create checks

--- src/tests/e2e/authentication/AnonymousAuthentication.cy.tsx
+++ src/tests/e2e/authentication/AnonymousAuthentication.cy.tsx
@@ -1,3 +1,5 @@
+import { Any } from '@/types'
+
 describe('Anonymous User Authentication: ', () => {
   it('Ensure users can signin anonymously and signout', () => {
     cy.visit('/account/login')
@@ -15,5 +17,26 @@ describe('Anonymous User Authentication: ', () => {
 
     cy.wait('@signout')
     cy.getCookie('better-auth.session_token').should('not.exist')
+  })
+
+  it('Ensure anonymous users can create KnowledgeChecks', () => {
+    cy.loginAnonymously()
+
+    cy.visit('/checks/create')
+
+    cy.get('input[name="check-name"]').type('Example Anonymous Check')
+
+    cy.get('#multi-stage-list-parent').children().filter(':visible').should('have.length', 1).children().last().click()
+    cy.intercept('POST', `/checks/create`).as('create-server-action')
+    cy.get("[aria-label='save knowledge check']").should('exist').click({ force: true })
+
+    cy.wait('@create-server-action').then((interception) => {
+      const request = interception.request
+      const body: Array<Any> = JSON.parse(request.body)
+      const response = interception.response
+
+      expect(body.at(0)).to.have.property('check')
+      expect(response?.statusCode).to.eq(303)
+    })
   })
 })
