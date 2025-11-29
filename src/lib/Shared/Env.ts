090d0a1bc9827ae1aaefece17cdc8610f980ff97
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-11-29 16:52:19 +0100

    chore: updated dex property requirements in `Env`
    This way the dex related environment variables are only required when the `MODE` property is set to `test`. Otherwise, the dex properties are optional and thus do not throw errors when they do not exist.

--- src/lib/Shared/Env.ts
+++ src/lib/Shared/Env.ts
@@ -3,45 +3,78 @@ import { z } from 'zod'
 
 dotenv.config()
 
-export const envSchema = z.object({
-  BETTER_AUTH_URL: z.string().url().describe('Defines the trusted domains at which users can authenticate themselves'),
-  NEXT_PUBLIC_BASE_URL: z.string().startsWith('http').includes('://'),
-  AUTH_SECRET: z.string().base64(),
+export const envSchema = z
+  .object({
+    BETTER_AUTH_URL: z.string().url().describe('Defines the trusted domains at which users can authenticate themselves'),
+    NEXT_PUBLIC_BASE_URL: z.string().startsWith('http').includes('://'),
+    AUTH_SECRET: z.string().base64(),
 
-  DATABASE_HOST: z.union([
-    z.string().regex(/^\S*$/, { message: 'When using the service-name as the database host, make sure that it does not contain any spaces! (Alternatively provide a valid URL / IP)' }),
-    z.string().ip({ message: 'Please provide a valid database host url / ip / service-name' }),
-    z.string().url({ message: 'Please provide a valid database host url / ip / service-name' }),
-    // .min(1, 'The database host must not be empty!')
-  ]),
-  DATABASE_PORT: z
-    .string()
-    .min(1, 'AUTH_MONGODB_HOST_PORT cannot be empty!')
-    .transform((val) => parseInt(val)),
-  DATABASE_NAME: z.string().min(1, 'AUTH_MONGODB_HOST_NAME cannot be empty!'),
-  DATABASE_USER: z.string().min(1, 'The database user must not be empty!'),
-  DATABASE_PASSWORD: z.string().optional(),
+    DATABASE_HOST: z.union([
+      z.string().regex(/^\S*$/, { message: 'When using the service-name as the database host, make sure that it does not contain any spaces! (Alternatively provide a valid URL / IP)' }),
+      z.string().ip({ message: 'Please provide a valid database host url / ip / service-name' }),
+      z.string().url({ message: 'Please provide a valid database host url / ip / service-name' }),
+      // .min(1, 'The database host must not be empty!')
+    ]),
+    DATABASE_PORT: z
+      .string()
+      .min(1, 'AUTH_MONGODB_HOST_PORT cannot be empty!')
+      .transform((val) => parseInt(val)),
+    DATABASE_NAME: z.string().min(1, 'AUTH_MONGODB_HOST_NAME cannot be empty!'),
+    DATABASE_USER: z.string().min(1, 'The database user must not be empty!'),
+    DATABASE_PASSWORD: z.string().optional(),
 
-  AUTH_GITHUB_ID: z.string().min(1, 'NEXTAUTH_GITHUB_ID cannot be empty!'),
-  AUTH_GITHUB_SECRET: z.string().min(1, 'NEXTAUTH_GITHUB_SECRET cannot be empty!'),
+    AUTH_GITHUB_ID: z.string().min(1, 'NEXTAUTH_GITHUB_ID cannot be empty!'),
+    AUTH_GITHUB_SECRET: z.string().min(1, 'NEXTAUTH_GITHUB_SECRET cannot be empty!'),
 
-  AUTH_GOOGLE_ID: z.string().min(1, 'NEXTAUTH_GOOGLE_ID cannot be empty!'),
-  AUTH_GOOGLE_SECRET: z.string().min(1, 'NEXTAUTH_GOOGLE_SECRET cannot be empty!'),
+    AUTH_GOOGLE_ID: z.string().min(1, 'NEXTAUTH_GOOGLE_ID cannot be empty!'),
+    AUTH_GOOGLE_SECRET: z.string().min(1, 'NEXTAUTH_GOOGLE_SECRET cannot be empty!'),
 
-  SHOW_APP_VERSION: z
-    .string()
-    .transform((val) => val.toLowerCase().trim() === 'true')
-    .optional(),
-  MODE: z.preprocess((v) => v ?? process.env.NODE_ENV ?? 'production', z.enum(['test', 'production', 'development'])),
-  //* Custom Provider
-  DEX_PROVIDER_URL: z.union([
-    z.string().regex(/^\S*$/, { message: 'When using the service-name as the host, make sure that it does not contain any spaces! (Alternatively provide a valid URL / IP)' }),
-    z.string().ip({ message: 'Please provide a valid host url / ip / service-name' }),
-    z.string().url({ message: 'Please provide a valid host url / ip / service-name' }),
-  ]),
-  DEX_CLIENT_ID: z.string().default('nextjs-client').catch('nextjs-client'),
-  DEX_CLIENT_SECRET: z.string().default('dev-secret').catch('dev-secret'),
-})
+    SHOW_APP_VERSION: z
+      .string()
+      .transform((val) => val.toLowerCase().trim() === 'true')
+      .optional(),
+    MODE: z.preprocess((v) => v ?? process.env.NODE_ENV ?? 'production', z.enum(['test', 'production', 'development'])),
+    //* Custom Provider
+    DEX_PROVIDER_URL: z
+      .union([
+        z.string().regex(/^\S*$/, { message: 'When using the service-name as the host, make sure that it does not contain any spaces! (Alternatively provide a valid URL / IP)' }),
+        z.string().ip({ message: 'Please provide a valid host url / ip / service-name' }),
+        z.string().url({ message: 'Please provide a valid host url / ip / service-name' }),
+      ])
+      .optional(),
+    DEX_CLIENT_ID: z.string().optional().default('nextjs-client').catch('nextjs-client'),
+    DEX_CLIENT_SECRET: z.string().optional().default('dev-secret').catch('dev-secret'),
+  })
+  .superRefine((env, ctx) => {
+    if (env.MODE === 'test') {
+      // DEX_PROVIDER_URL required in test
+      if (!env.DEX_PROVIDER_URL) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ['DEX_PROVIDER_URL'],
+          message: 'DEX_PROVIDER_URL is required when MODE is "test".',
+        })
+      }
+
+      // DEX_CLIENT_ID required in test
+      if (!env.DEX_CLIENT_ID) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ['DEX_CLIENT_ID'],
+          message: 'DEX_CLIENT_ID is required when MODE is "test".',
+        })
+      }
+
+      // DEX_CLIENT_SECRET required in test
+      if (!env.DEX_CLIENT_SECRET) {
+        ctx.addIssue({
+          code: z.ZodIssueCode.custom,
+          path: ['DEX_CLIENT_SECRET'],
+          message: 'DEX_CLIENT_SECRET is required when MODE is "test".',
+        })
+      }
+    }
+  })
 
 const res = envSchema.safeParse(process.env)
 if (res.error) {
