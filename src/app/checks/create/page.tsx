35c41ecc912de2679d18e9b357b19b45c69412f9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-11-30 10:43:50 +0100

    ref: removed unused `createDummyCheckAction` code

--- src/app/checks/create/page.tsx
+++ src/app/checks/create/page.tsx
@@ -1,79 +1,8 @@
-import { redirect } from 'next/navigation'
-import insertKnowledgeCheck from '@/database/knowledgeCheck/insert'
 import { ConfigureKnowledgeCheck } from '@/src/components/checks/ConfigureKnowledgeCheck'
 import requireAuthentication from '@/src/lib/auth/requireAuthentication'
-import { getUUID } from '@/src/lib/Shared/getUUID'
-import lorem from '@/src/lib/Shared/Lorem'
 
 export default async function CreateCheckPage() {
   await requireAuthentication()
-
-  const createDummyCheckAction = async () => {
-    'use server'
-    const { user } = await requireAuthentication()
-
-    insertKnowledgeCheck(user.id, {
-      id: getUUID(),
-      name: (Math.random() * 100 + 10)
-        .toPrecision(20)
-        .replace(/\./g, '')
-        .split('')
-        .map((char) => String.fromCharCode(65 + parseInt(char)).toLowerCase())
-        .join(''),
-      description: lorem()
-        .split(' ')
-        .slice(0, Math.random() * 100 + 10)
-        .join(' '),
-      share_key: null,
-      closeDate: null,
-      difficulty: 2,
-      openDate: new Date(Date.now()),
-      questionCategories: [{ id: getUUID(), name: 'Geography', skipOnMissingPrequisite: false }],
-      questions: [
-        {
-          id: getUUID(),
-          type: 'multiple-choice',
-          question: 'What is the capital of France?',
-          category: 'Geography',
-          answers: [
-            { id: getUUID(), answer: 'Paris', correct: true },
-            { id: getUUID(), answer: 'Berlin', correct: false },
-            { id: getUUID(), answer: 'Madrid', correct: false },
-            { id: getUUID(), answer: 'Rome', correct: true },
-          ],
-          points: 5,
-        },
-        {
-          id: getUUID(),
-          type: 'multiple-choice',
-          question: 'What is the capital of Greece?',
-          category: 'Geography',
-          answers: [
-            { id: getUUID(), answer: 'Paris', correct: true },
-            { id: getUUID(), answer: 'Berlin', correct: false },
-            { id: getUUID(), answer: 'Madrid', correct: false },
-            { id: getUUID(), answer: 'Rome', correct: true },
-          ],
-          points: 5,
-        },
-        {
-          id: getUUID(),
-          type: 'multiple-choice',
-          question: 'What is the capital of Australia?',
-          category: 'Geography',
-          answers: [
-            { id: getUUID(), answer: 'Paris', correct: true },
-            { id: getUUID(), answer: 'Berlin', correct: false },
-            { id: getUUID(), answer: 'Madrid', correct: false },
-            { id: getUUID(), answer: 'Rome', correct: true },
-          ],
-          points: 5,
-        },
-      ],
-    })
-
-    redirect('/checks')
-  }
 
   return <ConfigureKnowledgeCheck mode='create' />
 }
