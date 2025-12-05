b448a113867957f48a59918e20387651533bdc19
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-05 20:48:07 +0100

    test: verifies that question / answer order works as expected
    This test case ensures that the questions and answers in a check are arranged correctly based on the `.settings.questionOrder` and `.settings.answerOrder`.

--- src/tests/integration/DatabaseInsertionOrder.test.ts
+++ src/tests/integration/DatabaseInsertionOrder.test.ts
@@ -4,7 +4,10 @@ import getDatabase from '@/database/Database'
 import { db_knowledgeCheck, db_user } from '@/database/drizzle/schema'
 import insertKnowledgeCheck from '@/database/knowledgeCheck/insert'
 import { getKnowledgeCheckById } from '@/database/knowledgeCheck/select'
-import { instantiateKnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import prepareExaminationCheck from '@/src/lib/checks/[share_token]/prepareExminationCheck'
+import { instantiateKnowledgeCheck, KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { KnowledgeCheckSettings } from '@/src/schemas/KnowledgeCheckSettingsSchema'
+import { instantiateDragDropQuestion, instantiateMultipleChoice, instantiateOpenQuestion, instantiateSingleChoice } from '@/src/schemas/QuestionSchema'
 
 describe('Validate the order of inserted database elements', () => {
   it('Verify question and answer retrieval order is the same as insertion-order ', async () => {
@@ -40,4 +43,58 @@ describe('Validate the order of inserted database elements', () => {
 
     db.$client.end()
   })
+
+  it.each(['create-order', 'random'] as Array<KnowledgeCheckSettings['questionOrder']>)(
+    "Ensure that `prepareExaminationCheck` shuffled questions & answers do/don't match input order based on order-settings",
+    (order) => {
+      const check: KnowledgeCheck = {
+        ...instantiateKnowledgeCheck(),
+
+        questions: [
+          { ...instantiateSingleChoice(), question: 'This is a single-choice question' },
+          { ...instantiateMultipleChoice(), question: 'This is a multiple-choice question' },
+          { ...instantiateDragDropQuestion(), question: 'This is a drag-drop question' },
+          { ...instantiateOpenQuestion(), question: 'This is an open-question question' },
+        ],
+      }
+
+      check.settings.questionOrder = order
+      check.settings.answerOrder = order
+
+      for (let i = 0; i < 10; i++) {
+        const preparedCheck = prepareExaminationCheck(check)
+
+        if (order === 'random') {
+          expect(preparedCheck.questions.map((q) => q.id).join(',')).not.toBe(check.questions.map((q) => q.id).join(','))
+
+          //* validate answer-order is random
+          expect(
+            preparedCheck.questions
+              .filter((q) => q.type === 'single-choice' || q.type === 'multiple-choice' || q.type === 'drag-drop')
+              .flatMap((q) => q.answers.map((a) => a.id))
+              .join(','),
+          ).not.toBe(
+            check.questions
+              .filter((q) => q.type === 'single-choice' || q.type === 'multiple-choice' || q.type === 'drag-drop')
+              .flatMap((q) => q.answers.map((a) => a.id))
+              .join(','),
+          )
+        } else {
+          expect(preparedCheck.questions.map((q) => q.id).join(',')).toBe(check.questions.map((q) => q.id).join(','))
+          //* validate answer-order is not random
+          expect(
+            preparedCheck.questions
+              .filter((q) => q.type === 'single-choice' || q.type === 'multiple-choice' || q.type === 'drag-drop')
+              .flatMap((q) => q.answers.map((a) => a.id))
+              .join(','),
+          ).toBe(
+            check.questions
+              .filter((q) => q.type === 'single-choice' || q.type === 'multiple-choice' || q.type === 'drag-drop')
+              .flatMap((q) => q.answers.map((a) => a.id))
+              .join(','),
+          )
+        }
+      }
+    },
+  )
 })
