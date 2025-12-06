69e2e29198f3caade385c7d8c01c39305050a9a9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-06 14:30:11 +0100

    ref: removed exception from settings `select` handler
    The reason for this change is that when a knowledgeCheck has no associated settings, then default settings are used instead of throwing an error. This provides backward compatibility.

--- database/knowledgeCheck/settings/select.ts
+++ database/knowledgeCheck/settings/select.ts
@@ -3,16 +3,12 @@ import { eq } from 'drizzle-orm'
 import getDatabase from '@/database/Database'
 import { db_knowledgeCheckSettings } from '@/database/drizzle/schema'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
-import { validateKnowledgeCheckSettings } from '@/src/schemas/KnowledgeCheckSettingsSchema'
+import { instantiateKnowledgeCheckSettings, validateKnowledgeCheckSettings } from '@/src/schemas/KnowledgeCheckSettingsSchema'
 
 export default async function getKnowledgeCheckSettingsById(id: KnowledgeCheck['id']) {
   const db = await getDatabase()
 
   const settings = await db.select().from(db_knowledgeCheckSettings).where(eq(db_knowledgeCheckSettings.knowledgecheckId, id))
 
-  if (settings.length === 0 || !settings.at(0)) {
-    throw new Error(`Settings not found for knowledge check: ${id}`)
-  }
-
-  return validateKnowledgeCheckSettings(settings.at(0))
+  return validateKnowledgeCheckSettings(settings?.at(0) ?? instantiateKnowledgeCheckSettings())
 }
