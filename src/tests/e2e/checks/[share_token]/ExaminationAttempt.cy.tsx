efae13d2909866f09b065172d41c59422282238e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-07 11:31:03 +0100

    test: verifies that exam.-attempt are submitted after time-frame
    Whenever a user exceeds the time-frame of an examination attempt the examination-attempt should be automatically submitted, which this test-case verifies.

---
+++ src/tests/e2e/checks/[share_token]/ExaminationAttempt.cy.tsx
@@ -0,0 +1,24 @@
+import { generateToken } from '@/src/lib/Shared/generateToken'
+import { instantiateKnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+
+describe('ExaminationAttempt Suite: ', () => {
+  beforeEach(() => {
+    cy.loginTestUser()
+  })
+
+  it('Verify that attempt is automatically closed when time-frame is reached', () => {
+    const check = instantiateKnowledgeCheck()
+    check.settings.examTimeFrameSeconds = 60
+    check.share_key = generateToken(8) + '-time-frame'
+
+    cy.request('POST', '/api/insert/knowledgeCheck', check).should('have.property', 'status').and('eq', 200)
+
+    cy.visit(`/checks/${check.share_key}`)
+    cy.get('h1').contains(check.name).should('exist').and('be.visible')
+
+    cy.intercept('POST', `/checks/${check.share_key}`).as('finishAttemptAction')
+
+    cy.wait('@finishAttemptAction').its('response.statusCode').should('eq', 200)
+    cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_BASE_URL')}/checks`)
+  })
+})
