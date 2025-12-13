100bc04c9d7487f21d09cb1ba9ea9bd285f10e11
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-13 12:22:52 +0100

    ref: removed practice category selection for singular category
    Whenever there is only one question category then users are not redirected to choose a category. Instead the `practiceQuestions` are simply set by default to all possible questions.

--- src/app/checks/[share_token]/practice/layout.tsx
+++ src/app/checks/[share_token]/practice/layout.tsx
@@ -17,6 +17,11 @@ export default async function PracticeLayout({
     notFound()
   }
 
-  const practiceQuestions = check.questions.filter((q) => q.accessibility === 'all' || q.accessibility === 'practice-only')
-  return <PracticeStoreProvider initialStoreProps={{ unfilteredQuestions: practiceQuestions }}>{children}</PracticeStoreProvider>
+  const unfilteredQuestions = check.questions.filter((q) => q.accessibility === 'all' || q.accessibility === 'practice-only')
+  const categories = Array.from(new Set(unfilteredQuestions.map((q) => q.category)))
+
+  // When there are no categories to switch between -> set (practice-) questions to be the base-questions.
+  const practiceQuestions = categories.length > 1 ? [] : unfilteredQuestions
+
+  return <PracticeStoreProvider initialStoreProps={{ unfilteredQuestions, practiceQuestions }}>{children}</PracticeStoreProvider>
 }
