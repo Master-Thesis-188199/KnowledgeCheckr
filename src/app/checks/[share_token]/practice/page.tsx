16b10446deee64ca539ecf2b3d6633f9f06086c8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-13 11:24:33 +0100

    ref: externalized practice question navigation bar
    Externalized the rendering of the question navigation bar into a separate component to use the `PracticeStore` hook to access the `practiceQuestions` state.

--- src/app/checks/[share_token]/practice/page.tsx
+++ src/app/checks/[share_token]/practice/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from 'next/navigation'
 import { getKnowledgeCheckByShareToken } from '@/database/knowledgeCheck/select'
 import { PracticeProgress } from '@/src/components/checks/[share_token]/practice/PracticeProgress'
+import { PracticeQuestionNavigation } from '@/src/components/checks/[share_token]/practice/PracticeQuestionNavigation'
 import { PracticeStoreProvider } from '@/src/components/checks/[share_token]/practice/PracticeStoreProvider'
 import { RenderPracticeQuestion } from '@/src/components/checks/[share_token]/practice/RenderPracticeQuestion'
 import PageHeading from '@/src/components/Shared/PageHeading'
@@ -21,14 +22,7 @@ export default async function PracticePage({ params }: { params: Promise<{ share
       <PageHeading title='Practice' />
 
       <div className='mx-auto'>
-        <div id='practice-question-steps' className='mx-4 mb-8 grid grid-cols-[repeat(auto-fill,minmax(64,1fr))] gap-x-5 gap-y-10 lg:gap-x-12'>
-          {practiceQuestions.map((q, i) => (
-            <div key={q.id} className='relative w-fit'>
-              <span className='absolute right-0 bottom-2 left-0 text-center text-sm text-neutral-300'>{i + 1}</span>
-              <div className='h-2 w-16 rounded-xl bg-neutral-400' children='' />
-            </div>
-          ))}
-        </div>
+        <PracticeQuestionNavigation />
 
         <PracticeProgress />
 
