b40a6e5324f37e28466cd1118d236f26a46caf2a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-14 11:24:25 +0100

    ref: added redirection logic to `PracticeCheckButton`
    When clicked users are now redirected / pushed to the practice page of the respective check, while not replacing their last location.

--- src/components/checks/PracticeKnowledgeCheckButton.tsx
+++ src/components/checks/PracticeKnowledgeCheckButton.tsx
@@ -2,9 +2,13 @@
 
 import { Tooltip } from '@heroui/tooltip'
 import { InfoIcon, PlayIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { cn } from '@/src/lib/Shared/utils'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 
 export function PracticeKnowledgeCheckButton({ check, className }: { check: KnowledgeCheck; className?: string }) {
+  const router = useRouter()
+
   return (
     <Tooltip
       content={
@@ -18,8 +22,14 @@ export function PracticeKnowledgeCheckButton({ check, className }: { check: Know
       closeDelay={0}
       shouldFlip
       className='rounded-md bg-neutral-100 p-2 text-sm shadow-sm shadow-neutral-400 dark:bg-neutral-800 dark:text-neutral-300 dark:shadow-neutral-700'>
-      <button className='group/play p-1.5'>
-        <PlayIcon className='size-4.5 group-hover/play:animate-pulse group-hover/play:cursor-pointer dark:text-neutral-400 dark:group-hover/play:text-green-500/90' />
+      <button
+        aria-label='practice KnowledgeCheck'
+        onClick={(e) => {
+          e.preventDefault()
+          router.push(`/checks/${check.share_key}/practice`)
+        }}
+        className='group/play p-1.5'>
+        <PlayIcon className={cn('size-4.5 group-hover/play:animate-pulse group-hover/play:cursor-pointer dark:text-neutral-400 dark:group-hover/play:text-green-500/90', className)} />
       </button>
     </Tooltip>
   )
