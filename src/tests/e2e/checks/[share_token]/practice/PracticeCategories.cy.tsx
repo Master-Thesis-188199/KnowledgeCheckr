6ba97d25ee4b18cf029d72186f377a2a78165a3d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-14 07:08:55 +0100

    test: verify practice category switching via breadcrumbs
    This test case verifies that users can update their desired practice-category through the breadcrumbs as well.

--- src/tests/e2e/checks/[share_token]/practice/PracticeCategories.cy.tsx
+++ src/tests/e2e/checks/[share_token]/practice/PracticeCategories.cy.tsx
@@ -86,4 +86,78 @@ describe('Verify selection of practice questions by category', () => {
 
     cy.get('#practice-question-steps').children().should('have.length', dummyCheck.questions.length)
   })
+
+  ParameterizedTest([{ categorySelection: 'random' }, { categorySelection: 'all' }] as const, ({ categorySelection }) =>
+    it(`Verify users can switch between practice categories through breadcrumbs (category-selection: ${categorySelection})`, () => {
+      const baseURL = Cypress.env('NEXT_PUBLIC_BASE_URL')
+      const dummyCheck = instantiateKnowledgeCheck()
+      const dummyCategories = ['general', 'geography', 'mathematics']
+
+      dummyCheck.share_key = 'select-category' + generateToken(8)
+
+      const questions: Question[] = []
+      const questionCategories: KnowledgeCheck['questionCategories'] = []
+
+      for (const category of dummyCategories) {
+        questions.push({ ...instantiateSingleChoice(), category }, { ...instantiateSingleChoice(), category })
+        questionCategories.push({
+          id: getUUID(),
+          name: category,
+          skipOnMissingPrequisite: false,
+        })
+      }
+
+      dummyCheck.questions = questions
+      dummyCheck.questionCategories = questionCategories
+
+      cy.insertKnowledgeCheck(dummyCheck)
+
+      cy.visit(`/checks/${dummyCheck.share_key}/practice`)
+
+      cy.url().should('eq', `${baseURL}/checks/${dummyCheck.share_key}/practice/category`)
+
+      cy.get('#category-selection')
+        .children()
+        .should('have.length', dummyCategories.length + 1)
+
+      const selection = categorySelection === 'random' ? dummyCategories.at((Math.random() * dummyCategories.length) % dummyCategories.length)! : 'all'
+      cy.get('#category-selection').children(`[data-category="${selection}"]`).should('exist').click()
+
+      cy.url({ timeout: 5 * 1000 }).should('eq', `${baseURL}/checks/${dummyCheck.share_key}/practice?category=${categorySelection === 'random' ? selection : '_none_'}`)
+
+      cy.get('#practice-question-steps')
+        .children()
+        .should('have.length', categorySelection === 'all' ? dummyCheck.questions.length : dummyCheck.questions.filter((q) => q.category === selection).length)
+
+      cy.get('[data-slot="breadcrumb"]').should('exist').and('be.visible')
+      cy.get('#category-switcher')
+        .should('exist')
+        .should('contain.text', selection === 'all' ? 'combined-questions' : selection)
+        .should('have.attr', 'data-state', 'closed')
+        .realHover()
+        .click()
+        .should('have.attr', 'data-state', 'open')
+
+      // select a _different_ category than before, by reverting selection condition (!== vs ===)
+      const breadcrumbSelection: string = categorySelection !== 'random' ? dummyCategories.at((Math.random() * dummyCategories.length) % dummyCategories.length)! : 'all'
+
+      // check drop-down-menu category options
+      cy.get('[data-slot="dropdown-menu-content"]')
+        .should('exist')
+        .and('be.visible')
+        .children()
+        .should('have.length', dummyCategories.length + 1)
+        .parent({ log: false })
+        .children(`[data-category="${breadcrumbSelection}"]`)
+        .should('exist')
+        .click()
+
+      cy.url({ timeout: 5 * 1000 }).should('eq', `${baseURL}/checks/${dummyCheck.share_key}/practice?category=${breadcrumbSelection === 'all' ? '_none_' : breadcrumbSelection}`)
+      cy.wait(250)
+
+      cy.get('#practice-question-steps')
+        .children()
+        .should('have.length', breadcrumbSelection === 'all' ? dummyCheck.questions.length : dummyCheck.questions.filter((q) => q.category === breadcrumbSelection).length)
+    }),
+  )
 })
