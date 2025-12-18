e36eb1895c4123bcfb2cc55e30becb065e6b3cef
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-18 09:58:54 +0100

    feat: added navigation to practice-navigation bar
    Now users can switch between practice questions by clicking on the question-navigation at the top.

--- src/components/checks/[share_token]/practice/PracticeQuestionNavigation.tsx
+++ src/components/checks/[share_token]/practice/PracticeQuestionNavigation.tsx
@@ -3,15 +3,15 @@
 import { usePracticeStore } from '@/src/components/checks/[share_token]/practice/PracticeStoreProvider'
 
 export function PracticeQuestionNavigation() {
-  const { practiceQuestions } = usePracticeStore((store) => store)
+  const { practiceQuestions, navigateToQuestion, currentQuestionIndex } = usePracticeStore((store) => store)
 
   return (
     <div id='practice-question-steps' className='mx-4 mb-8 grid grid-cols-[repeat(auto-fill,minmax(64,1fr))] gap-x-5 gap-y-10 lg:gap-x-12'>
       {practiceQuestions.map((q, i) => (
-        <div key={q.id} className='relative w-fit'>
-          <span className='absolute right-0 bottom-2 left-0 text-center text-sm text-neutral-300'>{i + 1}</span>
-          <div className='h-2 w-16 rounded-xl bg-neutral-400' children='' />
-        </div>
+        <button type='button' disabled={currentQuestionIndex === i} key={q.id} className='group relative w-fit hover:cursor-pointer' onClick={() => navigateToQuestion(i)}>
+          <span className='absolute right-0 bottom-2 left-0 text-center text-sm text-neutral-300 group-hover:font-semibold'>{i + 1}</span>
+          <div className='h-2 w-16 rounded-xl bg-neutral-400 group-hover:bg-neutral-500 dark:group-active:bg-neutral-500/60' children='' />
+        </button>
       ))}
     </div>
   )
