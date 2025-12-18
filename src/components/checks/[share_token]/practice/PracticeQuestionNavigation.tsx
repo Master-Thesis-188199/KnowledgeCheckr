cc092bfac1ca07a83eb9c9a397c01cd6d8c0043c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-18 10:10:20 +0100

    style: added selection-highlight and hover, active styles
    The practice question navigation bar now highlights the currently selected question and now has hover and active styles.

--- src/components/checks/[share_token]/practice/PracticeQuestionNavigation.tsx
+++ src/components/checks/[share_token]/practice/PracticeQuestionNavigation.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { usePracticeStore } from '@/src/components/checks/[share_token]/practice/PracticeStoreProvider'
+import { cn } from '@/src/lib/Shared/utils'
 
 export function PracticeQuestionNavigation() {
   const { practiceQuestions, navigateToQuestion, currentQuestionIndex } = usePracticeStore((store) => store)
@@ -8,9 +9,16 @@ export function PracticeQuestionNavigation() {
   return (
     <div id='practice-question-steps' className='mx-4 mb-8 grid grid-cols-[repeat(auto-fill,minmax(64,1fr))] gap-x-5 gap-y-10 lg:gap-x-12'>
       {practiceQuestions.map((q, i) => (
-        <button type='button' disabled={currentQuestionIndex === i} key={q.id} className='group relative w-fit hover:cursor-pointer' onClick={() => navigateToQuestion(i)}>
-          <span className='absolute right-0 bottom-2 left-0 text-center text-sm text-neutral-300 group-hover:font-semibold'>{i + 1}</span>
-          <div className='h-2 w-16 rounded-xl bg-neutral-400 group-hover:bg-neutral-500 dark:group-active:bg-neutral-500/60' children='' />
+        <button
+          aria-label={`select question ${i}`}
+          type='button'
+          data-selected={currentQuestionIndex === i}
+          disabled={currentQuestionIndex === i}
+          key={q.id}
+          className={cn('group relative w-fit enabled:hover:cursor-pointer disabled:hover:cursor-auto')}
+          onClick={() => navigateToQuestion(i)}>
+          <span className='absolute right-0 bottom-2 left-0 text-center text-sm text-neutral-300 group-enabled:group-hover:font-semibold'>{i + 1}</span>
+          <div className='h-2 w-16 rounded-xl bg-neutral-600 group-enabled:group-hover:bg-neutral-500 group-data-[selected="true"]:bg-neutral-400 dark:group-active:bg-neutral-500/60' children='' />
         </button>
       ))}
     </div>
