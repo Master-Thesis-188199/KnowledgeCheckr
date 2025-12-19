0b51c568eef88fa8cfa98b8f6fec1d63bd9a865c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-19 09:13:43 +0100

    ref: updated `prequsiteCategory` prop in `CategorySchema`
    By changing the property name to `prequisiteCategoryId` makes it more obvious what value it holds. Also, the type was simplifies be only a string instead of string | string[]. The reason for this is that a single-dependency is sufficient.

--- src/schemas/CategorySchema.ts
+++ src/schemas/CategorySchema.ts
@@ -3,11 +3,7 @@ import { z } from 'zod'
 export const CategorySchema = z.object({
   id: z.string(),
   name: z.string(),
-  prequisiteCategory: z
-    .union([z.string().min(1, 'A prerequisite category may not be empty!'), z.array(z.string().min(1, 'A prerequisite category may not be empty!'))])
-    .transform((val) => (typeof val === 'string' ? [val] : val))
-    .refine((pre) => pre.filter((c, i, a) => a.indexOf(c) === i).length !== pre.length, { message: 'Please remove duplicate prequisite categories.' })
-    .optional(),
+  prequisiteCategoryId: z.string().min(1, 'A prerequisite category may not be empty!').optional(),
   skipOnMissingPrequisite: z.boolean().default(false),
 })
 
