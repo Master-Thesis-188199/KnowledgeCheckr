a07227d7d3316c2ce0837225bd0ec9fbb06767e6
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-19 14:19:50 +0100

    ref: simplified `insert` category handler
    This way the category `insert` handler no longer handles the duplicate entry event, but instead throws an error when a duplicate category is inserted. The reason for this change is that the `.onDuplicateKeyUpdate` utility function does not return the existing category-id when a duplicateKey error is suppressed. Hence, the error is essentially unhandled and passed along.

--- database/knowledgeCheck/catagories/insert.ts
+++ database/knowledgeCheck/catagories/insert.ts
@@ -1,17 +1,14 @@
 import 'server-only'
-import { and, eq } from 'drizzle-orm'
 import { DrizzleDB } from '@/database/Database'
 import { db_category } from '@/database/drizzle/schema'
 
-export async function insertCategory(db: DrizzleDB, props: typeof db_category.$inferInsert) {
-  const duplicates = await db
-    .select()
-    .from(db_category)
-    .where(and(eq(db_category.knowledgecheckId, props.knowledgecheckId), eq(db_category.name, props.name)))
-    .limit(1)
-
-  if (duplicates.length > 0) return duplicates[0].id
-
+/**
+ * This function inserts a new category that is associated to a given check. In case it tries to insert a duplicate category-name and knowledgeCheckId pair an error will be thrown.
+ * @param db The database / db-transaction to use
+ * @param props The category-props that are to be inserted
+ * @returns The inserted data
+ */
+export async function insertCategory(db: DrizzleDB, props: typeof db_category.$inferInsert): Promise<typeof db_category.$inferInsert | never> {
   const [{ id }] = await db.insert(db_category).values(props).$returningId()
-  return id
+  return { ...props, id }
 }
