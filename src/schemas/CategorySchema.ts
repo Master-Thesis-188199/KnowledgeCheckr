2dbaebefbe8d6f35429070e8a44c995cc8cb16f4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-19 16:33:59 +0100

    chore: updated `CategorySchema` id prop type to `.uuid()`
    This way the `id` property of a category object is typed, instantiated and validated as a UUID.

--- src/schemas/CategorySchema.ts
+++ src/schemas/CategorySchema.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod'
+import { getUUID } from '@/src/lib/Shared/getUUID'
 
 export const CategorySchema = z.object({
-  id: z.string(),
+  id: z
+    .string()
+    .uuid()
+    .default(() => getUUID()),
   name: z.string(),
   prequisiteCategoryId: z.string().min(1, 'A prerequisite category may not be empty!').optional(),
   skipOnMissingPrequisite: z.boolean().default(false),
