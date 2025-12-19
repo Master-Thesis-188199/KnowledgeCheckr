bd17376ce68bad177535b2bfeb651f5e032fddea
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-19 14:36:19 +0100

    ref: updated category `insert` dao to handle duplicate keys

--- database/knowledgeCheck/catagories/insert.ts
+++ database/knowledgeCheck/catagories/insert.ts
@@ -1,4 +1,5 @@
 import 'server-only'
+import { and, eq } from 'drizzle-orm'
 import { DrizzleDB } from '@/database/Database'
 import { db_category } from '@/database/drizzle/schema'
 
@@ -8,7 +9,16 @@ import { db_category } from '@/database/drizzle/schema'
  * @param props The category-props that are to be inserted
  * @returns The inserted data
  */
-export async function insertCategory(db: DrizzleDB, props: typeof db_category.$inferInsert): Promise<typeof db_category.$inferInsert | never> {
-  const [{ id }] = await db.insert(db_category).values(props).$returningId()
-  return { ...props, id }
+export async function insertCategory(db: DrizzleDB, props: typeof db_category.$inferInsert) {
+  try {
+    const [{ id }] = await db.insert(db_category).values(props).$returningId()
+    return { ...props, id }
+  } catch (_) {
+    const [existingCategory] = await db
+      .select()
+      .from(db_category)
+      .where(and(eq(db_category.name, props.name), eq(db_category.knowledgecheckId, props.knowledgecheckId)))
+
+    return existingCategory
+  }
 }
