67e14c115e5e4a272757f3c0574f58b6245d8a8a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-19 14:14:22 +0100

    Revert "ref: removed duplicate category race condition"
    This reverts commit bd5d900a641799343818475cc5f164e4a98339ea.
    
    The reason for this is that when the `.onDuplicateKeyUpdate()` is used the `.$returningId()` utility stops working and throws an error that the id cannot be retrieved.

--- database/knowledgeCheck/catagories/insert.ts
+++ database/knowledgeCheck/catagories/insert.ts
@@ -1,8 +1,17 @@
 import 'server-only'
+import { and, eq } from 'drizzle-orm'
 import { DrizzleDB } from '@/database/Database'
 import { db_category } from '@/database/drizzle/schema'
 
 export async function insertCategory(db: DrizzleDB, props: typeof db_category.$inferInsert) {
-  const [{ id }] = await db.insert(db_category).values(props).onDuplicateKeyUpdate({ set: {} }).$returningId()
+  const duplicates = await db
+    .select()
+    .from(db_category)
+    .where(and(eq(db_category.knowledgecheckId, props.knowledgecheckId), eq(db_category.name, props.name)))
+    .limit(1)
+
+  if (duplicates.length > 0) return duplicates[0].id
+
+  const [{ id }] = await db.insert(db_category).values(props).$returningId()
   return id
 }
