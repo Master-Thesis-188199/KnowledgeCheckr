08ecf8bc93a8ae3c6221473b00cd56bb7411ea5f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-21 13:05:54 +0100

    chore: added `.setContext` method to winston logger
    This way a 'context', which is essentially any string that helps identify the origin of module-specific log statements, can be set.

--- src/lib/log/Logger.ts
+++ src/lib/log/Logger.ts
@@ -1,5 +1,23 @@
 import 'winston-daily-rotate-file'
-import winston from 'winston'
+import winston, { Logger as WinstonLogger } from 'winston'
+
+/**
+ * Logger type extended with a convenience method to set a module-context
+ * The `context` is can be any string that helps identify the origin of a log entry.
+ */
+export interface ContextLogger extends WinstonLogger {
+  /**
+   *
+   * @example
+   * ```ts
+   * logger.setContext("checks")
+   * logger.info('Fetch checks...');
+   * ```
+   */
+  setContext(context: string): void
+}
+
+let loggerContext: string | null = null
 
 const logFormat = winston.format.printf(({ level, message, timestamp }) => {
   return `${timestamp} [${level}]: ${message}`
@@ -20,11 +38,13 @@ const logger = winston.createLogger({
     new winston.transports.Console({
       format: winston.format.combine(
         winston.format.colorize({ level: true }),
-        winston.format.printf(({ level, message }) => `[${level}]: ${message}`),
+        winston.format.printf(({ level, message }) => {
+          return `${loggerContext ? `${loggerContext}` : ''} [${level}]: ${message}`.trim()
+        }),
       ),
     }),
   ],
-})
+}) as ContextLogger
 
 if (process.env.NODE_ENV === 'production') {
   const transportAll = new winston.transports.DailyRotateFile({
@@ -46,6 +66,10 @@ if (process.env.NODE_ENV === 'production') {
 
   logger.add(transportAll)
   logger.add(transportError)
+}
+
+logger.setContext = function (context: string) {
+  loggerContext = context
 }
 
 export default logger
