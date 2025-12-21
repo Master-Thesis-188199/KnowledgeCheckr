7f8cec772a03c5e27473662829ca15534d19ecd3
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-21 08:25:20 +0100

    ref: updated save-button validation to not crash app
    Instead of using the `validateKnowledgeCheck` utility function the `safeParseKnowledgeCheck` function is used instead. When the object violates the schema definition the save-button is now disabled to prevent the insertion of invalid data and/or the app from crashing.

--- src/components/checks/create/SaveCheckButton.tsx
+++ src/components/checks/create/SaveCheckButton.tsx
@@ -5,32 +5,39 @@ import { useCheckStore } from '@/src/components/checks/create/CreateCheckProvide
 import { useNavigationAbort } from '@/src/components/navigation-abortion/NavigationAbortProvider'
 import { Button } from '@/src/components/shadcn/button'
 import { saveAction } from '@/src/lib/checks/create/SaveAction'
-import { KnowledgeCheck, validateKnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { safeParseKnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 
 export function SaveCheckButton({ cacheKey }: { cacheKey?: string }) {
   const store = useCheckStore((store) => store)
   const { clearNavigationAbort } = useNavigationAbort()
-  const check: KnowledgeCheck = validateKnowledgeCheck(store)
+  const safeParse = safeParseKnowledgeCheck(store)
+
+  if (!safeParse.success) {
+    console.error('[SaveCheckButton]: Parsing of store data failed, save-button disabled. ', safeParse.error)
+  }
 
   return (
     <Button
+      disabled={!safeParse.success}
       aria-label='save knowledge check'
       type='submit'
       formAction={() =>
-        saveAction({ check }).catch((e) => {
-          if (isRedirectError(e)) {
-            const key = cacheKey ?? 'check-store'
-            const hasCache = !!sessionStorage.getItem(key)
+        safeParse.success
+          ? saveAction({ check: safeParse.data }).catch((e) => {
+              if (isRedirectError(e)) {
+                const key = cacheKey ?? 'check-store'
+                const hasCache = !!sessionStorage.getItem(key)
 
-            if (hasCache) {
-              sessionStorage.removeItem(key)
-            } else {
-              console.debug(`[SaveCheckButton]: No cached data was found for cacheKey: ${key}.`)
-            }
+                if (hasCache) {
+                  sessionStorage.removeItem(key)
+                } else {
+                  console.debug(`[SaveCheckButton]: No cached data was found for cacheKey: ${key}.`)
+                }
 
-            clearNavigationAbort()
-          }
-        })
+                clearNavigationAbort()
+              }
+            })
+          : new Promise(() => null)
       }>
       Save
     </Button>
