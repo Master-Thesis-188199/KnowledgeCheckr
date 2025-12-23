b33187aff13c81033c6f9925af75df06f115a4aa
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-23 16:32:42 +0100

    test: updated simple test-case to validate actual output

--- src/tests/integration/Logger.test.ts
+++ src/tests/integration/Logger.test.ts
@@ -3,30 +3,27 @@ import * as console from 'console'
 import logger from '@/src/lib/log/Logger'
 
 describe('Ensure logger input / output mimics console.log: ', () => {
-  let consoleSpy: jest.SpyInstance
-  let loggerSpy: jest.SpyInstance
   let writeSpy: jest.SpyInstance
 
   beforeEach(() => {
     global.console = console
-    consoleSpy = jest.spyOn(console, 'log')
-    loggerSpy = jest.spyOn(logger, 'info')
     writeSpy = jest.spyOn(process.stdout, 'write')
   })
 
   afterEach(() => {
-    consoleSpy.mockRestore()
-    loggerSpy.mockRestore()
+    writeSpy.mockRestore()
   })
 
   it('Verify logging simple messages mimics console.log output', () => {
     const message = 'This is a simple message...'
 
     console.log(message)
-    expect(consoleSpy.mock.calls[0][0]).toBe(message)
-
     logger.info(message)
-    expect(loggerSpy.mock.calls[0][0]).toBe(message)
+
+    const consoleOutput = writeSpy.mock.calls[0][0]
+    const loggerOutput = writeSpy.mock.calls[1][0]
+
+    expect(loggerOutput.replace(/ /g, '')).toContain(consoleOutput.replace(/ /g, ''))
   })
 
   it('Verify logging objects mimics console.log output', () => {
