b9eabf60a230c0a2401821f35e1a592d68778b34
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-23 15:25:16 +0100

    test: verified logger logs simple messages correctly

---
+++ src/tests/integration/Logger.test.ts
@@ -0,0 +1,27 @@
+import { describe, expect, it } from '@jest/globals'
+import logger from '@/src/lib/log/Logger'
+
+describe('Ensure logger input / output mimics console.log: ', () => {
+  let consoleSpy: jest.SpyInstance
+  let loggerSpy: jest.SpyInstance
+
+  beforeEach(() => {
+    consoleSpy = jest.spyOn(console, 'log')
+    loggerSpy = jest.spyOn(logger, 'info')
+  })
+
+  afterEach(() => {
+    consoleSpy.mockRestore()
+    loggerSpy.mockRestore()
+  })
+
+  it('Verify logging simple messages mimics console.log output', () => {
+    const message = 'This is a simple message...'
+
+    console.log(message)
+    expect(consoleSpy.mock.calls[0][0]).toBe(message)
+
+    logger.info(message)
+    expect(loggerSpy.mock.calls[0][0]).toBe(message)
+  })
+})
