41b1191640dbeb52bf0575a67e9a86a899dc3cbf
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-27 08:01:48 +0100

    test: verified eslint rule error and suggestions for static classNames
    This test-case verifies that the color-mode-styles eslint rule detects faulty classNames correctly and provides the expected color-mode suggestions.

--- config/eslint_rules/color-mode-styles/tests/color-mode-tests.test.ts
+++ config/eslint_rules/color-mode-styles/tests/color-mode-tests.test.ts
@@ -46,5 +46,91 @@ ruleTester.run('color-mode-styles rule', requireColorModeStylesRule, {
     `,
     },
   ],
-  invalid: [],
+  invalid: [
+    {
+      //* test missing dark-mode styles with NO existing opposite-class matches in static className properties
+      code: `const A = () => (<div className="bg-neutral-200 text-neutral-700" />)`,
+      errors: [
+        {
+          messageId: 'missingDark',
+          data: {
+            key: 'bg, text',
+            lightStyles: '—',
+            darkStyles: "'dark:bg-neutral-700', 'dark:text-neutral-200'",
+          },
+          suggestions: [
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: "Add bg, text classes 'dark:bg-neutral-700', 'dark:text-neutral-200' in className",
+              output: `const A = () => (<div className="bg-neutral-200 text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200" />)`,
+            },
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: 'Add dark-mode dark:bg-neutral-700',
+              output: `const A = () => (<div className="bg-neutral-200 text-neutral-700 dark:bg-neutral-700" />)`,
+            },
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: 'Add dark-mode dark:text-neutral-200',
+              output: `const A = () => (<div className="bg-neutral-200 text-neutral-700 dark:text-neutral-200" />)`,
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      //* test missing dark-mode styles with existing opposite-class matches in static className properties
+      code: `const A = () => (<div className="bg-neutral-200 text-neutral-700 dark:text-neutral-200" />)`,
+      errors: [
+        {
+          messageId: 'missingDark',
+          data: {
+            key: 'bg',
+            lightStyles: '—',
+            darkStyles: "'dark:bg-neutral-700'",
+          },
+          suggestions: [
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: "Add bg classes 'dark:bg-neutral-700' in className",
+              output: `const A = () => (<div className="bg-neutral-200 text-neutral-700 dark:text-neutral-200 dark:bg-neutral-700" />)`,
+            },
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: 'Add dark-mode dark:bg-neutral-700',
+              output: `const A = () => (<div className="bg-neutral-200 text-neutral-700 dark:text-neutral-200 dark:bg-neutral-700" />)`,
+            },
+          ],
+        },
+      ],
+    },
+
+    {
+      //* test missing light-mode styles with existing opposite-class matches in static className properties
+      code: `const A = () => (<div className="bg-neutral-200 dark:text-neutral-200 dark:bg-neutral-200" />)`,
+      errors: [
+        {
+          messageId: 'missingLight',
+          data: {
+            key: 'text',
+            lightStyles: "'text-neutral-700'",
+            darkStyles: '—',
+          },
+          suggestions: [
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: "Add text classes 'text-neutral-700' in className",
+              output: `const A = () => (<div className="bg-neutral-200 dark:text-neutral-200 dark:bg-neutral-200 text-neutral-700" />)`,
+            },
+            {
+              //@ts-expect-error Type declaration does not recognize 'desc' field, even though it exists.
+              desc: 'Add light-mode text-neutral-700',
+              output: `const A = () => (<div className="bg-neutral-200 dark:text-neutral-200 dark:bg-neutral-200 text-neutral-700" />)`,
+            },
+          ],
+        },
+      ],
+    },
+  ],
 })
