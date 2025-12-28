e4f76d495f54b1f831deca0dd388f1512bce8269
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-28 10:09:59 +0100

    ref: deduplicated jsx by creating internal component
    Externalized the rendering of statistical elements like the amount of Questions, Points and the estimatedTime of a KnowledgeCheck, by creating a `StatisticalElement` component that renders a label and value.

--- src/components/checks/KnowledgeCheckCard.tsx
+++ src/components/checks/KnowledgeCheckCard.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import React from 'react'
 import { motion } from 'framer-motion'
 import { PracticeKnowledgeCheckButton } from '@/src/components/checks/PracticeKnowledgeCheckButton'
 import { ShareKnowledgeCheckButton } from '@/src/components/checks/ShareKnowledgeCheckButton'
@@ -20,23 +21,17 @@ export function KnowledgeCheckCard(check: KnowledgeCheck) {
         <h2 className='text-center text-xl font-semibold text-neutral-600 dark:text-neutral-300'>{check.name}</h2>
         <span className='line-clamp-2 text-center text-sm text-balance text-neutral-500 dark:text-neutral-400'>{check.description}</span>
       </div>
-      <div className='flex flex-wrap justify-evenly gap-8 px-6 text-neutral-300'>
-        <div className='flex max-w-fit flex-col items-center gap-1'>
-          <dt className='text-sm text-neutral-500 dark:text-neutral-400'>Questions</dt>
-          <dd className='order-first text-lg font-semibold tracking-tight text-neutral-600/90 dark:text-neutral-300'>{check.questions.length}</dd>
-        </div>
-        <div className='flex max-w-fit flex-col items-center gap-1'>
-          <dt className='text-sm text-neutral-500 dark:text-neutral-400'>estimated Time</dt>
-          <dd className='order-first text-lg font-semibold tracking-tight text-neutral-600/90 dark:text-neutral-300'>
-            10<span className='text-base'>m</span>
-          </dd>
-        </div>
-        <div className='flex max-w-fit flex-col items-center gap-1'>
-          <dt className='text-sm text-neutral-500 dark:text-neutral-400'>Points</dt>
-          <dd className='order-first text-lg font-semibold tracking-tight text-neutral-600/90 dark:text-neutral-300'>
-            {check.questions.map((q) => q.points).reduce((prev, current) => (prev += current), 0)}
-          </dd>
-        </div>
+      <div className='flex flex-wrap justify-evenly gap-8 px-6 text-neutral-600 dark:text-neutral-300'>
+        <StatisticElement label='Questions' value={check.questions.length} />
+        <StatisticElement
+          label='estimatedTime'
+          value={
+            <>
+              10<span className='text-base'>m</span>
+            </>
+          }
+        />
+        <StatisticElement label='Points' value={check.questions.map((q) => q.points).reduce((prev, current) => (prev += current), 0)} />
       </div>
       <Footer updatedAt={check.updatedAt} />
     </Card>
@@ -47,6 +42,15 @@ function Footer({ updatedAt }: { updatedAt?: Date }) {
   return (
     <div className='-mt-6 -mb-1 flex flex-row-reverse justify-between border-t border-neutral-700 px-4 pt-3 text-xs dark:text-neutral-400/70'>
       <div>last modified: {updatedAt ? new Date(updatedAt).toLocaleDateString('de', { year: '2-digit', month: '2-digit', day: '2-digit' }) : 'N/A'}</div>
+    </div>
+  )
+}
+
+function StatisticElement({ label, value }: { label: string; value: React.ReactNode }) {
+  return (
+    <div className='flex max-w-fit flex-col items-center gap-1'>
+      <dt className='text-sm text-neutral-500 dark:text-neutral-400'>{label}</dt>
+      <dd className='order-first text-lg font-semibold tracking-tight text-neutral-600/90 dark:text-neutral-300'>{value}</dd>
     </div>
   )
 }
