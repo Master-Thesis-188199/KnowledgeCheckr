b50e0a70816362c02b86cab2cbb9f9fa097ec62d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-29 17:19:25 +0100

    style: updated ring variable classes in `Dialog`

--- src/components/Shared/Dialog.tsx
+++ src/components/Shared/Dialog.tsx
@@ -52,7 +52,7 @@ function DialogContent({ className, children, onClose, ...props }: React.Compone
         <DialogPrimitive.Close
           aria-label='close'
           onClick={onClose}
-          className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:scale-110 hover:cursor-pointer hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
+          className="ring-offset-background focus:ring-ring-focus data-[state=open]:bg-accent data-[state=open]:text-muted-foreground dark:focus:ring-ring-focus absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:scale-110 hover:cursor-pointer hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4">
           <XIcon />
           <span className='sr-only'>Close</span>
         </DialogPrimitive.Close>
