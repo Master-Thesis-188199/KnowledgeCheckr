07851c6dbc19c7fc6c923f5e182791f5b8d42e88
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-29 09:21:21 +0100

    ref: added optional (config) test-arguments to `createTest`
    This way users can pass properties like `parser`, `settings` and more to the `createTests` function just like they could when creating their own test manually.

--- config/eslint_rules/color-mode-styles/tests/createTest.ts
+++ config/eslint_rules/color-mode-styles/tests/createTest.ts
@@ -3,7 +3,7 @@ import { ElementType } from 'react'
 import { RunTests } from '@typescript-eslint/utils/ts-eslint'
 import { MessageIds, Options } from '../require-color-mode-styles'
 
-type SpecProps = {
+type SpecProps = Omit<RunTests<MessageIds, Options[]>['invalid'][number], 'code' | 'errors' | 'output' | 'options'> & {
   errorType: MessageIds
   name: string
   baseClasses: string[]
@@ -17,29 +17,30 @@ const quoteString = (input: string) => `'${input}'`
 const quoteJoinArray = (options: string[], seperator = ', ') => options.map(quoteString).join(seperator)
 const joinArray = (options: string[], seperator = ', ') => options.join(seperator)
 
-export default function createTest(spec: SpecProps): RunTests<MessageIds, Options[]>['invalid'][number] {
-  const tag = spec.jsxTag ?? 'div'
-  const baseClassStr = spec.baseClasses.join(' ')
+export default function createTest({ jsxTag, baseClasses, missingClasses, utilities, ruleOptions, name, errorType, ...optionalProps }: SpecProps): RunTests<MessageIds, Options[]>['invalid'][number] {
+  const tag = jsxTag ?? 'div'
+  const baseClassStr = baseClasses.join(' ')
 
-  const options = spec.ruleOptions ? ([spec.ruleOptions] as never) : undefined
+  const options = ruleOptions ? ([ruleOptions] as never) : undefined
 
   return {
-    name: spec.name,
+    ...optionalProps,
+    name: name,
     options,
     code: `const A = () => (<${tag} className="${baseClassStr}" />)`,
     errors: [
       {
-        messageId: spec.errorType,
+        messageId: errorType,
         data: {
-          key: joinArray(spec.utilities),
-          missing: quoteJoinArray(spec.missingClasses),
+          key: joinArray(utilities),
+          missing: quoteJoinArray(missingClasses),
         },
         suggestions: [
           {
-            desc: `Add all missing classes for ${joinArray(spec.utilities)}`,
-            output: `const A = () => (<${tag} className="${baseClassStr} ${spec.missingClasses.join(' ')}" />)`,
+            desc: `Add all missing classes for ${joinArray(utilities)}`,
+            output: `const A = () => (<${tag} className="${baseClassStr} ${missingClasses.join(' ')}" />)`,
           },
-          ...spec.missingClasses.map((missingClass): any => ({
+          ...missingClasses.map((missingClass): any => ({
             desc: `Add ${missingClass.includes('dark:') ? 'dark' : 'light'}-mode ${missingClass}`,
             output: `const A = () => (<${tag} className="${baseClassStr} ${missingClass}" />)`,
           })),
