d96d854712db709973ac666705f4e8746fcf3890
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2025-12-31 08:25:35 +0100

    ref: simplified closure of heroui-tooltip within `Tooltip`

--- src/components/Shared/Tooltip.tsx
+++ src/components/Shared/Tooltip.tsx
@@ -18,6 +18,7 @@ export default function Tooltip({ showsWarning, showsError, ...props }: { disabl
         'rounded-md border-t border-r border-l border-neutral-300 bg-neutral-100 p-2 text-sm text-neutral-600 shadow-sm shadow-neutral-400 dark:border-neutral-600 dark:bg-neutral-800 dark:text-neutral-300/90 dark:shadow-neutral-700',
         showsWarning && 'text-[#BF8415] shadow-orange-500/20 dark:text-orange-400 dark:shadow-orange-400/40',
         showsError && 'text-[#D44D35] shadow-red-500/30 dark:text-red-400/90 dark:shadow-red-400/40',
-      )}></HerouiTooltip>
+      )}
+    />
   )
 }
