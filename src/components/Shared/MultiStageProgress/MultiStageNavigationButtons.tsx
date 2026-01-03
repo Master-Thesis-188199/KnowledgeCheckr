24a5b8a611c47fa32a3ef957cc28e6684c77d727
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-03 08:17:51 +0100

    ref: added tooltips to stage navigation when disabled
    This way users are told why they cannot navigate away from the current stage, e.g. because there are errors in the current form.

--- src/components/Shared/MultiStageProgress/MultiStageNavigationButtons.tsx
+++ src/components/Shared/MultiStageProgress/MultiStageNavigationButtons.tsx
@@ -4,6 +4,7 @@ import { ButtonHTMLAttributes } from 'react'
 import { VariantProps } from 'class-variance-authority'
 import { Button, buttonVariants } from '@/src/components/shadcn/button'
 import { useMultiStageStore } from '@/src/components/Shared/MultiStageProgress/MultiStageStoreProvider'
+import Tooltip from '@/src/components/Shared/Tooltip'
 
 export function MultiStageNextButton({
   ...props
@@ -11,9 +12,13 @@ export function MultiStageNextButton({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const { nextStage, stage, stages, enabled } = useMultiStageStore((store) => store)
+  const { nextStage, stage, stages, enabled, reason } = useMultiStageStore((store) => store)
 
-  return <Button disabled={stage === stages.length || !enabled} {...props} type='button' onClick={() => nextStage()} />
+  return (
+    <Tooltip isDisabled={enabled} showsError content={reason}>
+      <Button disabled={stage === stages.length || !enabled} {...props} type='button' onClick={() => nextStage()} />
+    </Tooltip>
+  )
 }
 
 export function MultiStageBackButton({
@@ -22,7 +27,11 @@ export function MultiStageBackButton({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
   }) {
-  const { previousStage, stage, enabled } = useMultiStageStore((store) => store)
+  const { previousStage, stage, enabled, reason } = useMultiStageStore((store) => store)
 
-  return <Button {...props} disabled={stage === 1 || !enabled} type='button' onClick={() => previousStage()} />
+  return (
+    <Tooltip isDisabled={enabled} showsError content={reason}>
+      <Button {...props} disabled={stage === 1 || !enabled} type='button' onClick={() => previousStage()} />
+    </Tooltip>
+  )
 }
