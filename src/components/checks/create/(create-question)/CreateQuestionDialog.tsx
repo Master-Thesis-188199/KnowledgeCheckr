8cfb3972599991b87030861573a0df8c8969965a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-04 08:50:27 +0100

    ref: applied `Field` to `CreateQuestionDialog`
    This way all previous usages of the `Input` component were replaced with the new `Field` component.

--- src/components/checks/create/(create-question)/CreateQuestionDialog.tsx
+++ src/components/checks/create/(create-question)/CreateQuestionDialog.tsx
@@ -6,16 +6,18 @@ import isEmpty from 'lodash/isEmpty'
 import isEqual from 'lodash/isEqual'
 import startCase from 'lodash/startCase'
 import { ArrowDown, ArrowUp, Check, Plus, Trash2, X } from 'lucide-react'
-import { FormState, useFieldArray, UseFieldArrayReturn, useForm, UseFormReturn } from 'react-hook-form'
+import { FormState, useFieldArray, UseFieldArrayReturn, useForm, useFormContext, UseFormReturn } from 'react-hook-form'
 import { twMerge } from 'tailwind-merge'
 import { useCheckStore } from '@/components/checks/create/CreateCheckProvider'
 import { Button } from '@/src/components/shadcn/button'
+import { Form } from '@/src/components/shadcn/form'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/src/components/Shared/Dialog'
+import Field from '@/src/components/Shared/form/Field'
 import FieldError from '@/src/components/Shared/form/FormFieldError'
-import Input from '@/src/components/Shared/form/Input'
 import { default as CreateableSelect, default as Select } from '@/src/components/Shared/form/Select'
 import Tooltip from '@/src/components/Shared/Tooltip'
 import { getUUID } from '@/src/lib/Shared/getUUID'
+import { cn } from '@/src/lib/Shared/utils'
 import {
   ChoiceQuestion,
   DragDropQuestion,
@@ -70,6 +72,11 @@ export default function CreateQuestionDialog({ children, initialValues }: { chil
   const computeFormDefaults = useCallback(() => (initialValues === undefined || isEmpty(initialValues) ? generateQuestionDefaults('drag-drop') : initialValues), [initialValues])
   const mode: 'edit' | 'create' = isEmpty(initialValues) ? 'create' : 'edit'
 
+  const form = useForm<Question>({
+    resolver: zodResolver(QuestionSchema),
+    defaultValues: computeFormDefaults(),
+  })
+
   const {
     register,
     handleSubmit,
@@ -80,10 +87,7 @@ export default function CreateQuestionDialog({ children, initialValues }: { chil
     control,
     setValue,
     getValues,
-  } = useForm<Question>({
-    resolver: zodResolver(QuestionSchema),
-    defaultValues: computeFormDefaults(),
-  })
+  } = form
 
   useEffect(() => {
     if (isEqual(getValues(), initialValues) || isEmpty(initialValues)) return
@@ -122,133 +126,120 @@ export default function CreateQuestionDialog({ children, initialValues }: { chil
         {children}
       </DialogTrigger>
       <DialogContent onClose={() => closeDialog()} onPointerDownOutside={() => closeDialog()} onEscapeKeyDown={() => closeDialog({ reset: true })} className='max-w-md' id='question-dialog'>
-        <form onSubmit={handleSubmit(onSubmit)} className='grid gap-6 py-1'>
-          <QuestionDialogHeader type={mode} />
-          <input {...register('id')} id='id' value={watch('id')} className='hidden' />
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className='grid gap-6 py-1'>
+            <QuestionDialogHeader type={mode} />
+            <input {...register('id')} id='id' value={watch('id')} className='hidden' />
 
-          <div className='grid items-center gap-2'>
-            <label htmlFor='question' className={twMerge(label_classes)}>
-              Question
-            </label>
-            <Input {...register('question')} id='question' placeholder='Formulate your question here' className='-ml-0.5 placeholder:text-[15px]' />
-            <FieldError field='question' errors={errors} />
-          </div>
-          <div className='flex items-baseline gap-x-12'>
             <div className='grid items-center gap-2'>
-              <label htmlFor='points' className={twMerge(label_classes)}>
-                Points
-              </label>
-              <Input
-                {...register('points', {
-                  setValueAs: (value: string) => Number(value),
-                })}
-                id='points'
-                type='number'
-                placeholder='How many points is this question worth?'
-                className='-ml-0.5 min-w-0 placeholder:text-[15px]'
-              />
-              <FieldError field='points' className='whitespace-nowrap' errors={errors} />
+              <Field form={form} name='question' placeholder='Formulate your question here' labelClassname={cn('text-base -ml-1', label_classes)} />
             </div>
-            <div className='grid items-center gap-2'>
-              <label htmlFor='type' className={twMerge(label_classes)}>
-                Question Type
-              </label>
-              <CreateableSelect
-                name='type'
-                defaultValue={{ label: watch('type').split('-').join(' '), value: watch('type') }}
-                onChange={(type) => {
-                  if (type !== watch('type')) {
-                    let defaults = generateQuestionDefaults(type as Any)
 
-                    if (mode === 'edit' && type === initialValues?.type && watch('type') === 'open-question') {
-                      //* Fill the initival values when swapping back to initial-edit-question and the values were lost because the user swapped to e.g. an open-question in between
-                      defaults = initialValues
-                    } else if ((type as Question['type']) !== 'open-question' && watch('type') !== 'open-question') {
-                      //* override the answer-options size to the previous size; e.g. when the prev. question had 3 answer-options and it is compatible the updated question-type will also show 3 options with the same values
-                      const previous = watch('answers').length
-                      if ((defaults as ChoiceQuestion | DragDropQuestion).answers.length !== previous) {
-                        console.debug(
-                          `Question-type changed ('${watch('type')}' --> '${type}'), overriding answer-options from previous compatible question-type from ${(defaults as ChoiceQuestion | DragDropQuestion).answers.length} to ${previous}`,
-                        )
-                        // @ts-expect-error potential type-mismatch the properties of (`answers.{i}`) might be of type DragDropQuestion while the defaults.answers could be of type ChoiceQuestion. Hence, it might include 'incorrect' / irrelevant props like position or correct.
-                        ;(defaults as ChoiceQuestion | DragDropQuestion).answers = Array.from({ length: previous }).map((_, i) => ({
-                          ...watch(`answers.${i}`),
-                        }))
+            <div className='flex items-baseline gap-x-12'>
+              <div className='grid items-center gap-2'>
+                <Field form={form} name='points' type='number' onChange={({ valueAsNumber }) => valueAsNumber} labelClassname={cn('text-base -ml-1', label_classes)} />
+              </div>
+              <div className='grid items-center gap-2'>
+                <label htmlFor='type' className={twMerge(label_classes)}>
+                  Question Type
+                </label>
+                <CreateableSelect
+                  name='type'
+                  defaultValue={{ label: watch('type').split('-').join(' '), value: watch('type') }}
+                  onChange={(type) => {
+                    if (type !== watch('type')) {
+                      let defaults = generateQuestionDefaults(type as Any)
+
+                      if (mode === 'edit' && type === initialValues?.type && watch('type') === 'open-question') {
+                        //* Fill the initival values when swapping back to initial-edit-question and the values were lost because the user swapped to e.g. an open-question in between
+                        defaults = initialValues
+                      } else if ((type as Question['type']) !== 'open-question' && watch('type') !== 'open-question') {
+                        //* override the answer-options size to the previous size; e.g. when the prev. question had 3 answer-options and it is compatible the updated question-type will also show 3 options with the same values
+                        const previous = watch('answers').length
+                        if ((defaults as ChoiceQuestion | DragDropQuestion).answers.length !== previous) {
+                          console.debug(
+                            `Question-type changed ('${watch('type')}' --> '${type}'), overriding answer-options from previous compatible question-type from ${(defaults as ChoiceQuestion | DragDropQuestion).answers.length} to ${previous}`,
+                          )
+                          // @ts-expect-error potential type-mismatch the properties of (`answers.{i}`) might be of type DragDropQuestion while the defaults.answers could be of type ChoiceQuestion. Hence, it might include 'incorrect' / irrelevant props like position or correct.
+                          ;(defaults as ChoiceQuestion | DragDropQuestion).answers = Array.from({ length: previous }).map((_, i) => ({
+                            ...watch(`answers.${i}`),
+                          }))
+                        }
+
+                        //* pre-fill answer-options based on previous-inputs when possible;  when the previous and new question-type has multiple answers
+                        defaults = { ...defaults, answers: (defaults as ChoiceQuestion | DragDropQuestion).answers.map((a, i) => ({ ...a, answer: watch(`answers.${i}.answer`) })) } as
+                          | ChoiceQuestion
+                          | DragDropQuestion
                       }
 
-                      //* pre-fill answer-options based on previous-inputs when possible;  when the previous and new question-type has multiple answers
-                      defaults = { ...defaults, answers: (defaults as ChoiceQuestion | DragDropQuestion).answers.map((a, i) => ({ ...a, answer: watch(`answers.${i}.answer`) })) } as
-                        | ChoiceQuestion
-                        | DragDropQuestion
+                      resetInputs({ ...defaults, id: watch('id'), question: watch('question'), points: watch('points'), category: watch('category') })
                     }
+                    register('type').onChange({ target: { value: type, name: 'type' } })
+                  }}
+                  options={[
+                    { label: 'Single Choice', value: 'single-choice' },
+                    { label: 'Multiple Choice', value: 'multiple-choice' },
+                    { label: 'Open Question', value: 'open-question' },
+                    { label: 'Drag Drop', value: 'drag-drop' },
+                  ]}
+                />
+                <FieldError field='type' errors={errors} />
+              </div>
+              <div className='grid items-center gap-2'>
+                <label htmlFor='access' className={twMerge(label_classes)}>
+                  Accessibility
+                </label>
 
-                    resetInputs({ ...defaults, id: watch('id'), question: watch('question'), points: watch('points'), category: watch('category') })
-                  }
-                  register('type').onChange({ target: { value: type, name: 'type' } })
-                }}
-                options={[
-                  { label: 'Single Choice', value: 'single-choice' },
-                  { label: 'Multiple Choice', value: 'multiple-choice' },
-                  { label: 'Open Question', value: 'open-question' },
-                  { label: 'Drag Drop', value: 'drag-drop' },
-                ]}
-              />
-              <FieldError field='type' errors={errors} />
+                <Select
+                  selectTriggerClassname='-ml-0.5 min-w-36'
+                  popoverContentClassname='w-[170px]'
+                  onChange={(accessibility) => register('accessibility').onChange({ target: { value: accessibility, name: 'accessibility' } })}
+                  options={[
+                    { label: 'Both', value: 'all' },
+                    { label: 'Practice only', value: 'practice-only' },
+                    { label: 'Exam only', value: 'exam-only' },
+                  ]}
+                  defaultValue={{
+                    label: watch('accessibility') === 'all' ? 'Both' : watch('accessibility').replace('-', ' ').split(' ').map(startCase).join(' '),
+                    value: watch('accessibility'),
+                  }}
+                />
+
+                <FieldError field='accessibility' className='whitespace-nowrap' errors={errors} />
+              </div>
             </div>
             <div className='grid items-center gap-2'>
-              <label htmlFor='access' className={twMerge(label_classes)}>
-                Accessibility
+              <label htmlFor='category' className={twMerge(label_classes)}>
+                Category
+              </label>
+              <Select
+                selectTriggerClassname='-ml-0.5'
+                popoverContentClassname='w-[470px]'
+                onChange={(category) => register('category').onChange({ target: { value: category, name: 'category' } })}
+                options={[...questionCategories.map((cat) => ({ label: cat.name, value: cat.name }))]}
+                createable
+                defaultValue={{ label: watch('category'), value: watch('category') }}
+              />
+              <FieldError field='category' errors={errors} />
+            </div>
+            <div className='grid items-center gap-2' id='question-answers'>
+              <label htmlFor='answers' className={twMerge(label_classes)}>
+                Answers
               </label>
 
-              <Select
-                selectTriggerClassname='-ml-0.5 min-w-36'
-                popoverContentClassname='w-[170px]'
-                onChange={(accessibility) => register('accessibility').onChange({ target: { value: accessibility, name: 'accessibility' } })}
-                options={[
-                  { label: 'Both', value: 'all' },
-                  { label: 'Practice only', value: 'practice-only' },
-                  { label: 'Exam only', value: 'exam-only' },
-                ]}
-                defaultValue={{
-                  label: watch('accessibility') === 'all' ? 'Both' : watch('accessibility').replace('-', ' ').split(' ').map(startCase).join(' '),
-                  value: watch('accessibility'),
-                }}
-              />
-
-              <FieldError field='accessibility' className='whitespace-nowrap' errors={errors} />
+              <AnswerOptions control={control} watch={watch} register={register} errors={errors} setValue={setValue} />
             </div>
-          </div>
-          <div className='grid items-center gap-2'>
-            <label htmlFor='category' className={twMerge(label_classes)}>
-              Category
-            </label>
-            <Select
-              selectTriggerClassname='-ml-0.5'
-              popoverContentClassname='w-[470px]'
-              onChange={(category) => register('category').onChange({ target: { value: category, name: 'category' } })}
-              options={[...questionCategories.map((cat) => ({ label: cat.name, value: cat.name }))]}
-              createable
-              defaultValue={{ label: watch('category'), value: watch('category') }}
-            />
-            <FieldError field='category' errors={errors} />
-          </div>
-          <div className='grid items-center gap-2' id='question-answers'>
-            <label htmlFor='answers' className={twMerge(label_classes)}>
-              Answers
-            </label>
-
-            <AnswerOptions control={control} watch={watch} register={register} errors={errors} setValue={setValue} />
-          </div>
-          <DialogFooter className='mt-4 grid grid-cols-2 gap-4'>
-            <Button size='lg' variant='outline' onClick={() => setDialogOpenState(false)} type='button'>
-              Cancel
-            </Button>
-            <Button size='lg' variant='primary' type='submit'>
-              {isSubmitting ? 'Processing' : mode === 'create' ? 'Add Question' : 'Update Question'}
-            </Button>
-            {errors.root && <div>{JSON.stringify(errors.root)}</div>}
-          </DialogFooter>
-        </form>
+            <DialogFooter className='mt-4 grid grid-cols-2 gap-4'>
+              <Button size='lg' variant='outline' onClick={() => setDialogOpenState(false)} type='button'>
+                Cancel
+              </Button>
+              <Button size='lg' variant='primary' type='submit'>
+                {isSubmitting ? 'Processing' : mode === 'create' ? 'Add Question' : 'Update Question'}
+              </Button>
+              {errors.root && <div>{JSON.stringify(errors.root)}</div>}
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
@@ -289,6 +280,7 @@ function AnswerOptions(options: AnswerOptionProps) {
 }
 
 function ChoiceQuestionAnswers({ control, watch, register, errors }: AnswerOptionProps) {
+  const form = useFormContext()
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'answers',
@@ -308,7 +300,7 @@ function ChoiceQuestionAnswers({ control, watch, register, errors }: AnswerOptio
                 <Check className={twMerge('size-5 text-green-500 dark:text-green-500', !(watch(`answers.${index}`) as unknown as ChoiceQuestion['answers'][number]).correct && 'hidden')} />
                 <X className={twMerge('size-5 text-red-400 dark:text-red-400/80', (watch(`answers.${index}`) as unknown as ChoiceQuestion['answers'][number]).correct && 'hidden')} />
               </CircleAnswer_IndicatorInput>
-              <Input {...register(`answers.${index}.answer` as const)} placeholder={`Answer ${index + 1} -  to your question`} className='-ml-0.5 flex-1 placeholder:text-[15px]' />
+              <Field form={form} name={`answers.${index}.answer` as const} className='flex-1' placeholder={`Answer ${index + 1} -  to your question`} showLabel={false} />
               <DeleteAnswerOptionButton index={index} remove={remove} />
             </div>
             {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
@@ -353,16 +345,19 @@ function AddAnswerButton({ type, watch, append }: { append: UseFieldArrayReturn<
   )
 }
 
-function OpenQuestionAnswers({ register, errors }: AnswerOptionProps) {
+function OpenQuestionAnswers({ errors }: AnswerOptionProps) {
+  const form = useFormContext()
+
   return (
     <>
-      <Input {...register('expectation')} id='expectation' placeholder='What answer are you looking expecting' className='-ml-0.5 placeholder:text-[15px]' />
+      <Field form={form} name='expectation' id='expectation' placeholder='What answer are you looking expecting' showLabel={false} />
       <FieldError<OpenQuestion> field='expectation' errors={errors} />
     </>
   )
 }
 
 function DragDropQuestionAnswers({ register, errors, control, watch, setValue }: AnswerOptionProps) {
+  const form = useFormContext()
   const { fields, append, remove, move } = useFieldArray({
     control,
     name: 'answers',
@@ -382,7 +377,7 @@ function DragDropQuestionAnswers({ register, errors, control, watch, setValue }:
                 registerProps={[`answers.${index}.position` as const, { valueAsNumber: true }]}>
                 <span className='field-sizing-content text-center outline-0'>{index + 1}</span>
               </CircleAnswer_IndicatorInput>
-              <Input {...register(`answers.${index}.answer` as const)} placeholder={`Moveable exemplary Answer ${index + 1}`} className='-ml-0.5 flex-1 placeholder:text-[15px]' />
+              <Field form={form} name={`answers.${index}.answer` as const} className='flex-1' showLabel={false} />
               <div className='flex gap-2'>
                 <button
                   aria-label='move answer up'
