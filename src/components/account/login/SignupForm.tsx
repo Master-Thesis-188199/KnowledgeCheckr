bc40eb3be61a46a2924a9c70e0ccad1fc6cb7e45
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-04 07:02:53 +0100

    ref: applied `Field` into `SignupForm`
    This way the `SignupForm` matches the appearance of the `LoginForm` and `GeneralSection`.

--- src/components/account/login/SignupForm.tsx
+++ src/components/account/login/SignupForm.tsx
@@ -7,8 +7,9 @@ import Link from 'next/link'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
 import { z } from 'zod'
+import { Form } from '@/src/components/shadcn/form'
+import Field from '@/src/components/Shared/form/Field'
 import FormFieldError from '@/src/components/Shared/form/FormFieldError'
-import Input from '@/src/components/Shared/form/Input'
 import { cn } from '@/src/lib/Shared/utils'
 import { SignupSchema } from '@/src/schemas/AuthenticationSchema'
 import { signup } from '../../../lib/account/login/AccountActions'
@@ -18,22 +19,19 @@ type FormValues = z.infer<typeof SignupSchema>
 export default function SignupForm({ callbackUrl }: { callbackUrl?: string }) {
   const [state, formAction] = useActionState(signup, { success: false, values: { callbackUrl: undefined } })
 
-  const {
-    register,
-    trigger,
-    setError,
-    formState: { errors, isValid },
-    reset,
-  } = useForm<FormValues>({
+  const form = useForm<FormValues>({
     resolver: zodResolver(SignupSchema),
     delayError: 150,
     mode: 'onChange',
     defaultValues: {
-      name: state.values?.name,
-      email: state.values?.email,
-      password: state.values?.password,
+      name: state.values?.name ?? '',
+      email: state.values?.email ?? '',
+      password: state.values?.password ?? '',
     },
   })
+
+  // prettier-ignore
+  const { trigger, setError, formState: { errors, isValid }, reset } = form
 
   useEffect(() => {
     if (state.fieldErrors) {
@@ -68,57 +66,47 @@ export default function SignupForm({ callbackUrl }: { callbackUrl?: string }) {
   }, [state.success, reset])
 
   return (
-    <form
-      id='signup-form'
-      noValidate
-      action={formAction}
-      onSubmit={async (e) => {
-        const ok = await trigger()
-        if (!ok) e.preventDefault()
-      }}
-      className='flex flex-col gap-6'>
-      <input className='hidden' name='callbackUrl' readOnly value={callbackUrl} />
+    <Form {...form}>
+      <form
+        id='signup-form'
+        noValidate
+        action={formAction}
+        onSubmit={async (e) => {
+          const ok = await trigger()
+          if (!ok) e.preventDefault()
+        }}
+        className='flex flex-col gap-3'>
+        <input className='hidden' name='callbackUrl' readOnly value={callbackUrl} />
 
-      <label className='flex flex-col gap-1'>
-        Username
-        <Input {...register('name')} className='input' defaultValue={state.values?.name} />
-        <FormFieldError field='name' errors={errors} />
-      </label>
+        <div className='grid items-baseline justify-baseline gap-3 p-2 *:odd:mt-4 *:odd:first:mt-0'>
+          <Field form={form} label='Username' name='name' placeholder='your username' type='text' />
+          <Field form={form} name='email' placeholder='user@email.com' type='email' />
+          <Field form={form} name='password' placeholder='your password' type='password' />
+        </div>
 
-      <label className='flex flex-col gap-1'>
-        Email
-        <Input {...register('email')} className='input' defaultValue={state.values?.email} />
-        <FormFieldError field='email' errors={errors} />
-      </label>
+        <FormFieldError field='root' errors={errors} className='-mt-2 -mb-4 text-center' />
 
-      <label className='flex flex-col gap-1'>
-        Password
-        <Input type='password' {...register('password')} className='input' defaultValue={state.values?.password} />
-        <FormFieldError field='password' errors={errors} />
-      </label>
-
-      <FormFieldError field='root' errors={errors} className='-mt-2 -mb-4 text-center' />
-
-      <div className='mt-2 flex flex-col items-center justify-center gap-3'>
-        <button
-          type='submit'
-          data-auth-provider='credential'
-          disabled={!isValid}
-          className={cn(
-            'mt-2 w-full max-w-xs self-center rounded-lg px-4 py-2 ring-1 outline-0',
-            'hover:cursor-pointer dark:hover:ring-[1.8px]',
-            'bg-neutral-100/80 shadow-sm shadow-neutral-400/70 ring-neutral-400/70 dark:bg-neutral-700/40 dark:shadow-inherit dark:ring-neutral-700',
-            'hover:ring-ring-hover dark:hover:ring-ring-hover hover:bg-neutral-50/80 hover:shadow-neutral-400 active:bg-neutral-200/90 active:ring-neutral-300 dark:hover:bg-neutral-700/70 dark:hover:shadow-inherit dark:active:bg-neutral-700/90 dark:active:ring-neutral-600',
-          )}>
-          Create account
-        </button>
-        <p className='text-sm text-neutral-400 dark:text-neutral-400/70'>
-          Already have an Account?
-          <Link href={{ pathname: '/account/login', query: { type: 'signin', referer: callbackUrl } }} className='px-2 text-neutral-600 hover:cursor-pointer hover:underline dark:text-neutral-200'>
-            Signin
-          </Link>
-        </p>
-      </div>
-    </form>
+        <div className='mt-2 flex flex-col items-center justify-center gap-3'>
+          <button
+            type='submit'
+            data-auth-provider='credential'
+            disabled={!isValid}
+            className={cn(
+              'mt-2 w-full max-w-xs self-center rounded-lg px-4 py-2 ring-1 outline-0',
+              'hover:cursor-pointer dark:hover:ring-[1.8px]',
+              'bg-neutral-100/80 shadow-sm shadow-neutral-400/70 ring-neutral-400/70 dark:bg-neutral-700/40 dark:shadow-inherit dark:ring-neutral-700',
+              'hover:ring-ring-hover dark:hover:ring-ring-hover hover:bg-neutral-50/80 hover:shadow-neutral-400 active:bg-neutral-200/90 active:ring-neutral-300 dark:hover:bg-neutral-700/70 dark:hover:shadow-inherit dark:active:bg-neutral-700/90 dark:active:ring-neutral-600',
+            )}>
+            Create account
+          </button>
+          <p className='text-sm text-neutral-400 dark:text-neutral-400/70'>
+            Already have an Account?
+            <Link href={{ pathname: '/account/login', query: { type: 'signin', referer: callbackUrl } }} className='px-2 text-neutral-600 hover:cursor-pointer hover:underline dark:text-neutral-200'>
+              Signin
+            </Link>
+          </p>
+        </div>
+      </form>
+    </Form>
   )
 }
