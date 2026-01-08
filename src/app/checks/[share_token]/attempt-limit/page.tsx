5129c711e8f7027e23daa70b3b9b82933cb10e18
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-08 20:50:12 +0100

    feat: created `[share-token]/attempt-limit` page
    Users are redirected to this page when they have exceeded the max-amount of examination attempts.

---
+++ src/app/checks/[share_token]/attempt-limit/page.tsx
@@ -0,0 +1,12 @@
+import PageHeading from '@/src/components/Shared/PageHeading'
+
+export default async function ExamAttemptLimitPage({ searchParams }: { searchParams: Promise<{ attemptLimit?: number }> }) {
+  const { attemptLimit } = await searchParams
+
+  return (
+    <div>
+      <PageHeading title='Limit reached' />
+      You have reached the maximum amount of Examination attempts{attemptLimit !== undefined ? `, which is ${attemptLimit}.` : '.'}
+    </div>
+  )
+}
