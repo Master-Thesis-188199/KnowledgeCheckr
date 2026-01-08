daf53d70b51eea8de7ee44a34ea1296812fd2b6e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-08 12:26:54 +0100

    ref: updated type declarations in `useLogger`

--- src/hooks/log/useLogger.ts
+++ src/hooks/log/useLogger.ts
@@ -1,19 +1,16 @@
 'use client'
 
-import { logClient } from '@/src/lib/log/LogAction'
 import { LoggerOptions } from '@/src/lib/log/type'
-
-type LogClientOptions = Parameters<typeof logClient>[0]
 
 /**
  * This hook exposes various log-level functions that can be used to send and log messages on the server. Note, these logging function cannot be used during the initial render, hence use with useEffects, functions, ...
  * @param context The optional context-prefix that shall be used for all messages.
  */
-export function useLogger(context?: LogClientOptions['context']) {
-  const info = (...messages: unknown[]) => sendClientLog({ level: 'info', context, messages })
-  const warn = (...messages: unknown[]) => sendClientLog({ level: 'warn', context, messages })
-  const error = (...messages: unknown[]) => sendClientLog({ level: 'error', context, messages })
-  const verbose = (...messages: unknown[]) => sendClientLog({ level: 'verbose', context, messages })
+export function useLogger(context?: LoggerOptions['context']) {
+  const info = (...messages: LoggerOptions['messages']) => sendClientLog({ level: 'info', context, messages })
+  const warn = (...messages: LoggerOptions['messages']) => sendClientLog({ level: 'warn', context, messages })
+  const error = (...messages: LoggerOptions['messages']) => sendClientLog({ level: 'error', context, messages })
+  const verbose = (...messages: LoggerOptions['messages']) => sendClientLog({ level: 'verbose', context, messages })
 
   return { info, warn, error, verbose }
 }
