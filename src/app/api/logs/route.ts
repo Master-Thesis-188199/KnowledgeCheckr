f2989153eb3162035d7ba63e8943f9740c371f3f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-08 13:24:09 +0100

    ref: added `capture-client-logs` flag to toggle client logs
    This way the collection of client-side logs can be disabled by setting the respective env-secret to false.

--- src/app/api/logs/route.ts
+++ src/app/api/logs/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import _logger from '@/src/lib/log/Logger'
 import { LogSchema } from '@/src/lib/log/type'
+import env from '@/src/lib/Shared/Env'
 import { Any } from '@/types'
 
 export const runtime = 'nodejs'
@@ -19,6 +20,8 @@ export async function POST(req: Request) {
   if (!parsed.success) {
     return NextResponse.json({ ok: false, error: 'Invalid logging options' }, { status: 400 })
   }
+
+  if (!env.CAPTURE_CLIENT_LOGS) return
 
   const { level, context, messages } = parsed.data
   const logger = _logger.createModuleLogger(context ? `client/${context}` : 'Client')
