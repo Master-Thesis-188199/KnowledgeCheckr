7a43909fd78b4684ff031f73d0387a10f68afda9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-08 14:38:47 +0100

    ref: added custom winston log level colors

--- src/lib/log/type.ts
+++ src/lib/log/type.ts
@@ -6,6 +6,14 @@ export const logLevels = {
   verbose: 3,
   debug: 4,
 }
+
+export const logLevelColors: { [key in LoggerLevels]: string } = {
+  error: 'bold red',
+  warn: 'bold yellow',
+  info: 'blue',
+  verbose: 'magenta',
+  debug: 'italic gray',
+}
 export type LoggerLevels = keyof typeof logLevels
 
 export const LogSchema = z.object({
