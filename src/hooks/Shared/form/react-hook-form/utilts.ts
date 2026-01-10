ee72897b95b5f147f1569223a79cef948f1529fd
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-10 09:21:45 +0100

    ref: joined server-field-errors when setting form errors
    The reason for this change is that: "Looping and calling setError per message overwrites previous errors"

--- src/hooks/Shared/form/react-hook-form/utilts.ts
+++ src/hooks/Shared/form/react-hook-form/utilts.ts
@@ -48,12 +48,8 @@ export function useServerValidationResults<TSchema extends z.ZodSchema>(
 
         if (!msgs) return
 
-        for (const msg of msgs) {
-          if (msg.length === 0) continue
-
-          // @ts-expect-error Expect key-of schema object not to satisfy `FieldPath` type
-          setError(key, { type: 'server', message: msg })
-        }
+        // @ts-expect-error Expect key-of schema object not to satisfy `FieldPath` type
+        setError(key, { type: 'server', message: msgs.join(' & ') })
       })
     }
 
