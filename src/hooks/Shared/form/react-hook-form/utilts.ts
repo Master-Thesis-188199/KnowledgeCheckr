cabf0015c695aeb0c38c5c52467017d14e42b24f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-10 08:10:42 +0100

    feat: created `useServerValidationResults` util function
    This utility hook essentially applies the server-validation-state `fieldErrors` if there are any to a given react-hook-form instance.

--- src/hooks/Shared/form/react-hook-form/utilts.ts
+++ src/hooks/Shared/form/react-hook-form/utilts.ts
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { DefaultValues, UseFormReturn } from 'react-hook-form'
 import { z } from 'zod'
 import { RHFBaseReturn, RHFServerAction, RHFServerState, UseRHFFormProps, UseRHFOptions } from '@/src/hooks/Shared/form/react-hook-form/type'
@@ -24,4 +25,45 @@ export function buildBaseReturn<TSchema extends z.ZodSchema>(form: UseFormReturn
     descriptions,
     baseFieldProps: { form, descriptions },
   }
+}
+
+/**
+ * This function listens to the server-validation-state and adds `fieldErrors` to the form when the validation fails. When the server-validation is successful it will reset the form.
+ * @param hasServerValidation Setting this value to false will effectively disable the functionality of this hook as it will exit each useEffect immediately.
+ * @param state The server-validation-state used to extract server-side-errors from to add them to the react-hook-form instance
+ * @param form.setErrror The `setError` function from the react-hook-form instance
+ * @param form.reset The `reset` function from the react-hook-form instance
+ */
+export function useServerValidationResults<TSchema extends z.ZodSchema>(
+  hasServerValidation: boolean,
+  state: RHFServerState<TSchema>,
+  { setError, reset }: {} & Pick<UseFormReturn<z.infer<TSchema>>, 'reset' | 'setError'>,
+) {
+  useEffect(() => {
+    if (!hasServerValidation) return
+
+    if (state.fieldErrors) {
+      Object.entries(state.fieldErrors).forEach(([_key, msgs]) => {
+        const key = _key as z.infer<TSchema>
+
+        if (!msgs) return
+
+        for (const msg of msgs) {
+          if (msg.length === 0) continue
+
+          setError(key, { type: 'server', message: msg })
+        }
+      })
+    }
+
+    if (state.rootError) {
+      setError('root', { type: 'server', message: state.rootError })
+    }
+  }, [state.fieldErrors, state.rootError, setError])
+
+  useEffect(() => {
+    if (!hasServerValidation) return
+
+    if (state.success) reset()
+  }, [state.success, reset])
 }
