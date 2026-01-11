ef895f8b8aa8716b7943035f1515d8aba44289d1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-11 09:09:55 +0100

    ref: externalized infinite rendering of KnowledgeChecks
    This way the rendering of "infinite" KnowledgeChecks in a grid can be re-used.

---
+++ src/components/checks/RenderInfiniteChecks.tsx
@@ -0,0 +1,26 @@
+import { LoaderCircleIcon } from 'lucide-react'
+import { KnowledgeCheckCard } from '@/src/components/checks/KnowledgeCheckCard'
+import { InfiniteScrollProvider, InfinityScrollFetcher, InfinityScrollFetcherProps, InfinityScrollRenderer } from '@/src/components/Shared/InfiniteScroll'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+
+/**
+ * This component renders a given set of (KnowledgeCheck) initialItems in a grid.
+ * When the user scrolls to the bottom of the grid, new items will be fetched and appended using the `fetchNewItems` function.
+ * @param props.initialItems The initial set of items that are usually fetched on the server that are to be shown.
+ * @param props.fetchNewItems The function used to retrieve new items (on the client)
+ */
+export default function InfiniteKnowledgeCheckGrid({ initialItems, fetchNewItems }: { initialItems: KnowledgeCheck[]; fetchNewItems: InfinityScrollFetcherProps['getItems'] }) {
+  return (
+    <InfiniteScrollProvider initialItems={initialItems}>
+      <div className='checks grid grid-cols-[repeat(auto-fill,minmax(380px,1fr))] gap-8'>
+        <InfinityScrollRenderer<KnowledgeCheck> component={KnowledgeCheckCard} />
+      </div>
+      <InfinityScrollFetcher getItems={fetchNewItems}>
+        <div className='mt-8 flex justify-center gap-2'>
+          <LoaderCircleIcon className='animate-spin' />
+          Loading...
+        </div>
+      </InfinityScrollFetcher>
+    </InfiniteScrollProvider>
+  )
+}
