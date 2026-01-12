d2c230633102b622eb13f609fd76b3df0a63371c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-12 07:25:06 +0100

    ref: updated `RenderInfChecks` to accept all props

--- src/components/checks/RenderInfiniteChecks.tsx
+++ src/components/checks/RenderInfiniteChecks.tsx
@@ -1,21 +1,26 @@
 import { KnowledgeCheckCard } from '@/src/components/checks/KnowledgeCheckCard'
 import { InfiniteScrollProvider, InfinityScrollFetcher, InfinityScrollFetcherProps, InfinityScrollRenderer } from '@/src/components/Shared/InfiniteScroll'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+import { Any } from '@/types'
 
 /**
  * This component renders a given set of (KnowledgeCheck) initialItems in a grid.
  * When the user scrolls to the bottom of the grid, new items will be fetched and appended using the `fetchNewItems` function.
  * @param props.initialItems The initial set of items that are usually fetched on the server that are to be shown.
- * @param props.fetchNewItems The function used to retrieve new items (on the client)
  */
-export default function InfiniteKnowledgeCheckGrid({ initialItems, fetchNewItems }: { initialItems: KnowledgeCheck[]; fetchNewItems: InfinityScrollFetcherProps['getItems'] }) {
+export default function InfiniteKnowledgeCheckGrid<TFunc extends (...args: Any) => Any>({
+  initialItems,
+  ...props
+}: {
+  initialItems: KnowledgeCheck[]
+} & InfinityScrollFetcherProps<TFunc>) {
   return (
     <InfiniteScrollProvider initialItems={initialItems}>
       <div className='checks grid grid-cols-[repeat(auto-fill,minmax(380px,1fr))] gap-8'>
         <InfinityScrollRenderer<KnowledgeCheck> component={KnowledgeCheckCard} />
       </div>
 
-      <InfinityScrollFetcher getItems={fetchNewItems} disabled={initialItems.length === 0} loadingLabel={'Loading more checks...'} />
+      <InfinityScrollFetcher {...props} loadingLabel={'Loading more checks...'} />
     </InfiniteScrollProvider>
   )
 }
