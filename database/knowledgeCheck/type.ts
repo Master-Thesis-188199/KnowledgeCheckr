53bb06ea7f03c8e2ed8a57b756ec833f4f1c9f28
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-12 12:18:44 +0100

    feat: allowed `DBOptions` arg to be optional
    That way a function can be defined both `function getOranges(id: string, options: {limit?: number, offset?: number} = {})`, `function getOranges(id: string, options: {limit?: number, offset?: number})` and `function getOranges( options: {limit?: number, offset?: number} = {})`. In other words, the options arguments that (which must be the last argument) can be both optional (initialized as {} by default) or required `(..., opt: {limit?: number, offset?: number})`.

--- database/knowledgeCheck/type.ts
+++ database/knowledgeCheck/type.ts
@@ -1,3 +1,4 @@
+import { getKnowledgeChecksByOwner, getPublicKnowledgeChecks } from '@/database/knowledgeCheck/select'
 import { Any } from '@/types'
 
 export interface DbKnowledgeCheck {
@@ -30,3 +31,48 @@ export type EnforceLastDbOptions<TFunc extends (...args: Any[]) => Promise<Any>>
         : TFunc // disallow `options?: ...` or `options: ... | undefined`
       : never
     : never
+
+//* type enforcment tests
+
+get(getPublicKnowledgeChecks)
+get(getPublicOptions)
+get(getPublic)
+//@ts-expect-error Expect to detect missing options...
+get(getPublicNoOptions)
+get(getKnowledgeChecksByOwner)
+get(getPublic3)
+get(getPublicOptionalOptions)
+
+type Options = { limit?: number; offset?: number }
+type MissingOptionsLastError<T extends Any> = T & {
+  __error__: 'Function must have `Options` as its LAST parameter'
+}
+
+type IsOptionsLike<T> = T extends { limit?: infer L; offset?: infer O } ? ([L] extends [number | undefined] ? ([O] extends [number | undefined] ? true : false) : false) : false
+
+// Validate last param (required or optional) is Options-like
+type RequireOptionsLast<F extends (...args: Any[]) => Any> =
+  Parameters<F> extends [...infer _Head, infer Last]
+    ? IsOptionsLike<NonNullable<Last>> extends true
+      ? object // ok
+      : MissingOptionsLastError<Parameters<F>>
+    : MissingOptionsLastError<Any>
+
+function get<F extends (...args: Any[]) => Any>(func: F & RequireOptionsLast<F>) {
+  return (...args: Parameters<F>): ReturnType<F> => func(...args)
+}
+async function getPublicOptions({ limit = 10, offset = 0 }: DatabaseOptions) {
+  return Promise.resolve({})
+}
+async function getPublicNoOptions(someValue: string) {
+  return Promise.resolve({})
+}
+async function getPublicOptionalOptions(someValue: string, opt: DatabaseOptions = {}) {
+  return Promise.resolve({})
+}
+async function getPublic(userId: string, { limit = 10, offset = 0 }: DatabaseOptions) {
+  return Promise.resolve({})
+}
+async function getPublic3(userId: string, more: string, { limit = 10, offset = 0 }: DatabaseOptions) {
+  return Promise.resolve({})
+}
