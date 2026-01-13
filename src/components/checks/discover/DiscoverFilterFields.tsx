aaeaa802fd075b1d09dffbbb187d9f3272a4ee67
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-13 16:34:39 +0100

    feat: added case-sensitivity toggle to `DiscovFilterFields`

--- src/components/checks/discover/DiscoverFilterFields.tsx
+++ src/components/checks/discover/DiscoverFilterFields.tsx
@@ -2,13 +2,15 @@
 
 import { useCallback, useState } from 'react'
 import isEqual from 'lodash/isEqual'
-import { FilterIcon, PlusIcon } from 'lucide-react'
+import { CaseSensitiveIcon, FilterIcon, PlusIcon } from 'lucide-react'
 import Link from 'next/link'
 import { StringOps } from '@/database/utils/buildWhere'
 import { useDiscoverFilterOptionsContext } from '@/src/components/checks/discover/DiscoverFilterOptionsProvider'
 import { Button } from '@/src/components/shadcn/button'
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/src/components/shadcn/dropdown-menu'
 import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from '@/src/components/shadcn/input-group'
+import { Toggle } from '@/src/components/shadcn/toggle'
+import Tooltip from '@/src/components/Shared/Tooltip'
 
 export function DiscoverFilterFields() {
   const { filter, setFuncProps } = useDiscoverFilterOptionsContext()
@@ -16,10 +18,11 @@ export function DiscoverFilterFields() {
   const filterOperands = ['contains' as const, 'startsWith' as const, 'endsWith' as const, 'eq' as const]
   const [selectedFilterOperand, setFilterOperand] = useState<(typeof filterOperands)[number]>('startsWith')
   const [nameFilterValue, setNameFilterValue] = useState<string | undefined>(undefined)
+  const [isCaseSensitive, setIsCaseSensitive] = useState(true)
 
   const updateFilter = useCallback(
     (op: typeof selectedFilterOperand, value?: Extract<StringOps, { op: 'contains' }>['value']) => {
-      const filters: typeof filter = value?.trim() ? { name: { value, op } } : undefined
+      const filters: typeof filter = value?.trim() ? { name: { value, op, ignoreCasing: !isCaseSensitive } } : undefined
 
       if (filter === filters || isEqual(filter, filters)) return console.log('aborting props update, equal...')
 
@@ -32,7 +35,7 @@ export function DiscoverFilterFields() {
         return update as typeof prev
       })
     },
-    [filter, setFuncProps],
+    [filter, setFuncProps, isCaseSensitive],
   )
 
   return (
@@ -50,7 +53,7 @@ export function DiscoverFilterFields() {
           <InputGroupAddon>
             <FilterIcon />
           </InputGroupAddon>
-          <InputGroupAddon align='inline-end'>
+          <InputGroupAddon align='inline-end' className='gap-1'>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <InputGroupButton variant='ghost' className='rounded-l-none border-l-2 border-l-neutral-500'>
@@ -75,6 +78,23 @@ export function DiscoverFilterFields() {
                 ))}
               </DropdownMenuContent>
             </DropdownMenu>
+            <Tooltip offset={12} placement='right' size='sm' content={isCaseSensitive ? 'filter is case-sensitive' : 'ignoring casing when filtering'}>
+              <Toggle
+                aria-label='Toggle bookmark'
+                size='xs'
+                onPressedChange={(pressed) => {
+                  setIsCaseSensitive(pressed)
+                  updateFilter(selectedFilterOperand, nameFilterValue)
+                }}
+                defaultPressed={isCaseSensitive}
+                variant='outline'
+                className='group data-[state=on]:bg-background-z-1 relative mr-0.5 data-[state=off]:*:[div]:block data-[state=on]:*:[div]:hidden data-[state=off]:*:[svg]:stroke-neutral-500 data-[state=on]:*:[svg]:stroke-neutral-700 dark:data-[state=off]:*:[svg]:stroke-neutral-400 dark:data-[state=on]:*:[svg]:stroke-neutral-300'>
+                <div className='absolute top-0 right-1 bottom-0 left-1 flex'>
+                  <div className='mt-2.5 h-[1.5px] w-full rotate-325 rounded-xs bg-red-500/50 dark:bg-red-400/70' />
+                </div>
+                <CaseSensitiveIcon className='size-5' />
+              </Toggle>
+            </Tooltip>
           </InputGroupAddon>
         </InputGroup>
 
