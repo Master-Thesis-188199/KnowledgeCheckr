2184a15e1a8468a363057d4cb77bd9a9910c0e69
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-13 07:02:50 +0100

    ref: moved `InfScrollFetcher` usage in `/discover` to client-side
    This way the `InfinityScrollFetcher` will re-fetch items using the modified `fetchProps` when the respective `fetchProps` change.

---
+++ src/components/checks/discover/DiscoverDynamicFilterFetcher.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { getPublicKnowledgeChecks } from '@/database/knowledgeCheck/select'
+import { useDiscoverFilterOptionsContext } from '@/src/components/checks/discover/DiscoverFilterOptionsProvider'
+import { InfinityScrollFetcher } from '@/src/components/Shared/InfiniteScroll'
+
+export default function DiscoverDynamicFilterFetcher() {
+  const { setFuncProps: setFilter, ...filter } = useDiscoverFilterOptionsContext()
+  return <InfinityScrollFetcher fetchItems={getPublicKnowledgeChecks} fetchProps={[filter]} loadingLabel={'Loading more checks...'} />
+}
