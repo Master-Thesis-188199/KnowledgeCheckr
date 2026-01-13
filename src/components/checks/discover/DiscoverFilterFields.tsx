7e8ead0035a8ad284d48f7a4c3c1a18273353036
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-13 07:01:07 +0100

    feat: drafted `DiscoverFilterFields` component
    This component essentially renders the exemplary input, filter-button and create-check button within a separate component. This way the `DiscoverContext` can be accessed and updated when the input changes.

---
+++ src/components/checks/discover/DiscoverFilterFields.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { FilterIcon, PlusIcon } from 'lucide-react'
+import Link from 'next/link'
+import { useDiscoverFilterOptionsContext } from '@/src/components/checks/discover/DiscoverFilterOptionsProvider'
+import { Button } from '@/src/components/shadcn/button'
+import { Input } from '@/src/components/shadcn/input'
+
+export function DiscoverFilterFields() {
+  const { filter, setFuncProps } = useDiscoverFilterOptionsContext()
+
+  return (
+    <div className='mb-4 flex justify-between'>
+      <Input
+        placeholder='Search for a specific KnowledgeCheck by name'
+        className='max-w-xl flex-1'
+        defaultValue={filter?.name && filter.name.op !== 'isNotNull' && filter.name.op !== 'isNull' ? filter.name?.value : ''}
+        onChange={(e) => {
+          setFuncProps((prev) => {
+            const update = {
+              ...prev,
+              filter: { name: { value: e.target.value, op: 'like' } },
+            }
+
+            return update as typeof prev
+          })
+        }}
+      />
+
+      <div className='flex gap-4'>
+        <Button variant='base'>
+          <FilterIcon className='size-4' />
+          Filter
+        </Button>
+        <Link href='/checks/create'>
+          <Button>
+            <PlusIcon className='size-5' />
+            Create Check
+          </Button>
+        </Link>
+      </div>
+    </div>
+  )
+}
