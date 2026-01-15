49272067d378e404061978980dc5b34b5eefca5a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-15 12:09:27 +0100

    feat: added select, insert dao handlers for collaborators

---
+++ database/knowledgeCheck/collaborators/insert.ts
@@ -0,0 +1,9 @@
+import { DrizzleDB } from '@/database/Database'
+import { db_userContributesToKnowledgeCheck } from '@/database/drizzle/schema'
+import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
+
+export async function insertCollaboratorsToKnowledgeCheck(db: DrizzleDB, checkId: KnowledgeCheck['id'], collaborators: KnowledgeCheck['collaborators']) {
+  const values: (typeof db_userContributesToKnowledgeCheck.$inferInsert)[] = collaborators.map((collaboratorId) => ({ knowledgecheckId: checkId, userId: collaboratorId }))
+
+  await db.insert(db_userContributesToKnowledgeCheck).values(values)
+}
