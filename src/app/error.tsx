0ba978ee421cd8492c126cc323c52c670e064be0
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-16 07:18:05 +0100

    ref: added overflow and scroll to `error` boundary
    This way when an error is shown with a large stack-trace in development, then the error message now no longer overflows without being scrollable.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -18,7 +18,7 @@ export default function RootErrorBoundary({ error }: { error: Error & { digest?:
     <div className='mb-10 flex h-full items-center justify-center'>
       <Card disableInteractions className={cn('flex max-w-xl flex-col gap-4', isDevelelopment && 'max-w-7xl')}>
         <h2 className='border-b border-neutral-400/70 pb-2 text-lg font-semibold text-red-500 dark:border-neutral-600 dark:text-red-400'>{error.name} </h2>
-        <div className={cn('px-2 text-red-600/90 dark:text-red-400/90', isProduction && 'hidden')}>
+        <div className={cn('max-h-[65vh] overflow-y-scroll px-2 text-red-600/90 dark:text-red-400/90', isProduction && 'hidden')}>
           <p className='mb-4'>{error.message}</p>
           {String(error.stack)
             .split('\n')
