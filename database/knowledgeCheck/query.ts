4719331650293beaaa30d8eb21103061b2da3b8a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-16 08:33:41 +0100

    ref: integrated settings filtering into knowCheck `query`
    This way one can use this `getKnowledgeCheck` utility function to query a given check but to also filter it based the settings associated with it.

--- database/knowledgeCheck/query.ts
+++ database/knowledgeCheck/query.ts
@@ -1,12 +1,16 @@
-import { and, BuildQueryResult, DBQueryConfig, ExtractTablesWithRelations } from 'drizzle-orm'
+import { BuildQueryResult, DBQueryConfig, eq, ExtractTablesWithRelations, SQL, sql } from 'drizzle-orm'
+import { alias } from 'drizzle-orm/mysql-core'
 import getDatabase from '@/database/Database'
-import { db_knowledgeCheck, DrizzleSchema } from '@/database/drizzle'
+import { db_knowledgeCheck, db_knowledgeCheckSettings, DrizzleSchema } from '@/database/drizzle'
 import { DatabaseOptions } from '@/database/knowledgeCheck/type'
 import buildWhere, { TableFilters } from '@/database/utils/buildWhere'
+import _logger from '@/src/lib/log/Logger'
 import { KnowledgeCheck, validateKnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 import { instantiateKnowledgeCheckSettings, KnowledgeCheckSettings, safeParseKnowledgeCheckSettings } from '@/src/schemas/KnowledgeCheckSettingsSchema'
 import { ChoiceQuestion, DragDropQuestion, OpenQuestion, Question } from '@/src/schemas/QuestionSchema'
 import { Any } from '@/types'
+
+const logger = _logger.createModuleLogger('/' + import.meta.url.split('/').reverse().slice(0, 2).reverse().join('/')!)
 
 //* joins the knowledgeCheck table with the following tables and aggregates the results: `Settings`, `Questions`, `Answers` and `Category`
 const knowledgeCheckWithAllConfig = {
@@ -46,13 +50,38 @@ type KnowledgeCheckTableConfig = Tables['db_knowledgeCheck']
 
 export type KnowledgeCheckWithAll = BuildQueryResult<Tables, KnowledgeCheckTableConfig, { with: typeof knowledgeCheckWithAllConfig }>
 
-export async function getKnowledgeCheck({ filter, limit = 10, offset }: { filter?: TableFilters<typeof db_knowledgeCheck> } & DatabaseOptions = {}) {
-  const clauses = buildWhere(db_knowledgeCheck, filter)
-
+export async function getKnowledgeCheck({
+  filter,
+  limit = 10,
+  offset,
+  settingsFilter,
+}: { filter?: TableFilters<typeof db_knowledgeCheck>; settingsFilter?: TableFilters<typeof db_knowledgeCheckSettings> } & DatabaseOptions = {}) {
   const db = await getDatabase()
 
   const checks = await db.query.db_knowledgeCheck.findMany({
-    where: and(clauses),
+    where: (kc, { and }) => {
+      const clauses: SQL[] = []
+
+      const rootWhere = buildWhere(db_knowledgeCheck, filter)
+      if (rootWhere) clauses.push(rootWhere)
+
+      if (settingsFilter) {
+        const kcs = alias(db_knowledgeCheckSettings, 'kcs')
+        const settingsWhere = buildWhere(kcs, settingsFilter)
+
+        if (settingsWhere) {
+          const sub = db
+            .select({ one: sql`1` })
+            .from(kcs)
+            .where(and(eq(kcs.knowledgecheckId, kc.id), settingsWhere))
+          logger.info(sub.toSQL())
+
+          clauses.push(sql`exists (${sub})`)
+        }
+      }
+
+      return clauses.length ? and(...clauses) : undefined
+    },
     with: knowledgeCheckWithAllConfig,
     orderBy: (kc, { desc }) => [desc(kc.updatedAt)],
     limit: limit,
