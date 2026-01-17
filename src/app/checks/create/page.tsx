ce4dc7d98a19bb065231a9ca3f790e413b2f11bb
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-17 10:45:57 +0100

    ref: defined `owner_id` in `ConfigureKnCheck`
    This way the `owner_id` is set to the current user's id by default, except when e.g. an existing check is being edited.

--- src/app/checks/create/page.tsx
+++ src/app/checks/create/page.tsx
@@ -1,8 +1,5 @@
 import { ConfigureKnowledgeCheck } from '@/src/components/checks/ConfigureKnowledgeCheck'
-import requireAuthentication from '@/src/lib/auth/requireAuthentication'
 
 export default async function CreateCheckPage() {
-  await requireAuthentication()
-
   return <ConfigureKnowledgeCheck mode='create' />
 }
