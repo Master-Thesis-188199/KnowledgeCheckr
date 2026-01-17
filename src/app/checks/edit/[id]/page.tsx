c451664814df85b2d599469a384cb30094878e2e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-17 11:30:41 +0100

    ref: updated `canEdit` condition to include collaborators
    This way the prepared `canEdit` variable is is also set to true when a given user is collaborators of a given check.

--- src/app/checks/edit/[id]/page.tsx
+++ src/app/checks/edit/[id]/page.tsx
@@ -13,7 +13,7 @@ export default async function CheckPage({ params }: { params: Promise<{ id: stri
     notFound()
   }
 
-  const canEdit = check.owner_id === user.id
+  const canEdit = check.owner_id === user.id || check.collaborators.includes(user.id)
   if (!canEdit) forbidden()
 
   return <ConfigureKnowledgeCheck mode='edit' initialStoreProps={check} options={{ cacheKey: 'check-exam-store', disableCache: true }} />
