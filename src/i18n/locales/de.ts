67a714fa2dfc270219b4fb922d4a696f8b7edd1a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-18 13:14:59 +0100

    ref: drafted /checks not-found translations

--- src/i18n/locales/de.ts
+++ src/i18n/locales/de.ts
@@ -2,4 +2,10 @@ export default {
   Home: {
     description: 'Create your own KnowledgeChecks to boost your knowledge to the next level. (de)',
   },
+
+  Checks: {
+    title: 'Deine Wissenschecks',
+    no_existing_checks: 'Keine Wissenschecks gefunden. Erstelle einen',
+    no_existing_checks_action_button: 'hier',
+  },
 } as const
--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -2,4 +2,9 @@ export default {
   Home: {
     description: 'Create your own KnowledgeChecks to boost your knowledge to the next level. (en)',
   },
+  Checks: {
+    title: 'Your Checks',
+    no_existing_checks: 'No checks found. Create a new one',
+    no_existing_checks_action_button: 'here',
+  },
 } as const
