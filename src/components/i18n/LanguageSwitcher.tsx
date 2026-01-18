376fbbbf346bb98f0b14abe2dd35b603ad38f167
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-18 11:02:06 +0100

    feat: created `LanguageSwitcher` client component

---
+++ src/components/i18n/LanguageSwitcher.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import DE from 'country-flag-icons/react/3x2/DE'
+import US from 'country-flag-icons/react/3x2/US'
+import { ChevronDownIcon, FileQuestionIcon } from 'lucide-react'
+import { Button } from '@/src/components/shadcn/button'
+import { Popover, PopoverContent, PopoverTrigger } from '@/src/components/Shared/Popover'
+import { useChangeLocale, useCurrentLocale } from '@/src/i18n/client-localization'
+import { i18nLocales } from '@/src/proxy'
+
+export default function LanguageSwitcher() {
+  const currentLocale = useCurrentLocale()
+  const { Icon } = useLocale(currentLocale, 'size-6')
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className='flex items-center' variant='input' size='sm'>
+          {Icon}
+          <ChevronDownIcon className='' />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-32 p-2 text-sm'>
+        <div className='grid gap-1'>
+          {i18nLocales.map((locale) => (
+            <LocaleButton key={locale} locale={locale} />
+          ))}
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+function LocaleButton({ locale }: { locale: (typeof i18nLocales)[number] }) {
+  const { Icon, long } = useLocale(locale)
+  const changeLocale = useChangeLocale()
+
+  return (
+    <Button type='button' variant='ghost' onClick={() => changeLocale(locale)} className='flex items-center gap-2.5 capitalize *:[svg]:size-5'>
+      {Icon}
+      <span>{long}</span>
+    </Button>
+  )
+}
+
+function useLocale(locale: (typeof i18nLocales)[number], defaultIconClass = 'size-5') {
+  switch (locale) {
+    case 'en':
+      return { Icon: <US title='United States' className={defaultIconClass} />, short: locale, long: 'English' }
+    case 'de':
+      return { Icon: <DE title='German' className={defaultIconClass} />, short: locale, long: 'German' }
+    default:
+      return { Icon: <FileQuestionIcon className={defaultIconClass} />, short: locale, long: 'Unknown' }
+  }
+}
