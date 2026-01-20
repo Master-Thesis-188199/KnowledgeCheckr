78b748ebe27f51c2f19168d59766db76c57856bc
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-20 13:41:41 +0100

    feat: updated `schemaUtilities` signature and exports
    The `schemaUtilities` function now has an updated signature and an additional overload. The newly introduced signature overload takes in a secondary argument, a drizzle table definition. When the drizzle table is provided as the second argument, then it returns an additional utility function `convertToDatabase` which will convert the given schema-object to the database-object as good as possible (for matching properties).

--- src/schemas/utils/schemaUtilities.ts
+++ src/schemas/utils/schemaUtilities.ts
@@ -1,7 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { MySqlTableWithColumns } from 'drizzle-orm/mysql-core'
 import { SafeParseReturnType, z } from 'zod'
 import schemaDefaults, { SchemaOptionalProps } from '@/schemas/utils/schemaDefaults'
 import { stripZodDefault } from '@/schemas/utils/stripZodDefaultValues'
+import createConvertToDatabase from '@/src/schemas/utils/createConvertToDatabase'
+import { Any } from '@/types'
+
+type SchemaUtilsBase<Schema extends z.ZodTypeAny> = {
+  validate: (obj: z.infer<Schema> | Any) => z.infer<Schema> | never
+  instantiate: (options?: SchemaOptionalProps) => z.infer<Schema>
+  safeParse: (obj: z.infer<Schema> | Any) => z.SafeParseReturnType<any, z.infer<Schema>>
+}
+
+type SchemaUtilsWithDb<Schema extends z.ZodTypeAny, Table extends MySqlTableWithColumns<Any>> = SchemaUtilsBase<Schema> & {
+  convertToDatabase: ReturnType<typeof createConvertToDatabase<Schema, Table>>
+}
+
+type SchemaUtilsWithoutDb<Schema extends z.ZodTypeAny> = SchemaUtilsBase<Schema>
+
+export function schemaUtilities<Schema extends z.ZodTypeAny>(schema: Schema): SchemaUtilsWithoutDb<Schema>
+export function schemaUtilities<Schema extends z.ZodTypeAny, Table extends MySqlTableWithColumns<Any>>(schema: Schema, table: Table): SchemaUtilsWithDb<Schema, Table>
 
 /**
  * A hook that provides utility functions for working with zod schemas
@@ -9,7 +27,7 @@ import { stripZodDefault } from '@/schemas/utils/stripZodDefaultValues'
  * @returns An object containing utility functions: getDummy, validate and safeParse based on the given schema
  * @internal
  */
-export function schemaUtilities<Schema extends z.ZodTypeAny>(schema: Schema) {
+export function schemaUtilities<Schema extends z.ZodTypeAny, Table extends MySqlTableWithColumns<Any>>(schema: Schema, table?: Table) {
   /**
    * Validates a given object against a given schema. Throws an error if the object is invalid
    * @param object - The object to be validated
@@ -30,9 +48,18 @@ export function schemaUtilities<Schema extends z.ZodTypeAny>(schema: Schema) {
    */
   const safeParse = (object: any): z.SafeParseReturnType<any, z.infer<Schema>> => stripZodDefault(schema).safeParse(object) as SafeParseReturnType<any, z.infer<Schema>>
 
-  return {
-    instantiate,
+  const base: SchemaUtilsBase<Schema> = {
     validate,
+    instantiate,
     safeParse,
+  }
+
+  if (!table) return base
+
+  const convertToDatabase = createConvertToDatabase(schema, table)
+
+  return {
+    ...base,
+    convertToDatabase,
   }
 }
