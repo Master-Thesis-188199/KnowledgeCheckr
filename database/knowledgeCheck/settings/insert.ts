f780d949f4e11729343f82509a07112256bc0dc2
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-20 09:05:10 +0100

    ref: applied settings `transform` handler to insert, select

--- database/knowledgeCheck/settings/insert.ts
+++ database/knowledgeCheck/settings/insert.ts
@@ -2,21 +2,20 @@
 
 import { DrizzleDB } from '@/database/Database'
 import { db_knowledgeCheckSettings } from '@/database/drizzle/schema'
+import { convertSettings } from '@/database/knowledgeCheck/settings/transform'
 import requireAuthentication from '@/src/lib/auth/requireAuthentication'
 import { KnowledgeCheck } from '@/src/schemas/KnowledgeCheck'
 
-export default async function insertKnowledgeCheckSettings(db: DrizzleDB, { id, settings: { examination: examSettings, ...settings } }: Pick<KnowledgeCheck, 'id' | 'settings'>) {
+export default async function insertKnowledgeCheckSettings(db: DrizzleDB, { id, settings }: Pick<KnowledgeCheck, 'id' | 'settings'>) {
   await requireAuthentication()
+
+  const dbSettings = convertSettings('to-database', settings)
 
   const [{ id: insertId }] = await db
     .insert(db_knowledgeCheckSettings)
     .values({
       knowledgecheckId: id,
-      ...settings,
-      ...examSettings,
-      allowAnonymous: examSettings.allowAnonymous ? 1 : 0,
-      allowFreeNavigation: examSettings.allowFreeNavigation ? 1 : 0,
-      shareAccessibility: settings.shareAccessibility ? 1 : 0,
+      ...dbSettings,
     })
     .$returningId()
 
