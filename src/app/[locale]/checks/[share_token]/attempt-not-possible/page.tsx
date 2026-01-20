6001266c5e41adc48f86a6b0a563d214dacc42f1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-20 07:01:38 +0100

    feat: created `/[share-token]/attempt-not-possible` page
    This page will be shown to users when users are not allowed to start an examination attempt, e.g. because the check is not open yet or already closed for examinations.

---
+++ src/app/[locale]/checks/[share_token]/attempt-not-possible/page.tsx
@@ -0,0 +1,32 @@
+import { isFuture } from 'date-fns/isFuture'
+import { isPast } from 'date-fns/isPast'
+import { notFound } from 'next/navigation'
+import { getKnowledgeCheckByShareToken } from '@/database/knowledgeCheck/select'
+import PageHeading from '@/src/components/Shared/PageHeading'
+import { getCurrentLocale, getScopedI18n } from '@/src/i18n/server-localization'
+import requireAuthentication from '@/src/lib/auth/requireAuthentication'
+
+export default async function ClosedExaminationPage({ params }: { params: Promise<{ share_token: string }> }) {
+  const { share_token } = await params
+  await requireAuthentication()
+
+  const check = await getKnowledgeCheckByShareToken(share_token)
+
+  if (!check) notFound()
+
+  const currentLocale = await getCurrentLocale()
+  const t = await getScopedI18n('Examination.attempt_not_possible')
+
+  let message: string = t('unavailable')
+
+  if (isFuture(check.openDate)) message = t('notOpenYet', { openDate: check.openDate.toLocaleDateString(currentLocale) })
+  if (check.closeDate !== null && isPast(check.closeDate)) message = t('checkClosed', { closeDate: check.closeDate.toLocaleDateString(currentLocale) })
+
+  return (
+    <>
+      <PageHeading title={t('title')} />
+
+      <p>{message}</p>
+    </>
+  )
+}
