da1f01e0c2e2338170c7ccaa7c581cb755434236
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-26 10:08:32 +0100

    ref: added translations to `CreateQuestionDialog`

--- src/i18n/locales/de.ts
+++ src/i18n/locales/de.ts
@@ -4,17 +4,23 @@ export default {
     navigation_button_next: 'Weiter',
     navigation_button_previous: 'Zurück',
     Question: {
+      question_label: 'Frage',
+      type_label: 'Art der Frage',
       type: {
         'multiple-choice': 'Mehrfach-auswahl',
         'single-choice': 'Einzel-auswahl',
         'open-question': 'Offene-Frage',
         'drag-drop': 'Drag-Drop'
       },
+      points_label: 'Punkte',
+      accessibility_label: 'Zugehörigkeit',
       accessibility: {
         all: 'Universal',
         'practice-only': 'Übungs Frage',
         'exam-only': 'Prüfungs Frage'
-      }
+      },
+      category_label: 'Kategorie',
+      answers_label: 'Antworten'
     }
   },
   Home: {
@@ -58,6 +64,30 @@ export default {
         questions: 'Fragen',
         settings: 'Einstellungen',
         conclusion: 'Zusammenfassung'
+      },
+      CreateQuestionDialog: {
+        Header: {
+          title_create: 'Frage erstellen',
+          description_create: 'Erstellen Sie eine neue Frage für diesen KnowledgeCheck',
+          title_edit: 'Frage bearbeiten',
+          description_edit: 'Bearbeiten Sie Ihre bestehende Frage Ihres KnowledgeChecks'
+        },
+        placeholders: {
+          question: 'Formuliere deine Frage hier',
+          choice_answer_option: 'Antwort {position} - dieser Frage',
+          open_question_expectation: 'Welche Antwort erwarten Sie von dieser Frage'
+        },
+        tooltips: {
+          choice_question_marked_correct: 'Anwort als richtig markiert',
+          choice_question_marked_incorrect: 'Antwort als falsch markiert',
+          drag_drop_correct_position: 'Die richtige position dieser Antwort'
+        },
+        buttons: {
+          cancel_button_label: 'Abbrechen',
+          add_submit_button_label: 'Frage erstellen',
+          update_submit_button_label: 'Frage aktualisieren',
+          add_answer_label: 'Weitere Antwort'
+        }
       }
     }
   },
