34646470f48e582473322329a8fb4f35b8f30427
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-28 20:34:41 +0100

    feat: introduce `DegradingScoreIndicator` component

---
+++ src/components/checks/DegradingScoreIndicator.tsx
@@ -0,0 +1,39 @@
+import { CircleIcon } from 'lucide-react'
+import Tooltip from '@/src/components/Shared/Tooltip'
+import { cn } from '@/src/lib/Shared/utils'
+
+/**
+ *
+ * @param knowledgeCheckScore A number between 0 and 100 where 100 means the user has likely retained 100% of the knowledge that he learned and had his exam about.
+ */
+export default function DegradingScoreIndicator({ knowledgeCheckScore }: { knowledgeCheckScore: number | undefined }) {
+  // very well retained, well-retained, good overview, mediocre, basic, lost
+  let level: '95-100' | '80-95' | '60-80' | '40-60' | '20-40' | '0-20' | 'none' = 'none'
+
+  if (knowledgeCheckScore === undefined) level = 'none'
+  else if (knowledgeCheckScore >= 95) level = '95-100'
+  else if (knowledgeCheckScore >= 80) level = '80-95'
+  else if (knowledgeCheckScore >= 60) level = '60-80'
+  else if (knowledgeCheckScore >= 40) level = '40-60'
+  else if (knowledgeCheckScore >= 20) level = '20-40'
+  else if (knowledgeCheckScore >= 0) level = '0-20'
+
+  return (
+    <Tooltip content='Estimated knowledge retainment'>
+      <div
+        className={cn(
+          'relative text-sm select-none dark:text-neutral-500/80',
+
+          level === '0-20' && 'dark:text-[oklch(55%_0.1_30)]',
+          level === '20-40' && 'dark:text-[oklch(50%_0.1_50)]',
+          level === '40-60' && 'dark:text-[oklch(50%_0.1_70)]',
+          level === '60-80' && 'dark:text-[oklch(50%_0.2_116)]',
+          level === '80-95' && 'dark:text-[oklch(45%_0.3_155)]',
+          level === '95-100' && 'dark:text-[oklch(55%_0.3_155)]',
+        )}>
+        <CircleIcon className={cn('size-10 stroke-[1.5px]', knowledgeCheckScore === undefined && 'stroke-1')} />
+        <span className='absolute inset-0 flex items-center justify-center'>{knowledgeCheckScore ?? 0}</span>
+      </div>
+    </Tooltip>
+  )
+}
