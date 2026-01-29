3c4e273fef1f5c263c280100ae59d313a9fd30f9
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-01-29 11:44:50 +0100

    feat: introduced `insert`, `update` practice dao handlers
    These handlers each provide one dao function to either insert or update a given practice results database entry. This way the practice results can be saved after the user answers a given question, to ensure that the most recent results are saved.

---
+++ database/practice/insert.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import getDatabase from '@/database/Database'
+import { db_userHasDoneKnowledgeCheck } from '@/database/drizzle'
+import { PracticeState } from '@/src/hooks/checks/[share_token]/practice/PracticeStore'
+import requireAuthentication from '@/src/lib/auth/requireAuthentication'
+import _logger from '@/src/lib/log/Logger'
+import { formatDatetime } from '@/src/lib/Shared/formatDatetime'
+
+const logger = _logger.createModuleLogger('/' + import.meta.url.split('/').reverse().slice(0, 2).reverse().join('/')!)
+
+export async function insertPracticeResults({
+  startedAt,
+  results,
+  ...values
+}: { results: PracticeState['answers']; startedAt: PracticeState['startedAt'] } & Omit<
+  typeof db_userHasDoneKnowledgeCheck.$inferInsert,
+  'startedAt' | 'results' | 'finishedAt' | 'id' | 'userId' | 'type'
+>) {
+  const {
+    user: { id: userId },
+  } = await requireAuthentication()
+
+  const db = await getDatabase()
+
+  logger.info(`Inserting practice data for user: ${userId}`)
+
+  await db.insert(db_userHasDoneKnowledgeCheck).values({
+    userId,
+    startedAt: formatDatetime(startedAt),
+    type: 'practice',
+    results: JSON.stringify(results),
+    finishedAt: formatDatetime(new Date(Date.now())),
+    ...values,
+  })
+}
---
+++ database/practice/update.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { and, eq } from 'drizzle-orm'
+import getDatabase from '@/database/Database'
+import { db_userHasDoneKnowledgeCheck } from '@/database/drizzle'
+import { PracticeState } from '@/src/hooks/checks/[share_token]/practice/PracticeStore'
+import requireAuthentication from '@/src/lib/auth/requireAuthentication'
+import _logger from '@/src/lib/log/Logger'
+import { formatDatetime } from '@/src/lib/Shared/formatDatetime'
+
+const logger = _logger.createModuleLogger('/' + import.meta.url.split('/').reverse().slice(0, 2).reverse().join('/')!)
+
+export async function updatePracticeResults({
+  startedAt,
+  results,
+  ...values
+}: { results: PracticeState['answers']; startedAt: PracticeState['startedAt'] } & Omit<
+  typeof db_userHasDoneKnowledgeCheck.$inferInsert,
+  'startedAt' | 'results' | 'finishedAt' | 'id' | 'userId' | 'type'
+>) {
+  const {
+    user: { id: userId },
+  } = await requireAuthentication()
+
+  const db = await getDatabase()
+
+  logger.info(`Updating practice results for user: ${userId}`)
+
+  const result = await db
+    .update(db_userHasDoneKnowledgeCheck)
+    .set({
+      results: JSON.stringify(results),
+      score: values.score,
+      finishedAt: formatDatetime(new Date(Date.now())),
+    })
+    .where(
+      and(
+        eq(db_userHasDoneKnowledgeCheck.knowledgeCheckId, values.knowledgeCheckId),
+        eq(db_userHasDoneKnowledgeCheck.startedAt, formatDatetime(startedAt)),
+        eq(db_userHasDoneKnowledgeCheck.userId, userId),
+      ),
+    )
+    .limit(1)
+
+  return { success: result[0].affectedRows === 1 }
+}
