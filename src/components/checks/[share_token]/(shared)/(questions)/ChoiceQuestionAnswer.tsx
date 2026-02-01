fba08a39bb44168c77f034226c0dc8adcb5fa7db
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-01 10:36:56 +0100

    ref: simplified `ChoiceQAnswer` args and registration
    Instead of registering the answer input manually without type-safety the `register` function is used instead with the respective key. That way, the onChange and default values are set automatically. Note that the value of the input is manually set to the answer-id so that the `selection` is set to the id and not the checked-state.

--- src/components/checks/[share_token]/(shared)/(questions)/ChoiceQuestionAnswer.tsx
+++ src/components/checks/[share_token]/(shared)/(questions)/ChoiceQuestionAnswer.tsx
@@ -1,37 +1,31 @@
 import { CircleIcon } from 'lucide-react'
+import { useFormContext, UseFormRegister } from 'react-hook-form'
 import { cn } from '@/src/lib/Shared/utils'
 import { ChoiceQuestion } from '@/src/schemas/QuestionSchema'
+import { QuestionInput } from '@/src/schemas/UserQuestionInputSchema'
 
-export default function DisplayChoiceQuestionAnswer({
+export default function DisplayChoiceQuestionAnswer<Type extends ChoiceQuestion['type']>({
   type,
-  answer,
-  index,
-  onChange,
-  defaultChecked,
+  answer: { answer, id },
   className,
   iconClassname,
   children,
+  registerKey,
 }: {
-  type: ChoiceQuestion['type']
-  index: number
-  answer: ChoiceQuestion['answers'][number]
-  onChange: (props: { selected: boolean; answer: ChoiceQuestion['answers'][number]; answerIndex: number }) => void
-  defaultChecked?: (props: { answer: ChoiceQuestion['answers'][number]; answerIndex: number }) => boolean | undefined
+  type: Type
+  answer: Pick<ChoiceQuestion['answers'][number], 'answer' | 'id'>
+  registerKey: Parameters<UseFormRegister<Extract<QuestionInput, { type: Type }>>>['0']
   className?: string
   iconClassname?: string
   children?: React.ReactNode
 }) {
+  const { register } = useFormContext<Extract<QuestionInput, { type: Type }>>()
+
   return (
     <label className={cn('flex items-center gap-2 hover:cursor-pointer', className)}>
-      <input
-        className='peer hidden'
-        type={type === 'multiple-choice' ? 'checkbox' : 'radio'}
-        name={type === 'single-choice' ? 'answer.correct' : (`answers.${index}.correct` as const)}
-        defaultChecked={defaultChecked ? defaultChecked({ answer, answerIndex: index }) : undefined}
-        onChange={(e) => onChange({ selected: e.target.checked, answer, answerIndex: index })}
-      />
+      <input className='peer hidden' type={type === 'multiple-choice' ? 'checkbox' : 'radio'} {...register(registerKey)} value={id} />
       <CircleIcon className={cn('size-4.5 peer-checked:fill-neutral-400 dark:peer-checked:fill-neutral-300', iconClassname)} />
-      {answer.answer}
+      {answer}
 
       {children}
     </label>
