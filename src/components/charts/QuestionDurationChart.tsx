e38c8feb04ba5e29fd106d4a9e8be2f247fef9a1
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-07 07:16:45 +0100

    style: improved exam duration chart tooltip styles
    Since the `chartConfig` used by the exam-question-duration chart has been cleared in the past, the colors used in front of labels did not use respective bg-colors. Thus, these colors were re-introduced. At the same time, this commit adds light variants for text-styles where needed.

--- src/components/charts/QuestionDurationChart.tsx
+++ src/components/charts/QuestionDurationChart.tsx
@@ -72,14 +72,7 @@ export function ExamQuestionDurationChart({ title, description, questions }: { t
                         <div className='flex flex-col gap-1.5'>
                           <div className='flex items-center justify-between gap-4'>
                             <div className='flex items-center gap-2'>
-                              <div
-                                className='h-2.5 w-2.5 shrink-0 rounded-[2px] bg-(--color-bg)'
-                                style={
-                                  {
-                                    '--color-bg': `var(--color-normal)`,
-                                  } as React.CSSProperties
-                                }
-                              />
+                              <div className={cn('h-2.5 w-2.5 shrink-0 rounded-[2px]', 'bg-chart-2')} />
                               Estimated Time
                             </div>
                             <div className='text-foreground flex flex-1 items-baseline justify-end gap-1 text-right font-mono font-medium tabular-nums'>
@@ -89,14 +82,7 @@ export function ExamQuestionDurationChart({ title, description, questions }: { t
                           </div>
                           <div className='flex items-center justify-between gap-4'>
                             <div className='flex items-center gap-2'>
-                              <div
-                                className='h-2.5 w-2.5 shrink-0 rounded-[2px] bg-(--color-bg)'
-                                style={
-                                  {
-                                    '--color-bg': `var(--color-anonymous)`,
-                                  } as React.CSSProperties
-                                }
-                              />
+                              <div className={cn('h-2.5 w-2.5 shrink-0 rounded-[2px]', 'bg-chart-3')} />
                               Time needed
                             </div>
                             <div className='text-foreground flex flex-1 items-baseline justify-end gap-1 text-right font-mono font-medium tabular-nums'>
@@ -107,11 +93,23 @@ export function ExamQuestionDurationChart({ title, description, questions }: { t
                         </div>
 
                         <div className='text-foreground mt-1.5 flex items-center border-t pt-1.5 text-xs font-medium'>
-                          {/* eslint-disable-next-line require-color-modes/require-color-mode-styles */}
-                          {item.payload.difference > 0 ? <span className='text-green-400'>Faster by</span> : <span className='text-red-400'>Slower by</span>}
+                          {item.payload.difference > 0 ? (
+                            <span className='text-[oklch(59.2%_0.309_151.711)] dark:text-[oklch(79.2%_0.209_151.711)]'>Faster by</span>
+                          ) : (
+                            // eslint-disable-next-line require-color-modes/require-color-mode-styles
+                            <span className='text-red-400'>Slower by</span>
+                          )}
                           <div className='text-foreground ml-auto flex items-baseline gap-0.5 font-mono font-medium tabular-nums'>
-                            <span className={cn(item.payload.difference > 0 ? 'text-green-400' : 'text-red-400')}>{Math.abs(item.payload.difference)}</span>
-                            <span className={cn('font-normal', item.payload.difference > 0 ? 'text-[oklch(79.2%_0.07_151.711)]' : 'text-[oklch(70.4%_0.07_22.216)]')}>minutes</span>
+                            <span className={cn(item.payload.difference > 0 ? 'text-[oklch(59.2%_0.309_151.711)] dark:text-[oklch(79.2%_0.209_151.711)]' : 'text-red-400')}>
+                              {Math.abs(item.payload.difference)}
+                            </span>
+                            <span
+                              className={cn(
+                                'font-normal',
+                                item.payload.difference > 0 ? 'text-[oklch(59.2%_0.309_151.711)]/50 dark:text-[oklch(79.2%_0.07_151.711)]' : 'text-[oklch(70.4%_0.07_22.216)]',
+                              )}>
+                              minutes
+                            </span>
                           </div>
                         </div>
                       </div>
