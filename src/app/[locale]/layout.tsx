9dcf8646ffc1c94c641147dd54b24c91748f2c77
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-10 13:27:35 +0100

    ref: updated locale root layout to be SSR
    Previously there was an issue with the i18n client provider and the retrieval of the current locale on the client side. This caused the page to never finish loading because a client-component ran async code, even though this was not true. While looking for the cause of this issue various changes were made in the hopes of resolving this issue. One of them was converting the layout to a client component to eliminate the awaiting of searchParams as the cause.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,7 @@
-'use client'
-
 import React from 'react'
 import { I18nProvider } from '@/src/components/root/I18nProvider'
 
-export default function LocaleRootLayout({ children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
-  // const { locale } = await params
-  return <I18nProvider locale={'en'}>{children}</I18nProvider>
+export default async function LocaleRootLayout({ children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+  return <I18nProvider locale={locale}>{children}</I18nProvider>
 }
