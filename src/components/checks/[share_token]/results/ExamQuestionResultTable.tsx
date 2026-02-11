904f198f763e8a58427ed6b6fd7aee354c846742
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-11 11:58:14 +0100

    feat: created exam question result table
    This table component uses the generic `DataTable` and specifies the columns and data to display within the table. This table is meant to list the questions of an examination attempt to allow administrators to view and grade answers.

---
+++ src/components/checks/[share_token]/results/ExamQuestionResultTable.tsx
@@ -0,0 +1,193 @@
+'use client'
+
+import { IconDotsVertical } from '@tabler/icons-react'
+import { ColumnDef } from '@tanstack/react-table'
+import { ChartColumnIcon, CheckIcon, ChevronRightIcon, EyeIcon, TrashIcon } from 'lucide-react'
+import { QuestionScoresLineChart } from '@/src/components/charts/QuestionScoresLineChart'
+import { Badge } from '@/src/components/shadcn/badge'
+import { Button } from '@/src/components/shadcn/button'
+import { Checkbox } from '@/src/components/shadcn/checkbox'
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/src/components/shadcn/drawer'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/src/components/shadcn/dropdown-menu'
+import { Input } from '@/src/components/shadcn/input'
+import { Label } from '@/src/components/shadcn/label'
+import { DataTable } from '@/src/components/Shared/Table/DataTable'
+import { useIsMobile } from '@/src/hooks/use-mobile'
+import { getUUID } from '@/src/lib/Shared/getUUID'
+import { Question } from '@/src/schemas/QuestionSchema'
+
+type QuestionItem = {
+  id: string
+  position: number
+  questionText: string
+  score: number
+  points: Question['points']
+  category: Question['category']
+}
+
+export default function ExamQuestionResultTable() {
+  const columns: ColumnDef<QuestionItem>[] = [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <div className='flex items-center justify-center'>
+          <Checkbox
+            checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && 'indeterminate')}
+            onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+            aria-label='Select all'
+          />
+        </div>
+      ),
+      cell: ({ row }) => (
+        <div className='flex items-center justify-center'>
+          <Checkbox checked={row.getIsSelected()} onCheckedChange={(value) => row.toggleSelected(!!value)} aria-label='Select row' />
+        </div>
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: 'position',
+      header: () => 'Pos',
+      cell: ({ row }) => (
+        <div className='text-foreground text-center' id={`${row.original.id}-duration`}>
+          {row.original.position}
+        </div>
+      ),
+    },
+    {
+      id: 'primary',
+      accessorKey: 'question',
+      header: 'Question',
+      cell: ({ row }) => {
+        return <div className='text-foreground text-left'>{row.original.questionText}</div>
+      },
+      enableHiding: false,
+    },
+
+    {
+      accessorKey: 'category',
+      header: () => <div className='w-full text-center'>Category</div>,
+      cell: ({ row }) => (
+        <Badge variant='outline' className='text-muted-foreground px-1.5'>
+          {row.original.category}
+        </Badge>
+      ),
+    },
+    {
+      accessorKey: 'score',
+      header: () => <div className='w-full text-center'>Score</div>,
+      cell: ({ row }) => (
+        <div className='text-foreground text-center text-xs' id={`${row.original.id}-score`}>
+          {row.original.score}
+        </div>
+      ),
+    },
+    {
+      accessorKey: 'points',
+      header: 'Points',
+      cell: ({ row }) => <div className='flex justify-center'>{row.original.points}</div>,
+    },
+    {
+      id: 'preview-action',
+      header: undefined,
+      cell: ({ row }) => {
+        return (
+          <div className='flex justify-center'>
+            <DrawerActionTableCell item={row.original}>
+              <Button variant='link' size='sm' className='text-foreground/50'>
+                <EyeIcon />
+                Show Answers
+              </Button>
+            </DrawerActionTableCell>
+          </div>
+        )
+      },
+    },
+    {
+      id: 'actions',
+      cell: () => (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='data-[state=open]:bg-muted text-muted-foreground flex size-8' size='icon'>
+              <IconDotsVertical />
+              <span className='sr-only'>Open menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end' className='w-40'>
+            <DropdownMenuItem className='justify-between'>
+              Show Answers
+              <ChartColumnIcon className='size-3.5' />
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem variant='destructive' className='justify-between'>
+              Delete Answer
+              <TrashIcon className='size-3.5' />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ),
+    },
+  ]
+  const data: QuestionItem[] = [
+    { id: getUUID(), category: 'general', points: 8, position: 1, questionText: 'What is capital of France?', score: 5 },
+    { id: getUUID(), category: 'geogaphy', points: 10, position: 2, questionText: 'What is round and shaped like a sphere?', score: 5 },
+  ]
+
+  return <DataTable data={data} columns={columns} />
+}
+
+function DrawerActionTableCell({ item, children }: { item: QuestionItem; children: React.ReactNode }) {
+  const isMobile = useIsMobile()
+
+  return (
+    <Drawer direction={isMobile ? 'bottom' : 'right'}>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
+      <DrawerContent className='data-[vaul-drawer-direction=right]:*:data-close:flex'>
+        <div data-close className='absolute top-0 bottom-0 -left-2.5 hidden items-center'>
+          <DrawerClose asChild>
+            <Button variant='ghost' size='icon' className='bg-background size-4 text-neutral-600 hover:scale-115 dark:text-neutral-300'>
+              <ChevronRightIcon className='' />
+            </Button>
+          </DrawerClose>
+        </div>
+        <DrawerHeader className='mb-6 gap-1 border-b'>
+          <DrawerTitle>Answers for: {item.questionText}</DrawerTitle>
+          <DrawerDescription>Showing user answers for question {item.position}.</DrawerDescription>
+        </DrawerHeader>
+        <div className='flex flex-1 flex-col gap-4 overflow-y-auto px-4 text-sm'>
+          <QuestionScoresLineChart />
+          <form className='mt-4 flex flex-col gap-6'>
+            <div className='col-span-2 flex flex-2 flex-col gap-3'>
+              <Label htmlFor='question'>Question</Label>
+              <Input id='question' readOnly defaultValue={item.questionText} />
+            </div>
+
+            <div className='flex flex-col gap-3'>
+              <Label htmlFor='answers'>Answers</Label>
+              <div className='grid-container mx-3 [--grid-column-count:2] [--grid-desired-gap-x:36px] [--grid-desired-gap:24px] [--grid-item-min-width:80px]'>
+                {Array.from({ length: 4 }, (_, i) => (
+                  <div key={i} className='relative flex h-10 w-full items-center justify-center rounded-md bg-neutral-200 dark:bg-neutral-700'>
+                    {(i === 0 || i === 3) && <CheckIcon className='absolute top-0.5 right-0.5 size-3.5' />}
+                    Answer {i + 1}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </form>
+        </div>
+        <DrawerFooter className='grid grid-cols-2 gap-12'>
+          <DrawerClose asChild>
+            <Button className='' variant='outline'>
+              Close
+            </Button>
+          </DrawerClose>
+
+          <DrawerClose asChild>
+            <Button className=''>Save Changes</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
+}
