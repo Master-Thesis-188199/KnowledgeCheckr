5259553385f0d73ea15428d7027a16c266ad24e3
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-12 15:22:09 +0100

    ref: improved `PinSidebarButton` accessibility
    Added aria-label attribute and updated tag from `div` to `button`.

--- src/components/root/Navigation/mobile/PinSidebarButton.tsx
+++ src/components/root/Navigation/mobile/PinSidebarButton.tsx
@@ -9,12 +9,13 @@ export function PinSidebarButton() {
   const { isOpen, isPinned, togglePinned, canDeviceHover } = useSidebarStore((state) => state)
 
   return (
-    <motion.div
+    <motion.button
+      aria-label='pin sidebar'
       initial={{ opacity: !isPinned ? 0 : 1 }}
       animate={{ opacity: !isPinned ? (isOpen ? 1 : 0) : 1, rotate: !isPinned ? 45 : 0 }}
       className={tw('absolute top-1.5 right-1.5 flex', !canDeviceHover && 'hidden')}
       onClick={togglePinned}>
       <Pin className={tw('size-5 stroke-1', isPinned && 'stroke-2')} />
-    </motion.div>
+    </motion.button>
   )
 }
