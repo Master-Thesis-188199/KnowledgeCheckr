671e82988d75d2573fff071469074ee7cddd1280
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-13 07:22:19 +0100

    chore: enabled component class detection setting
    By enabling this setting the `better-tailwindcss` eslint plugin will detect those classes and not report them as unknown classes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = defineConfig([
     settings: {
       'better-tailwindcss': {
         entryPoint: 'src/app/globals.css',
+        detectComponentClasses: true,
       },
     },
     rules: {
