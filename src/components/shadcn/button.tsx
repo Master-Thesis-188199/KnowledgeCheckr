3b9fdb0c14c4fb7d2a9e691f01a3a034a41cef0e
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-15 08:11:05 +0100

    ref: applied `RippleButton` as base button in `Button`
    By renaming the previously used `Button` to `SimpleButton` and re-exporting the new `RippleButton` as `Button` all existing usages of `Button` now use the `RippleButton instead`.

--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { LoaderCircleIcon } from 'lucide-react'
 import { cn } from '@/lib/Shared/utils'
+import { RippleButton } from '@/src/components/shadcn/ripple-button'
 
 const buttonVariants = cva(
   "inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap outline-none select-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 enabled:hover:cursor-pointer disabled:opacity-50 disabled:hover:cursor-not-allowed aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
@@ -73,4 +74,4 @@ function Button({
   )
 }
 
-export { Button, buttonVariants }
+export { Button as SimpleButton, RippleButton as Button, buttonVariants }
