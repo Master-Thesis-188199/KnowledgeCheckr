5f0be6c3a51f50d493c8b81681f2209a47f20b77
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-17 15:51:53 +0100

    ref: added open-question input in `createDummyUserInput`
    This way the generated dummy inputs for open-question questions are filled with random words, to simulate user-inputs.

--- src/lib/dummy/createDummyUserInput.tsx
+++ src/lib/dummy/createDummyUserInput.tsx
@@ -1,4 +1,5 @@
 import shuffle from 'lodash/shuffle'
+import lorem from '@/src/lib/Shared/Lorem'
 import { DragDropQuestion, MultipleChoice, OpenQuestion, Question, SingleChoice } from '@/src/schemas/QuestionSchema'
 import { QuestionInput } from '@/src/schemas/UserQuestionInputSchema'
 import { Any } from '@/types'
@@ -17,7 +18,17 @@ export default function createDummyQuestionInput(question: Question): QuestionIn
       return { question_id: question.id, type: question.type, selection: [question.answers[randomIndex(question.answers)].id, question.answers[randomIndex(question.answers)].id] }
     }
     case 'open-question': {
-      return { question_id: question.id, type: question.type, input: '' }
+      return {
+        question_id: question.id,
+        type: question.type,
+        input:
+          Math.random() > 0.8
+            ? ''
+            : lorem()
+                .split(' ')
+                .slice(0, Math.round(Math.random() * 25) + 1)
+                .join(' '),
+      }
     }
     case 'drag-drop': {
       return { question_id: question.id, type: question.type, input: (Math.random() > 0.8 ? question.answers : shuffle(question.answers)).map((a) => a.id) }
