8f178729bf91baff51eb06c49bc3622e38d8342f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-17 08:34:29 +0100

    style: lowered slider track color contrast

--- src/components/shadcn/slider.tsx
+++ src/components/shadcn/slider.tsx
@@ -15,7 +15,7 @@ function Slider({ className, defaultValue, value, min = 0, max = 100, ...props }
       min={min}
       max={max}
       className={cn(
-        'relative flex w-full touch-none items-center select-none data-[disabled]:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col',
+        'relative flex w-full touch-none items-center select-none data-disabled:opacity-50 data-[orientation=vertical]:h-full data-[orientation=vertical]:min-h-44 data-[orientation=vertical]:w-auto data-[orientation=vertical]:flex-col',
         className,
       )}
       {...props}>
@@ -24,7 +24,7 @@ function Slider({ className, defaultValue, value, min = 0, max = 100, ...props }
         className={cn(
           'bg-accent relative grow overflow-hidden rounded-full data-[orientation=horizontal]:h-1.5 data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-1.5',
         )}>
-        <SliderPrimitive.Range data-slot='slider-range' className={cn('bg-primary/70 absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full')} />
+        <SliderPrimitive.Range data-slot='slider-range' className={cn('bg-muted-foreground absolute data-[orientation=horizontal]:h-full data-[orientation=vertical]:w-full')} />
       </SliderPrimitive.Track>
       {Array.from({ length: _values.length }, (_, index) => (
         <SliderPrimitive.Thumb
