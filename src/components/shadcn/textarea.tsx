b6e616dd1a4acfc3d5ca24a4937cff9c9974857b
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-18 08:23:23 +0100

    style: updated `Textarea` to match `Input` styles

--- src/components/shadcn/textarea.tsx
+++ src/components/shadcn/textarea.tsx
@@ -6,7 +6,14 @@ function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
     <textarea
       data-slot='textarea'
       className={cn(
-        'rounded-md bg-neutral-100/90 px-3 py-1.5 text-neutral-600 ring-1 ring-neutral-400 outline-none placeholder:text-neutral-400/90 hover:cursor-text hover:ring-ring-hover focus:ring-[1.2px] focus:ring-ring-focus dark:bg-neutral-800 dark:text-neutral-300/80 dark:ring-neutral-500 dark:placeholder:text-neutral-400/50 dark:hover:ring-ring-hover dark:focus:ring-ring-focus',
+        'rounded-md border border-input-ring bg-transparent px-3 py-1 text-sm shadow-xs transition-[color,box-shadow] outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 dark:disabled:opacity-75',
+        'focus-visible:border-ring-hover focus-visible:ring-[3px] focus-visible:ring-ring-hover/50',
+
+        'px-3 py-1.5',
+        'bg-input text-neutral-600 hover:cursor-text dark:text-neutral-300/80',
+        'enabled:hover:border-ring-hover dark:enabled:hover:border-ring-hover',
+        'aria-invalid:border-destructive aria-invalid:ring-destructive/20 aria-invalid:hover:border-destructive dark:aria-invalid:ring-destructive/40',
+
         className,
       )}
       {...props}
