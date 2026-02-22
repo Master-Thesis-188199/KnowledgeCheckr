920b173686b2a71941a5d2cf7619cf84eeb9e0d4
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-22 20:27:33 +0100

    feat: added feedback text display to open-question

--- src/components/checks/[share_token]/(shared)/(questions)/OpenQuestionAnswer.tsx
+++ src/components/checks/[share_token]/(shared)/(questions)/OpenQuestionAnswer.tsx
@@ -1,5 +1,7 @@
-import { HTMLProps } from 'react'
+import { HTMLProps, useState } from 'react'
+import { MessageCircleQuestionIcon } from 'lucide-react'
 import { useFormContext } from 'react-hook-form'
+import DisplayFeedbackText from '@/src/components/checks/[share_token]/practice/FeedbackText'
 import { usePracticeFeeback } from '@/src/hooks/checks/[share_token]/practice/usePracticeFeedback'
 import { cn } from '@/src/lib/Shared/utils'
 import type { OpenQuestion } from '@/src/schemas/QuestionSchema'
@@ -16,37 +18,45 @@ export function FeedbackOpenQuestion({
   question: OpenQuestion
 } & Pick<HTMLProps<HTMLTextAreaElement>, 'disabled'>) {
   const { isCorrect, isIncorrect, reasoning } = getFeedbackEvaluation(question)
+  const [isPinned, setPinned] = useState(false)
 
   return (
     <OpenQuestionAnswer
       {...props}
       disabled={isEvaluated || props.disabled}
-      title={reasoning}
       data-evaluation-result={isEvaluated ? (isCorrect ? 'correct' : isIncorrect ? 'incorrect' : 'none') : 'none'}
       className={cn(
         isEvaluated && 'relative ring-2',
         isCorrect && 'bg-radial from-neutral-200/60 via-neutral-200/60 to-green-500/20 ring-green-400/70 dark:from-neutral-700/60 dark:via-neutral-700/60 dark:to-green-500/15 dark:ring-green-500/70',
-        isIncorrect &&
-          'cursor-help bg-radial from-neutral-200/60 via-neutral-200/60 to-red-500/20 ring-red-500/70 dark:from-neutral-700/60 dark:via-neutral-700/60 dark:to-red-400/15 dark:ring-red-400/70',
-      )}
-    />
+        isIncorrect && 'bg-radial from-neutral-200/60 via-neutral-200/60 to-red-500/20 ring-red-500/70 dark:from-neutral-700/60 dark:via-neutral-700/60 dark:to-red-400/15 dark:ring-red-400/70',
+        isEvaluated && 'pr-8',
+      )}>
+      <DisplayFeedbackText disabled={!isEvaluated} pinned={isPinned} feedback={reasoning} side='right'>
+        <div className={cn('group/tooltip absolute top-1 right-1.5 flex flex-row-reverse gap-1.5')}>
+          <MessageCircleQuestionIcon className={cn('size-4.5 text-warning', !isPinned ? 'not-group-hover/tooltip:group-hover:animate-scale' : 'scale-110', !isEvaluated && 'hidden')} />
+        </div>
+      </DisplayFeedbackText>
+    </OpenQuestionAnswer>
   )
 }
 
-export function OpenQuestionAnswer({ ...props }: {} & HTMLProps<HTMLTextAreaElement>) {
+export function OpenQuestionAnswer({ children, ...props }: { children?: React.ReactNode } & HTMLProps<HTMLTextAreaElement>) {
   const { register } = useFormContext<QuestionInput>()
 
   return (
-    <textarea
-      {...props}
-      {...register('input')}
-      className={cn(
-        'rounded-md bg-neutral-100/90 px-3 py-1.5 text-neutral-600 ring-1 ring-neutral-400 outline-none placeholder:text-neutral-400/90 dark:bg-neutral-800 dark:text-neutral-300/80 dark:ring-neutral-500 dark:placeholder:text-neutral-400/50',
-        'enabled:hover:cursor-text enabled:hover:ring-ring-hover enabled:focus:ring-[1.2px] enabled:focus:ring-ring-focus enabled:dark:hover:ring-ring-hover enabled:dark:focus:ring-ring-focus',
-        'resize-none',
-        'my-auto h-full min-h-32',
-        props.className,
-      )}
-    />
+    <div className='relative *:w-full'>
+      <textarea
+        {...props}
+        {...register('input')}
+        className={cn(
+          'rounded-md bg-neutral-100/90 px-3 py-1.5 text-neutral-600 ring-1 ring-neutral-400 outline-none placeholder:text-neutral-400/90 dark:bg-neutral-800 dark:text-neutral-300/80 dark:ring-neutral-500 dark:placeholder:text-neutral-400/50',
+          'enabled:hover:cursor-text enabled:hover:ring-ring-hover enabled:focus:ring-[1.2px] enabled:focus:ring-ring-focus enabled:dark:hover:ring-ring-hover enabled:dark:focus:ring-ring-focus',
+          'resize-none',
+          'my-auto h-full min-h-32',
+          props.className,
+        )}
+      />
+      {children}
+    </div>
   )
 }
