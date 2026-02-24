bbaf52b42d265ac65c636445eb8799202d3a365f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-02-24 09:43:48 +0100

    feat: created `RHFProvider` and `useRHFContext`
    With the help of this new provider and useContext handler the instantiation of form through `useRHF` can be shared across components without the need of property-drilling. This way the signature of components within such a provider can be simplified.

---
+++ src/hooks/Shared/form/react-hook-form/RHFProvider.tsx
@@ -0,0 +1,19 @@
+import { createContext, ReactNode, useContext } from 'react'
+import { RHFBaseReturn, RHFServerReturn } from '@/src/hooks/Shared/form/react-hook-form/type'
+import { Any } from '@/types'
+
+type RHFContext<T extends object> = RHFBaseReturn<T> & Partial<RHFServerReturn<T>>
+
+const RHFContext = createContext<RHFContext<Any> | null>(null)
+
+export const useRHFContext = <T extends object>() => {
+  const context = useContext(RHFContext)
+  if (!context) {
+    throw new Error('useRHFContext must be used within a RHFProvider')
+  }
+  return context as RHFContext<T>
+}
+
+export const RHFProvider = <T extends object>({ children, ...props }: { children: ReactNode } & RHFContext<T>) => {
+  return <RHFContext.Provider value={props}>{children}</RHFContext.Provider>
+}
