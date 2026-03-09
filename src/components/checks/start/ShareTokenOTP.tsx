845dee4748009096b6988936962c2e5293d15a4d
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-09 09:22:20 +0100

    feat: added share-token check user actions
    This way users are shown the available options (start-practice, start examination) after entering a respective share-token.

--- src/components/checks/start/ShareTokenOTP.tsx
+++ src/components/checks/start/ShareTokenOTP.tsx
@@ -2,8 +2,12 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import { LoaderCircleIcon } from 'lucide-react'
+import { isAfter } from 'date-fns/isAfter'
+import { isBefore } from 'date-fns/isBefore'
+import { ExternalLinkIcon, LoaderCircleIcon } from 'lucide-react'
+import Link from 'next/link'
 import { getKnowledgeCheckByShareToken } from '@/database/knowledgeCheck/select'
+import { Button } from '@/src/components/shadcn/button'
 import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from '@/src/components/shadcn/input-otp'
 import SmoothPresenceTransition from '@/src/components/Shared/Animations/SmoothPresenceTransition'
 import debounceFunction from '@/src/hooks/Shared/debounceFunction'
@@ -26,8 +30,31 @@ export default function ShareTokenOTP() {
       .then((check) => {
         if (!check) return setStatus('not-found')
 
-        // todo set status based on what options are available
-        setStatus('done')
+        if (
+          check.settings.practice.enablePracticing &&
+          check.settings.examination.enableExaminations &&
+          isBefore(check.settings.examination.startDate, new Date(Date.now())) &&
+          (check.settings.examination.endDate === null || isAfter(check.settings.examination.endDate, new Date(Date.now())))
+        )
+          return setStatus('exam-and-practice')
+
+        if (
+          check.settings.practice.enablePracticing &&
+          !(
+            check.settings.examination.enableExaminations &&
+            isBefore(check.settings.examination.startDate, new Date(Date.now())) &&
+            (check.settings.examination.endDate === null || isAfter(check.settings.examination.endDate, new Date(Date.now())))
+          )
+        )
+          return setStatus('practice-only')
+
+        if (
+          !check.settings.practice.enablePracticing &&
+          check.settings.examination.enableExaminations &&
+          isBefore(check.settings.examination.startDate, new Date(Date.now())) &&
+          (check.settings.examination.endDate === null || isAfter(check.settings.examination.endDate, new Date(Date.now())))
+        )
+          return setStatus('exam-only')
       })
       .catch(() => setStatus('not-found'))
   }, [status])
@@ -65,6 +92,28 @@ export default function ShareTokenOTP() {
           Parsing token
         </SmoothPresenceTransition>
       </div>
+
+      <SmoothPresenceTransition
+        active={status === 'practice-only' || status === 'exam-only' || status === 'exam-and-practice'}
+        presenceTiming={{ showDelayMs: 0 }}
+        initial={{ opacity: 0, height: 0 }}
+        animate={{ opacity: 1, height: 'auto' }}
+        exit={{ opacity: 0, height: 1, margin: 0 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        className='flex justify-center gap-6 text-muted-foreground'>
+        <Link href={`/checks/${shareTokenInput}/practice`} className='flex-1' tabIndex={-1}>
+          <Button variant='base' className='flex min-h-12 w-full items-center justify-center rounded-md'>
+            Start Practicing
+            <ExternalLinkIcon />
+          </Button>
+        </Link>
+        <Link href={`/checks/${shareTokenInput}/`} className='flex-1' tabIndex={-1}>
+          <Button variant='base' className='flex min-h-12 w-full items-center justify-center rounded-md'>
+            Start Examination Attempt
+            <ExternalLinkIcon />
+          </Button>
+        </Link>
+      </SmoothPresenceTransition>
     </>
   )
 }
