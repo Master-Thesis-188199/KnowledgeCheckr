8686ba4acf354806d21877de3e82628e9719c5bd
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-10 09:08:35 +0100

    ref: improved share-token input page layout
    Instead of manually building a card that wraps the share-token input component the shadcn card component is used to achieve a more unified appearance.

--- src/app/[locale]/checks/start/page.tsx
+++ src/app/[locale]/checks/start/page.tsx
@@ -1,5 +1,5 @@
 import ShareTokenOTP from '@/src/components/checks/start/ShareTokenOTP'
-import Card from '@/src/components/Shared/Card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/src/components/shadcn/card'
 import PageHeading from '@/src/components/Shared/PageHeading'
 
 export default async function StartCheckPage() {
@@ -8,10 +8,14 @@ export default async function StartCheckPage() {
       <PageHeading title='Start your practice / exam journey' />
 
       {/* // todo ensure max-w is a either 50vw or 300 px for small screens */}
-      <Card className='mx-auto flex w-full max-w-[50vw] flex-col justify-center gap-6 px-6 py-4' disableInteractions>
-        <h2 className='mb-4 text-center font-semibold'>Find KnowledgeCheck by share-token</h2>
-
-        <ShareTokenOTP />
+      <Card className='mx-auto w-full max-w-2xl'>
+        <CardHeader className='border-b'>
+          <CardTitle>Find KnowledgeCheck by share-token</CardTitle>
+          <CardDescription>Provide a given share-token to begin practice / examination.</CardDescription>
+        </CardHeader>
+        <CardContent className='flex flex-col gap-6'>
+          <ShareTokenOTP />
+        </CardContent>
       </Card>
     </>
   )
