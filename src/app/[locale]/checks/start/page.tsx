f0035c65ba5fb336f2a9e3e055ec0a793d1dae65
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-10 10:48:12 +0100

    ref: removed unused todo comment

--- src/app/[locale]/checks/start/page.tsx
+++ src/app/[locale]/checks/start/page.tsx
@@ -9,7 +9,6 @@ export default async function StartCheckPage() {
     <>
       <PageHeading title='Start your practice / exam journey' />
 
-      {/* // todo ensure max-w is a either 50vw or 300 px for small screens */}
       <Card className='mx-auto w-full max-w-2xl'>
         <CardHeader className='border-b'>
           <CardTitle>Find KnowledgeCheck by share-token</CardTitle>
