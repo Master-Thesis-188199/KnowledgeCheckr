145ddf4db04e77ffd4de6402e8f09ad0328f9c54
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-13 08:29:26 +0100

    ref: added param support to custom i18n types
    The reason for this change is that while the previously created types work as expected for typing basic translation use-cases they did not type and thereby enforce parameters. Now the types include parameter information to behave very similar to the actual `next-international` translate function.

--- src/i18n/locales/types.ts
+++ src/i18n/locales/types.ts
@@ -1,4 +1,3 @@
-import { Any } from '@/types'
 import englishTranslations from './en'
 
 type Translations = typeof englishTranslations
@@ -21,13 +20,24 @@ type ScopeKeys<T> = T extends Primitive
 
 type ValueAtPath<T, Path extends string> = Path extends `${infer Head}.${infer Rest}` ? (Head extends keyof T ? ValueAtPath<T[Head], Rest> : never) : Path extends keyof T ? T[Path] : never
 
+type ExtractPlaceholders<T extends string> = T extends `${string}{${infer Param}}${infer Rest}` ? Param | ExtractPlaceholders<Rest> : never
+
 export type TranslationScopes = ScopeKeys<Translations>
 
 export type TranslationKeys = LeafKeys<Translations>
+
+type TranslationValue<Key extends TranslationKeys> = ValueAtPath<Translations, Key>
+
+type TranslationParams<Value> = Value extends string ? ([ExtractPlaceholders<Value>] extends [never] ? never : { [K in ExtractPlaceholders<Value>]: string | number }) : never
+
+type TranslationArgs<Value> = TranslationParams<Value> extends never ? [] : [params: TranslationParams<Value>]
+
+export type Translator = <Key extends TranslationKeys>(key: Key, ...args: TranslationArgs<TranslationValue<Key>>) => string
 
 export type ScopedTranslationKeys<Scope extends TranslationScopes> = LeafKeys<ValueAtPath<Translations, Scope>>
 
 export type ScopedTranslations<Scope extends TranslationScopes> = ValueAtPath<Translations, Scope>
 
-export type ScopedTranslator<Scope extends TranslationScopes> = (key: ScopedTranslationKeys<Scope>, ...args: unknown[]) => string
-export type Translator = (key: TranslationKeys, ...args: Any[]) => string
+type ScopedTranslationValue<Scope extends TranslationScopes, Key extends ScopedTranslationKeys<Scope>> = ValueAtPath<ScopedTranslations<Scope>, Key>
+
+export type ScopedTranslator<Scope extends TranslationScopes> = <Key extends ScopedTranslationKeys<Scope>>(key: Key, ...args: TranslationArgs<ScopedTranslationValue<Scope, Key>>) => string
