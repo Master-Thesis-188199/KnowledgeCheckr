9b7fbbe2d3c1d62c34e0e77838e3b0eac6596d97
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-13 09:00:30 +0100

    chore: wrapped `CourseSchema` in getter function
    This way the course schema is localized when it is being retrieved. This means that where ever the schema is used the environment specific translator is passed to the getter to actually localize the schema.

--- src/schemas/CourseSchema.ts
+++ src/schemas/CourseSchema.ts
@@ -1,13 +1,113 @@
 import { isBefore } from 'date-fns/isBefore'
 import { z } from 'zod'
 import { schemaUtilities } from '@/schemas/utils/schemaUtilities'
+import { Translator } from '@/src/i18n/locales/types'
 import { getUUID } from '@/src/lib/Shared/getUUID'
 import lorem from '@/src/lib/Shared/Lorem'
 import { CategorySchema } from '@/src/schemas/CategorySchema'
 import { CourseSettingsSchema } from '@/src/schemas/CourseSettingsSchema'
 import { StringDate } from '@/src/schemas/CustomZodTypes'
 import { QuestionSchema } from '@/src/schemas/QuestionSchema'
+export function getCourseSchema(t: Translator) {
+  return (
+    z
+      .object({
+        id: z.uuidv4().default(() => getUUID()),
 
+        name: z.string().default(t('schemas.Course.name.default')).describe(t('schemas.Course.name.description')),
+
+        description: z
+          .string()
+          .nullable()
+          .default(() => lorem().substring(0, Math.floor(Math.random() * 100)))
+          .describe(t('schemas.Course.description.description')),
+
+        difficulty: z
+          .number()
+          .min(1, t('schemas.Course.difficulty.min_max_message'))
+          .max(10, t('schemas.Course.difficulty.min_max_message'))
+          .optional()
+          .default(() => (Math.floor(Math.random() * 1000) % 10) + 1)
+          .describe(t('schemas.Course.difficulty.description')),
+
+        questions: z.array(QuestionSchema).refine((questions) => questions.length === new Set(questions.map((q) => q.id)).size, { message: t('schemas.Course.questions.refinement_message') }),
+        questionCategories: z
+          .array(CategorySchema)
+          .optional()
+          .default(() => [{ id: getUUID(), name: 'general', skipOnMissingPrequisite: false, prequisiteCategoryId: null }]),
+
+        share_key: z.string().nullable().default(null),
+
+        openDate: z
+          .date()
+          .or(z.string())
+          .transform((date) => (typeof date === 'string' ? new Date(date) : date))
+          .refine((course) => !isNaN(course.getTime()), t('schemas.Shared.date_nan_time'))
+          // .refine((date) => isFuture(addDays(date, 1)), 'The openDate cannot be in the past!')
+          .default(() => new Date(Date.now()))
+          .describe(t('schemas.Course.openDate.description')),
+        closeDate: z
+          .date()
+          .or(z.string())
+          .transform((date) => (typeof date === 'string' ? new Date(date) : date))
+          .refine((course) => !isNaN(course.getTime()), t('schemas.Shared.date_nan_time'))
+          // .refine((date) => isFuture(addDays(date, 1)), 'The closeDate cannot be in the past!')
+          .nullable()
+          .default(null)
+          .describe(t('schemas.Course.closeDate.description')),
+
+        createdAt: StringDate.default(() => new Date(Date.now())).optional(),
+        updatedAt: StringDate.default(() => new Date(Date.now())).optional(),
+
+        owner_id: z.string().nonempty().max(36, t('schemas.Course.owner_id.max_message')).default('unknown'),
+        collaborators: z.array(z.string()).default([]),
+
+        settings: CourseSettingsSchema,
+
+        /* todo:
+      - question-order: 'shuffle, static, ...'
+      - question-answer-type: 'drag-drop', 'select', ....
+
+    */
+      })
+      //* Declares missing question-catgegories in `questionCategories`
+      .transform((course) => {
+        const questionCategories = course.questionCategories
+
+        // declare missing question categories
+        Array.from(new Set(course.questions.map((q) => q.category)))
+          .filter((categoryName) => !questionCategories.some((c) => c.name === categoryName))
+          .forEach((missingCategoryName) => {
+            questionCategories.push({
+              id: getUUID(),
+              name: missingCategoryName,
+              prequisiteCategoryId: null,
+              skipOnMissingPrequisite: false,
+            })
+          })
+
+        return course
+      })
+      .refine(({ questions, questionCategories }) => questions.every((question) => !!questionCategories?.find((qc) => qc.name === question.category)), {
+        message: t('schemas.Course.questionCategories.refinement_message'),
+      })
+      .superRefine(({ openDate, closeDate }, ctx) => {
+        if (closeDate === null) return
+
+        if (isBefore(closeDate, openDate)) {
+          ctx.addIssue({
+            code: 'custom',
+            message: t('schemas.Course.closeDate.superRefine_message'),
+            path: ['closeDate'],
+          })
+        }
+      })
+  )
+}
+
+/**
+ * @deprecated
+ */
 export const CourseSchema = z
   .object({
     id: z.uuidv4().default(() => getUUID()),
@@ -101,7 +201,7 @@ export const CourseSchema = z
     }
   })
 
-export type Course = z.output<typeof CourseSchema>
+export type Course = z.output<ReturnType<typeof getCourseSchema>>
 
 const { validate: validateCourse, instantiate: instantiateCourse, safeParse: safeParseCourse } = schemaUtilities(CourseSchema)
 export { instantiateCourse, safeParseCourse, validateCourse }
