9b0a4f0e522268d12bdaa4877ba88efb5a73039f
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-17 15:22:56 +0100

    ref: updated `MultiStageRender` to be generic
    This way users can define which wrapper-component is used by the `MultiStageRenderer` and thus which arguments it accepts. This way the wrapper can be set to a fragment, form and many more tags.

--- src/components/Shared/MultiStageProgress/MutliStageRenderer.tsx
+++ src/components/Shared/MultiStageProgress/MutliStageRenderer.tsx
@@ -1,13 +1,30 @@
 'use client'
 
-import React from 'react'
-import { Stage, useMultiStageStore } from '@/src/components/Shared/MultiStageProgress/MultiStageStoreProvider'
+import React, { ElementType } from 'react'
+import { useMultiStageStore } from '@/src/components/Shared/MultiStageProgress/MultiStageStoreProvider'
 import { cn } from '@/src/lib/Shared/utils'
 
-export function MutliStageRenderer({ children, stage, className }: { children: React.ReactNode; stage: Stage['stage']; className?: string }) {
+type WrapperProps<T extends React.ElementType> = {
+  as?: T
+  children: React.ReactNode
+  stage: number
+} & React.ComponentPropsWithoutRef<T>
+
+type Props<T extends React.ElementType = 'div'> = WrapperProps<T>
+
+export function MutliStageRenderer<C extends ElementType = 'div'>({ children, stage, as, className, ...rest }: Props<C>) {
   const { stage: currentStage } = useMultiStageStore((store) => store)
+  const Component = (as ?? 'div') as ElementType
+
+  if (Component === React.Fragment) {
+    return <>{children}</>
+  }
 
   if (currentStage !== stage) return null
 
-  return <div className={cn(currentStage !== stage && 'hidden', className)}>{children}</div>
+  return (
+    <Component className={cn(currentStage !== stage && 'hidden', className)} {...rest}>
+      {children}
+    </Component>
+  )
 }
