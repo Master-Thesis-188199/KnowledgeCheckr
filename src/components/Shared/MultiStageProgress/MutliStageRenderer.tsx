5b8480fca43ed86fc71f620cf704a287e4c2315c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-17 07:03:34 +0100

    Revert "ref: removed wrapper div in `MultiStateRenderer`"
    This reverts commit e5ca770a9e6757ff1ef36a7fac087da898d364f9.

--- src/components/Shared/MultiStageProgress/MutliStageRenderer.tsx
+++ src/components/Shared/MultiStageProgress/MutliStageRenderer.tsx
@@ -2,11 +2,12 @@
 
 import React from 'react'
 import { Stage, useMultiStageStore } from '@/src/components/Shared/MultiStageProgress/MultiStageStoreProvider'
+import { cn } from '@/src/lib/Shared/utils'
 
-export function MutliStageRenderer({ children, stage }: { children: React.ReactNode; stage: Stage['stage'] }) {
+export function MutliStageRenderer({ children, stage, className }: { children: React.ReactNode; stage: Stage['stage']; className?: string }) {
   const { stage: currentStage } = useMultiStageStore((store) => store)
 
   if (currentStage !== stage) return null
 
-  return <>{children}</>
+  return <div className={cn(currentStage !== stage && 'hidden', className)}>{children}</div>
 }
