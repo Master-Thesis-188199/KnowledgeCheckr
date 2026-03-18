6aef4581380c0152de588a1cbfb9a4232db0e5b8
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-18 10:27:00 +0100

    style: extended button variants with additional sizes

--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -38,10 +38,13 @@ const buttonVariants = cva(
         ),
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        'default': 'h-9 px-4 py-2 has-[>svg]:px-3',
+        'sm': 'h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5',
+        'lg': 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        'icon': 'size-9',
+        'icon-xs': "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
+        'icon-sm': 'size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg',
+        'icon-lg': 'size-9',
       },
     },
     defaultVariants: {
