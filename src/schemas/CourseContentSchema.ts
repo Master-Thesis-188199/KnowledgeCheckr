88fa807fd8ae7766536221e0d2565fac6b1e7843
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-22 11:14:45 +0100

    ref: added human-readable zod error msg for `title`

--- src/schemas/CourseContentSchema.ts
+++ src/schemas/CourseContentSchema.ts
@@ -5,7 +5,7 @@ import { schemaUtilities } from '@/src/schemas/utils/schemaUtilities'
 const tiptapContent: z.ZodType<JSONContent> = z.any()
 
 export const CourseContentSchema = z.object({
-  title: z.string().nonempty().describe('Used to quickly identify a given content of a category'),
+  title: z.string().nonempty("A content's title can not be empty.").describe('Used to quickly identify a given content of a category'),
   description: z.string().optional().describe('Describes the content associated to a given category.'),
   categoryId: z.uuidv4().nonempty(),
   content: tiptapContent.optional(),
