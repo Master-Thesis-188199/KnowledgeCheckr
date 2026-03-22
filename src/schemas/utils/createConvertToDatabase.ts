1b820449250247fdc08d8e686cb03659c693b9a3
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-22 08:23:47 +0100

    ref: updated `convertToDB` type to recursive contents prop
    With the introduction of the new `contents` property that holds a `content` prop of type `JSONContent`, the type conversion lead to a type of `string`.
    
    The root cause in your case is the JSONContent type from Tiptap: it explicitly declares an index signature ([key: string]: any]), so keyof JSONContent is string.  When DeepPropertyKeys recurses into a property of that type, string ends up in the union and the whole union gets widened, which is then removed by the NarrowKeys type. This causes the output type be a `string`.
    
    With this change, when DeepPropertyKeys reaches JSONContent it will detect that keyof JSONContent is string and return never instead of string.

--- src/schemas/utils/createConvertToDatabase.ts
+++ src/schemas/utils/createConvertToDatabase.ts
@@ -35,7 +35,14 @@ export type DeepPropertyKeys<T, Depth extends unknown[] = []> = Depth['length'] 
   : T extends readonly (infer U)[]
     ? DeepPropertyKeys<U, [0, ...Depth]>
     : T extends AnyRecord
-      ? keyof T | { [K in keyof T]-?: DeepPropertyKeys<T[K], [0, ...Depth]> }[keyof T]
+      ? // if T has an index signature (keyof T is string), don’t recurse into it
+        string extends keyof T
+        ? never
+        :
+            | keyof T
+            | {
+                [K in keyof T]-?: DeepPropertyKeys<T[K], [0, ...Depth]>
+              }[keyof T]
       : never
 
 /**
