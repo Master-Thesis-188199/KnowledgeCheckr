5edd490b31ce408bf1710baaa94bfef4eb628a63
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-03-24 09:28:11 +0100

    chore: removed deprecated `QuestionSchema` declarations

--- src/schemas/QuestionSchema.ts
+++ src/schemas/QuestionSchema.ts
@@ -11,11 +11,6 @@ const getAnswerId = (t: Translator) =>
     .uuid('An answer must have an uuid to identify it!')
     .catch(() => getUUID())
 
-const AnswerId = z
-  .string()
-  .uuid('An answer must have an uuid to identify it!')
-  .catch(() => getUUID())
-
 function getBaseQuestionSchema(translator: Translator) {
   return z.object({
     id: z.uuidv4(),
@@ -36,28 +31,6 @@ function getBaseQuestionSchema(translator: Translator) {
     accessibility: z.enum(['all', 'practice-only', 'exam-only']).default('all').catch('all'),
   })
 }
-
-/**
- * @deprecated
- */
-const baseQuestion = z.object({
-  id: z.uuidv4(),
-  points: z.number().positive('Number must be greater than 0'),
-  category: z.string().default('general'),
-
-  question: z
-    .string()
-    .refine((q) => q.split(' ').length > 2, 'Please reformulate your question to be at least 3 words long.')
-    .default(
-      lorem()
-        .substring(0, Math.floor(Math.random() * 100) + 20)
-        .split('\n')
-        .join(''),
-    ),
-
-  //* specifies how / in which environments the question should be displayed to users.
-  accessibility: z.enum(['all', 'practice-only', 'exam-only']).default('all').catch('all'),
-})
 
 /**
  * Ensures that the `answers` have unique names and ids, otherwise location / property based issues are created.
@@ -104,30 +77,6 @@ function getMultipleChoiceAnswerSchema(translator: Translator) {
   })
 }
 
-/**
- * @deprecated
- */
-const multipleChoiceAnswerSchema = z.object({
-  type: z.literal('multiple-choice'),
-  answers: z
-    .array(
-      z.object({
-        id: AnswerId,
-        answer: z.string().min(1, 'An answer must not be empty!'),
-        correct: z.boolean(),
-      }),
-    )
-    .min(1, 'Please provide at least one answer')
-    .refine((answers) => answers.find((answer) => answer.correct), { message: 'At least one answer has to be correct!' })
-    .superRefine(uniqueAnswerConstraints)
-    .default(() => [
-      { id: getUUID(), answer: 'Answer 1', correct: false },
-      { id: getUUID(), answer: 'Answer 2', correct: true },
-      { id: getUUID(), answer: 'Answer 3', correct: true },
-      { id: getUUID(), answer: 'Answer 4', correct: false },
-    ]),
-})
-
 function getSingleChoiceAnswerSchema(translator: Translator) {
   return z.object({
     type: z.literal('single-choice'),
@@ -150,30 +99,6 @@ function getSingleChoiceAnswerSchema(translator: Translator) {
       ]),
   })
 }
-
-/**
- * @deprecated
- */
-const singleChoiceAnswerSchema = z.object({
-  type: z.literal('single-choice'),
-  answers: z
-    .array(
-      z.object({
-        id: AnswerId,
-        answer: z.string(),
-        correct: z.boolean(),
-      }),
-    )
-    .min(1, 'Please provide at least one answer')
-    .superRefine(uniqueAnswerConstraints)
-    .refine((answers) => answers.filter((answer) => answer.correct).length === 1, { message: 'A single-choice question must have *one* correct answer!' })
-    .default(() => [
-      { id: getUUID(), answer: 'Answer 1', correct: false },
-      { id: getUUID(), answer: 'Answer 2', correct: true },
-      { id: getUUID(), answer: 'Answer 3', correct: false },
-      { id: getUUID(), answer: 'Answer 4', correct: false },
-    ]),
-})
 
 function getDragDropAnswerSchema(translator: Translator) {
   return z.object({
@@ -237,69 +162,6 @@ function getDragDropAnswerSchema(translator: Translator) {
   })
 }
 
-/**
- * @deprecated
- */
-const dragDropAnswerSchema = z.object({
-  type: z.literal('drag-drop'),
-  answers: z
-    .array(
-      z.object({
-        id: AnswerId,
-        answer: z.string(),
-        position: z.number().min(0, 'Position must be positive'),
-      }),
-    )
-    .superRefine((answers, ctx) => {
-      const n = answers.length
-      const seen = new Set<number>()
-      const minPos = Math.min(...answers.map((a) => a.position))
-
-      if (minPos !== 0) {
-        ctx.addIssue({
-          code: 'too_small',
-          origin: 'number',
-          minimum: 0,
-          type: 'number',
-          inclusive: true,
-          message: `[drag-drop] positions must begin from 0; received: ${minPos} `,
-          path: [answers.findIndex((a) => a.position === minPos), 'position'],
-        })
-        return
-      }
-
-      answers.forEach((answer, i) => {
-        if (seen.has(answer.position)) {
-          ctx.addIssue({
-            code: 'custom',
-            message: `[drag-drop] duplicate position: ${answer.position}`,
-            path: [i, 'position'],
-          })
-        }
-        seen.add(answer.position)
-      })
-
-      //* Identify gaps in continuous range of positions
-      for (let pos = 0; pos <= n - 1; pos++) {
-        if (!seen.has(pos)) {
-          ctx.addIssue({
-            code: 'custom',
-            message: `[drag-drop] positions must form a continuous range: [0...${n - 1}]; received: [${answers.map((a) => a.position).join(', ')}]. Position ${pos} is missing!`,
-          })
-
-          break
-        }
-      }
-    })
-    .superRefine(uniqueAnswerConstraints)
-    .default(() => [
-      { id: getUUID(), answer: 'Answer 1', position: 0 },
-      { id: getUUID(), answer: 'Answer 2', position: 1 },
-      { id: getUUID(), answer: 'Answer 3', position: 2 },
-      { id: getUUID(), answer: 'Answer 4', position: 3 },
-    ]),
-})
-
 function getOpenAnswerSchema(translator: Translator) {
   return z.object({
     type: z.literal('open-question'),
@@ -307,22 +169,9 @@ function getOpenAnswerSchema(translator: Translator) {
   })
 }
 
-/**
- * @deprecated
- */
-const openAnswerSchema = z.object({
-  type: z.literal('open-question'),
-  expectation: z.string().optional(),
-})
-
 function getQuestionAnswerTypes(t: Translator) {
   return z.discriminatedUnion('type', [getSingleChoiceAnswerSchema(t), getMultipleChoiceAnswerSchema(t), getDragDropAnswerSchema(t), getOpenAnswerSchema(t)])
 }
-
-/**
- * @deprecated
- */
-const questionAnswerTypes = z.discriminatedUnion('type', [singleChoiceAnswerSchema, multipleChoiceAnswerSchema, openAnswerSchema, dragDropAnswerSchema])
 
 export function getQuestionSchema(t: Translator) {
   return z.intersection(getBaseQuestionSchema(t), getQuestionAnswerTypes(t)).default(() => generateRandomQuestion())
@@ -377,8 +226,8 @@ function generateRandomQuestion(): Question {
   }
 }
 
-function generateRandomQuestionType(): AnswerTypes & Pick<BaseQuestion, 'question'> {
-  const dummyQuestions: (z.output<typeof questionAnswerTypes> & Pick<z.output<typeof baseQuestion>, 'question'> & Partial<z.output<typeof baseQuestion>>)[] = [
+function generateRandomQuestionType(): AnswerTypes & Pick<BaseQuestion, 'question'> & Partial<BaseQuestion> {
+  const dummyQuestions: (AnswerTypes & Pick<BaseQuestion, 'question'> & Partial<BaseQuestion>)[] = [
     {
       type: 'multiple-choice',
       question: 'In the PAYDAY series, who betrayed the PAYDAY gang that got Hoxton arrested?',
