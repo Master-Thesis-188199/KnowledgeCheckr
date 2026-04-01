f6dfcf77838d1ab29a9175fb0605cafb2abf2140
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-04-01 08:24:17 +0200

    feat: redesigned course content navigation btn appearances
    This way the buttons show a label on top of the content title ("previous", "next") to indicate it as an call-to-action.

--- src/components/courses/contents/[courseId]/ContentNavigationButtons.tsx
+++ src/components/courses/contents/[courseId]/ContentNavigationButtons.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
 import { useContentContext } from '@/src/components/courses/contents/[courseId]/ContentProvider'
 import { Button } from '@/src/components/shadcn/button'
 
@@ -7,10 +8,13 @@ export function NextContentButton() {
   const { contents, currentContentIndex, setCurrentIndex } = useContentContext()
 
   if (contents.length <= currentContentIndex + 1) return <div />
-
   return (
-    <Button onClick={() => setCurrentIndex((prev) => prev + 1)} variant='link'>
-      Continue to {contents.at(currentContentIndex + 1)?.title}
+    <Button onClick={() => setCurrentIndex((prev) => prev + 1)} variant='ghost' className='group flex h-fit max-w-56 flex-col gap-1'>
+      <span className='self-start text-sm text-muted-foreground'>Next</span>
+      <div className='flex items-center gap-1 group-hover:text-primary group-hover:underline'>
+        {contents.at(currentContentIndex + 1)?.title}
+        <ChevronRightIcon className='transition-transform group-hover:scale-125' />
+      </div>
     </Button>
   )
 }
@@ -21,8 +25,12 @@ export function PreviousContentButton() {
   if (currentContentIndex - 1 < 0) return <div />
 
   return (
-    <Button onClick={() => setCurrentIndex((prev) => prev - 1)} variant='link'>
-      Go back to {contents.at(currentContentIndex - 1)?.title}
+    <Button onClick={() => setCurrentIndex((prev) => prev - 1)} variant='ghost' className='group flex h-fit max-w-56 flex-col gap-1'>
+      <span className='self-end text-right text-sm text-muted-foreground'>Previous </span>
+      <div className='flex items-center gap-1 group-hover:text-primary group-hover:underline'>
+        <ChevronLeftIcon className='transition-transform group-hover:scale-125' />
+        {contents.at(currentContentIndex - 1)?.title}
+      </div>
     </Button>
   )
 }
