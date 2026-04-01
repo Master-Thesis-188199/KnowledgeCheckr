9b53b6473de7df5fdbebfcebb443d501b6daf959
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-04-01 07:42:08 +0200

    feat: added `ContentRenderer` component
    This component renders the currently selected course content in a readOnly `RichtTextEditor`.

--- src/app/[locale]/courses/contents/[courseId]/page.tsx
+++ src/app/[locale]/courses/contents/[courseId]/page.tsx
@@ -1,12 +1,10 @@
 import { notFound } from 'next/navigation'
 import { getCourseById } from '@/database/course/select'
 import { ContentProvider } from '@/src/components/courses/contents/[courseId]/ContentProvider'
+import { ContentRenderer } from '@/src/components/courses/contents/[courseId]/ContentRenderer'
 import { ContentSwitchButton } from '@/src/components/courses/contents/[courseId]/ContentSwitchButton'
 import { Button } from '@/src/components/shadcn/button'
 import PageHeading from '@/src/components/Shared/PageHeading'
-import { RichTextEditor } from '@/src/components/tiptap-examples/RichTextEditor'
-import { cn } from '@/src/lib/Shared/utils'
-import { Course } from '@/src/schemas/CourseSchema'
 
 export default async function CourseContentsPage({ params }: { params: Promise<{ courseId: string }> }) {
   const { courseId } = await params
@@ -21,7 +19,7 @@ export default async function CourseContentsPage({ params }: { params: Promise<{
       <ContentProvider initialProps={{ contents: course.contents }}>
         <div className='flex flex-1 flex-col gap-6'>
           <div className='flex flex-1 gap-8'>
-            <ContentWrapper course={course} />
+            <ContentRenderer />
             <div className='flex flex-col gap-2'>
               <h2 className='font-semibold'>Available Contents</h2>
               {course.contents.map((c) => (
@@ -36,16 +34,6 @@ export default async function CourseContentsPage({ params }: { params: Promise<{
           </div>
         </div>
       </ContentProvider>
-    </>
-  )
-}
-
-function ContentWrapper({ course }: { course: Course }) {
-  if (course.contents.length === 0 || !course.contents[0].content) return <>Course has no contents...</>
-
-  return (
-    <>
-      <RichTextEditor readOnly defaultContent={course.contents[0].content} editorPaneClassname={cn('border-dashed p-4')} />
     </>
   )
 }
