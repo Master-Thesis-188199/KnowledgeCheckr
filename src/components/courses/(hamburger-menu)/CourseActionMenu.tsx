d44b783718fea08b87bb440e2549bfd38774f55a
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2026-04-05 16:45:30 +0200

    ref: removed deprecated Statistics item from `CourseActionMenu`
    The reason for this change is that because the examination and practice results were unified in the `/[locale]/results` directory the share-key is no longer needed. Hence the old drag-drop that was not removed in the previous migration is now removed.

--- src/components/courses/(hamburger-menu)/CourseActionMenu.tsx
+++ src/components/courses/(hamburger-menu)/CourseActionMenu.tsx
@@ -165,11 +165,6 @@ export default function CourseActionMenu({ id, questions, share_key, owner_id, c
             {t('clone_course.label')}
             <CopyPlusIcon className='size-4 text-neutral-600 group-data-disabled:text-inherit dark:text-neutral-400 dark:group-data-disabled:text-inherit' />
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href={`/checks/${share_key}/results`} className='flex flex-1 justify-between'>
-              {t('inspect_statistics.label')}
-            </Link>
-          </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
 
